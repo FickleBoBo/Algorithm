7f6fac7d48ed13841ce2a548b097b08a85e4a04c
FickleBoBo <yukmw0704@gmail.com>
2024-03-21 00:12:10 +0900

    2024 03 20 집

---
+++ month_03/src/day_20/BOJ_S3_15652/Main.java
@@ -0,0 +1,7 @@
+package day_21.BOJ_S3_15652;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ month_03/src/day_20/BOJ_S3_15654/Main.java
@@ -0,0 +1,7 @@
+package day_21.BOJ_S3_15654;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
