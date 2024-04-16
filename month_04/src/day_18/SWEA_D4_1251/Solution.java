5a4ddd59b76f53da80e2930fbc4739a0bd5331f4
FickleBoBo <yukmw0704@gmail.com>
2024-04-17 00:48:46 +0900

    2024 04 16 집

---
+++ month_04/src/day_18/SWEA_D4_1251/Solution.java
@@ -0,0 +1,45 @@
+//package day_18.SWEA_D4_1251;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Scanner;
+//
+//public class Solution {
+//
+//	static class Edge implements Comparable<Edge> {
+//		int x, y, w;
+//
+//		public Edge(int x, int y, int w) {
+//			this.x = x;
+//			this.y = y;
+//			this.w = w;
+//		}
+//
+//		@Override
+//		public int compareTo(Edge o) {
+//			return this.w - o.w;
+//		}
+//	}
+//
+//	public static void main(String[] args) {
+//
+//		Scanner sc = new Scanner(System.in);
+//		int T = sc.nextInt();
+//
+//		for(int tc=1 ; tc<=T ; tc++) {
+//			int V = sc.nextInt();
+//			int[][] pos = new int[V][2];
+//			for(int j=0 ; j<2 ; j++) {
+//				for(int i=0 ; i<V ; i++) {
+//					pos[i][j] = sc.nextInt();
+//				}
+//			}
+//			double E = sc.nextDouble();
+//
+//			List<Edge>[] edges = new ArrayList()[];
+//
+//		}
+//
+//		sc.close();
+//	}
+//}
