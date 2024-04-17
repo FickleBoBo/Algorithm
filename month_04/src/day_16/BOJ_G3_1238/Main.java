dcec4279f90c51da50060f96ee005ae72f57c29b
FickleBoBo <yukmw0704@gmail.com>
2024-04-17 16:56:06 +0900

    2024 04 17 싸피

--- month_04/src/day_16/BOJ_G3_1238/Main.java
+++ month_04/src/day_16/BOJ_G3_1238/Main.java
@@ -38,14 +38,16 @@ public class Main {
 			edges[st].add(new Edge(st, ed, w));
 		}
 		
+		/* 학생 마을 -> X 마을 -> 학생 마을로 가는 걸 구하는 문제로 유방향 그래프라서 다익스트라로 해결(무조건 연결 + 간선 양수)
+		 * X 마을 -> 학생 마을은 한번만 구해서 사용해도 되는데 학생 마을 -> X 마을은 매번 구해야 됨(다익은 시작점 기준이라)
+		 */
 		int ans = 0;
 		int[] XToHome = Dijkstra(X);
 		for(int i=1 ; i<=N ; i++) {
-			ans = Math.max(ans, Dijkstra(i)[X] + XToHome[i]);
+			ans = Math.max(ans, Dijkstra(i)[X] + XToHome[i]);    // 다익에서는 최솟값을 찾아야하지만 출력 정답은 최솟값 중 최댓값을 찾아야 함
 		}
 		
 		System.out.println(ans);
-		
 		sc.close();
 	}
 
@@ -56,7 +58,7 @@ public class Main {
 		Arrays.fill(dist, Integer.MAX_VALUE);
 		dist[start] = 0;
 		
-		PriorityQueue<Edge> pq = new PriorityQueue<>();
+		PriorityQueue<Edge> pq = new PriorityQueue<>();    // 유튜브 보니까 다익은 그냥 우선순위 큐로 하는게 좋아 보임(노드 많아지면 느리다고 함)
 		for(Edge e : edges[start]) {
 			dist[e.ed] = dist[e.st] + e.w;
 			pq.add(e);
@@ -72,7 +74,7 @@ public class Main {
 	            
                 if(dist[e.ed] > dist[e.st] + e.w) {
                     dist[e.ed] = dist[e.st] + e.w;
-                    pq.add(new Edge(e.st, e.ed, dist[e.ed]));
+                    pq.add(new Edge(e.st, e.ed, dist[e.ed]));    // 프림이랑 구현 비슷해보이지만 다름
                 }
             }
         }
