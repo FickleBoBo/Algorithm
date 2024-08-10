8985720bf48c2e20c7fbe9f96cdf42b24c5d8dd2
FickleBoBo <yukmw0704@gmail.com>
2024-08-10 18:07:40 +0900

    2024 08 10 home

---
+++ month_08/src/day_10/BOJ_B5_2743/Main.java
@@ -0,0 +1,11 @@
+package day_10.BOJ_B5_2743;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(br.readLine().length());
+    }
+}
