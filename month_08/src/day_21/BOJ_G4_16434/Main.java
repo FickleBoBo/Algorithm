05614183956e676f207e776b1a5e842c77a70414
FickleBoBo <yukmw0704@gmail.com>
2024-08-30 01:20:59 +0900

    2024 08 29 home

--- month_08/src/day_21/BOJ_G4_16434/Main.java
+++ month_08/src/day_21/BOJ_G4_16434/Main.java
@@ -3,12 +3,14 @@ package day_21.BOJ_G4_16434;
 import java.io.*;
 import java.util.*;
 
+// 타입 캐스팅 예외가 많이 나서 long이 섞이면 그냥 long으로 대충 통일하는게 맘편함
 public class Main {
 
+    // 방 클래스
     static class Room{
-        int type;
-        long atk;
-        long hp;
+        int type;    // 몬스터 방은 1, 포션 방은 2 (enum으로 해보려했는데 문법을 몰라서 포기)
+        long atk;    // 몬스터 방이면 몬스터의 공격력, 포션 방이면 포션의 공격력 증가
+        long hp;    // 몬스터 방이면 몬스터의 체력, 포션 방이면 포션의 체력 증가
 
         public Room(int type, long atk, long hp) {
             this.type = type;
@@ -17,17 +19,23 @@ public class Main {
         }
     }
 
+    // 용사 클래스
     static class Hero{
-        long heroMaxHP;
-        long heroCurHP;
-        long heroAtk;
+        long heroMaxHP;    // 용사의 최대 체력
+        long heroCurHP;    // 용사의 현재 체력
+        long heroAtk;    // 용사의 공격력
 
+        // 생성자는 최대 체력과 공격력만 받고 현재 체력도 최대 체력으로 초기화
         public Hero(long heroMaxHP, long heroAtk) {
             this.heroMaxHP = heroMaxHP;
             this.heroCurHP = heroMaxHP;
             this.heroAtk = heroAtk;
         }
 
+        // 몬스터와 싸워서 용사가 사망하면 false, 몬스터가 사망하면 true
+        // 싸우는 원리는 서로의 공격력과 현재 체력을 기반으로 공격 횟수를 계산
+        // 용사가 선빵이니까 용사의 공격 횟수가 몬스터의 공격 횟수보다 많으면 게 진다(false)
+        // 반복문으로 한번씩 공방을 주고 받으면 시간초과
         public boolean fight(long monsHP, long monsAtk){
             long heroAtkCnt = monsHP / heroAtk;
             if(monsHP % heroAtk > 0) heroAtkCnt += 1;
@@ -44,6 +52,7 @@ public class Main {
             }
         }
 
+        // 포션 먹기
         public void drinkPotion(long potionHP, long potionAtk){
             heroCurHP = Math.min(heroCurHP + potionHP, heroMaxHP);
             heroAtk += potionAtk;
@@ -73,6 +82,7 @@ public class Main {
         System.out.println(ans);
     }
 
+    // 매개변수 이분탐색으로 용사가 드래곤을 쓰러트리기 위한 체력을 찾음
     private static long binarySearchLowerBound(long heroAtk, int N, Room[] input){
         long left = 1;
         long right = Long.MAX_VALUE - 1;
@@ -80,9 +90,11 @@ public class Main {
         while(left < right){
             long heroMaxHP = left / 2 + right / 2;
 
+            // 매개변수 이분탐색으로 용사의 체력을 세팅
             boolean flag = true;
             Hero hero = new Hero(heroMaxHP, heroAtk);
 
+            // 해당 체력으로 던전을 돌아서 사망했는지 살아있는지 flag에 저장
             for(int i=0 ; i<N ; i++){
                 if(input[i].type == 1){
                     if(!hero.fight(input[i].hp, input[i].atk)){
@@ -95,6 +107,7 @@ public class Main {
                 }
             }
 
+            // 용사가 사망했으면 체력을 높여서 탐색하고 용사가 살아있으면 체력을 낮춰서 탐색
             if(!flag){
                 left = heroMaxHP + 1;
             }
--- month_08/src/day_25/BOJ_G4_9935/Main.java
+++ month_08/src/day_25/BOJ_G4_9935/Main.java
@@ -13,15 +13,18 @@ public class Main {
         char[] input = br.readLine().toCharArray();
         String str = br.readLine();
 
-        Deque<Character> deque = new ArrayDeque<>();
-        Deque<Character> stack = new ArrayDeque<>();
+        Deque<Character> deque = new ArrayDeque<>();    // 문자열의 문자를 저장하는 디큐로 출력은 큐로 판정은 스택으로 해줌
+        Deque<Character> stack = new ArrayDeque<>();    // 폭발 문자열인지 판단하기 위한 스택
 
         for(char c : input){
+            // 폭발 문자열의 끝 문자를 찾으면 폭발 문자열인지 디큐에서 하나씩 꺼내보면서 확인
+            // 폭발 문자열의 끝 문자를 스택에 넣고 디큐의 나머지 문자들이 폭발 문자열이 되는지 확인해서 flag를 갱신
             if(c == str.charAt(str.length()-1)){
                 stack.push(c);
 
                 boolean flag = true;
 
+                // 폭발 문자열의 길이가 디큐의 길이보다 크면 폭발 문자열일 수 없다
                 if(deque.size() < str.length() - 1){
                     flag = false;
                 }
@@ -37,6 +40,7 @@ public class Main {
                     }
                 }
 
+                // 폭발 문자열이면 스택을 비우면 되고 폭발 문자열이 아니면 스택에 있는 문자를 디큐에 다 넣어줌
                 if(flag){
                     stack.clear();
                 }
@@ -46,6 +50,7 @@ public class Main {
                     }
                 }
             }
+            // 폭발 문자열의 끝 문자가 아니면 그냥 디큐에 넣으면 됨
             else{
                 deque.addLast(c);
             }
