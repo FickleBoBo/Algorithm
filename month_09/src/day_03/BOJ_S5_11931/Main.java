15f38f862daf094d9d5c982642199b6f50bdf35d
FickleBoBo <yukmw0704@gmail.com>
2024-09-05 23:52:38 +0900

    2024 09 05 home

---
+++ month_09/src/day_03/BOJ_S5_11931/Main.java
@@ -0,0 +1,29 @@
+package day_03.BOJ_S5_11931;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        final int MAX = 1_000_000;
+
+        boolean[] visited = new boolean[MAX+1+MAX];
+        for(int i=0 ; i<N ; i++){
+            int n = Integer.parseInt(br.readLine());
+            visited[n + MAX] = true;
+        }
+
+        for(int i=(MAX * 2) ; i>=0 ; i--){
+            if(visited[i]){
+                sb.append(i - MAX).append("\n");
+            }
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
