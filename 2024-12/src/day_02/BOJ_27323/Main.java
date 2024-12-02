810164972d82b9a7f36061052f393f80edb19d97
FickleBoBo <yukmw0704@gmail.com>
2024-12-03 00:57:28 +0900

    BOJ 27323 직사각형

---
+++ 2024-12/src/day_02/BOJ_27323/Main.java
@@ -0,0 +1,14 @@
+package day_02.BOJ_27323;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+
+        System.out.println(A * B);
+    }
+}
