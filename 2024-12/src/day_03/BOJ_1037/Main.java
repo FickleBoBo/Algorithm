6a447a54c9773b5edba86762e9df54c9e99b2532
FickleBoBo <yukmw0704@gmail.com>
2024-12-03 19:14:36 +0900

    BOJ 1037 약수

---
+++ 2024-12/src/day_03/BOJ_1037/Main.java
@@ -0,0 +1,22 @@
+package day_03.BOJ_1037;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        System.out.println(arr[0] * arr[N - 1]);
+    }
+}
