4836d0462625683a217241328c7274c9a49c9f4f
FickleBoBo <yukmw0704@gmail.com>
2024-12-29 23:27:03 +0900

    BOJ 13414 수강신청

---
+++ 2024-12/src/day_28/BOJ_13414/Main.java
@@ -0,0 +1,60 @@
+package day_28.BOJ_13414;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 풀이 시간 : 20분
+ * 시간 : 556ms
+ * 메모리 : 66744KB
+ * 코드 길이 : 1339B
+ */
+
+public class Main {
+
+    static class Student implements Comparable<Student> {
+        String id;
+        int order;
+
+        public Student(String id, int order) {
+            this.id = id;
+            this.order = order;
+        }
+
+        @Override
+        public int compareTo(Student o) {
+            return Integer.compare(this.order, o.order);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int K = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+
+        Map<String, Student> map = new HashMap<>();
+
+        for (int i = 0; i < L; i++) {
+            String id = br.readLine();
+
+            map.put(id, new Student(id, i));
+        }
+
+        PriorityQueue<Student> pq = new PriorityQueue<>(map.values());
+
+        int cnt = 0;
+        while (!pq.isEmpty()) {
+            sb.append(pq.poll().id).append("\n");
+            cnt++;
+
+            if (cnt == K) break;
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
