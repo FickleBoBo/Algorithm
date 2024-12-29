9453ec1e6369fa2d81d58f3c8856e4f9c60a5bb5
FickleBoBo <yukmw0704@gmail.com>
2024-12-29 23:25:58 +0900

    BOJ 2742 기찍 N

---
+++ 2024-12/src/day_29/BOJ_2742/Main.java
@@ -0,0 +1,20 @@
+package day_29.BOJ_2742;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = N; i >= 1; i--) {
+            sb.append(i).append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
