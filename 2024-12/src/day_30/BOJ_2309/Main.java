299d1c167010643234432e159ec2c1b4abffb023
FickleBoBo <yukmw0704@gmail.com>
2024-12-30 01:56:00 +0900

    BOJ 2309 일곱 난쟁이

---
+++ 2024-12/src/day_30/BOJ_2309/Main.java
@@ -0,0 +1,39 @@
+package day_30.BOJ_2309;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int[] arr = new int[9];
+        int sum = 0;
+
+        for (int i = 0; i < 9; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
+        }
+
+        out:
+        for (int i = 0; i < 8; i++) {
+            for (int j = i + 1; j < 9; j++) {
+                if (sum - arr[i] - arr[j] == 100) {
+                    arr[i] = 0;
+                    arr[j] = 0;
+                    break out;
+                }
+            }
+        }
+
+        Arrays.sort(arr);
+        for (int i = 2; i < 9; i++) {
+            sb.append(arr[i]).append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
