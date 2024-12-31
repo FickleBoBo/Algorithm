82ab8cd32317967fd88ba58b0027bacfdb47c2c1
FickleBoBo <yukmw0704@gmail.com>
2024-12-31 22:05:12 +0900

    BOJ 10813 공 바꾸기

---
+++ 2024-12/src/day_31/BOJ_10813/Main.java
@@ -0,0 +1,42 @@
+package day_31.BOJ_10813;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = i + 1;
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken()) - 1;
+            int b = Integer.parseInt(st.nextToken()) - 1;
+            swap(arr, a, b);
+        }
+
+        for (int n : arr) {
+            sb.append(n).append(" ");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+    private static void swap(int[] arr, int a, int b) {
+        int tmp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = tmp;
+    }
+
+}
