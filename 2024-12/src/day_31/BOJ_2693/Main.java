0e6ecfde4d2e6469d53da71afeaed84784ba3a40
FickleBoBo <yukmw0704@gmail.com>
2025-01-01 00:42:31 +0900

    BOJ 2693 N번째 큰 수

---
+++ 2024-12/src/day_31/BOJ_2693/Main.java
@@ -0,0 +1,30 @@
+package day_31.BOJ_2693;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int tc = 1; tc <= T; tc++) {
+            int[] arr = new int[10];
+
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < 10; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+            Arrays.sort(arr);
+
+            sb.append(arr[7]).append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
