3ce89692e079e719097094e7d724ea347aa282f3
FickleBoBo <yukmw0704@gmail.com>
2024-12-31 21:43:44 +0900

    BOJ 5537 웰컴

---
+++ 2024-12/src/day_31/BOJ_5537/Main.java
@@ -0,0 +1,9 @@
+package day_31.BOJ_5537;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(".  .   .\n" +
+                "|  | _ | _. _ ._ _  _\n" +
+                "|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
