921010a91633b32682550d363ab160c16e5e3199
FickleBoBo <yukmw0704@gmail.com>
2024-12-31 22:21:19 +0900

    BOJ 11654 아스키 코드

---
+++ 2024-12/src/day_31/BOJ_11654/Main.java
@@ -0,0 +1,13 @@
+package day_31.BOJ_11654;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        char input = br.readLine().charAt(0);
+
+        System.out.println((int) input);
+    }
+}
