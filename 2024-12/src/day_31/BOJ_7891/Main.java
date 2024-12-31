3ee29a1e4e95d2b97806b1e59501f0d4dd7cae5b
FickleBoBo <yukmw0704@gmail.com>
2024-12-31 21:52:12 +0900

    BOJ 7891 Can you add this?

---
+++ 2024-12/src/day_31/BOJ_7891/Main.java
@@ -0,0 +1,27 @@
+package day_31.BOJ_7891;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            sb.append(a + b).append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
