46480e29300e00851d96f092dadb95d82e7c2df0
FickleBoBo <yukmw0704@gmail.com>
2025-01-01 00:39:32 +0900

    BOJ 1789 수들의 합

---
+++ 2024-12/src/day_31/BOJ_1789/Main.java
@@ -0,0 +1,18 @@
+package day_31.BOJ_1789;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        long S = Long.parseLong(br.readLine());
+
+        long n = 1;
+        while (n * (n + 1) / 2 <= S) {
+            n++;
+        }
+
+        System.out.println(n - 1);
+    }
+}
