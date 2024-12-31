6a6e8f3e2ace31dc5f7ad8fbb03de020994808de
FickleBoBo <yukmw0704@gmail.com>
2024-12-31 22:23:33 +0900

    BOJ 11382 꼬마 정민

---
+++ 2024-12/src/day_31/BOJ_11382/Main.java
@@ -0,0 +1,17 @@
+package day_31.BOJ_11382;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long C = Long.parseLong(st.nextToken());
+
+        System.out.println(A + B + C);
+    }
+}
