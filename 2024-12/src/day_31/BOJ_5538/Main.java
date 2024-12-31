b2c7c392e1f340ba85883c4c7cc69859a0d2bbcd
FickleBoBo <yukmw0704@gmail.com>
2024-12-31 21:44:04 +0900

    BOJ 5538 마이크로소프트 로고

---
+++ 2024-12/src/day_31/BOJ_5538/Main.java
@@ -0,0 +1,11 @@
+package day_31.BOJ_5538;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("       _.-;;-._\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-;;-._|\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-''-._|");
+    }
+}
