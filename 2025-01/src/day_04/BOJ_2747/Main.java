798c205177bdabb091402debcfeda13c5f0d5d86
FickleBoBo <yukmw0704@gmail.com>
2025-01-04 03:16:50 +0900

    BOJ 2747 피보나치 수

---
+++ 2025-01/src/day_04/BOJ_2747/Main.java
@@ -0,0 +1,20 @@
+package day_04.BOJ_2747;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[1 + N];
+        dp[1] = 1;
+
+        for (int i = 2; i <= N; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        System.out.println(dp[N]);
+    }
+}
