302a569a44b123044e31c7680426311f5e8c53d7
FickleBoBo <yukmw0704@gmail.com>
2025-01-03 22:19:28 +0900

    BOJ 23037 5의 수난

---
+++ 2025-01/src/day_03/BOJ_23037/Main.java
@@ -0,0 +1,18 @@
+package day_03.BOJ_23037;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        char[] input = br.readLine().toCharArray();
+
+        int sum = 0;
+        for (char c : input) {
+            sum += (int) Math.pow(c - '0', 5);
+        }
+
+        System.out.println(sum);
+    }
+}
