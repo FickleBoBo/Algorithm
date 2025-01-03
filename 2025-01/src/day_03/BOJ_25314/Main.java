102b81bb9d67c35d507678533377294d3dc0eccb
FickleBoBo <yukmw0704@gmail.com>
2025-01-03 22:21:24 +0900

    BOJ 25314 코딩은 체육과목 입니다

---
+++ 2025-01/src/day_03/BOJ_25314/Main.java
@@ -0,0 +1,18 @@
+package day_03.BOJ_25314;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N / 4; i++) {
+            bw.write("long ");
+        }
+        bw.write("int");
+        bw.flush();
+    }
+}
