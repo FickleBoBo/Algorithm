ea69e85ec81521f9d6655239c8a91ca55861dee9
FickleBoBo <yukmw0704@gmail.com>
2025-01-03 22:18:07 +0900

    BOJ 1520 내리막 길

---
+++ 2025-01/src/day_03/BOJ_1520/Main.java
@@ -0,0 +1,55 @@
+package day_03.BOJ_1520;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static final int[] dr = {-1, 0, 1, 0};
+    private static final int[] dc = {0, 1, 0, -1};
+    private static int M;
+    private static int N;
+    private static int[][] map;
+    private static int[][] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+
+        map = new int[M][N];
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        dp = new int[M][N];
+        for (int i = 0; i < M; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+
+        System.out.println(dfs(0, 0));
+    }
+
+    private static int dfs(int r, int c) {
+        if (r == M - 1 && c == N - 1) return 1;
+        if (dp[r][c] != -1) return dp[r][c];
+
+        int sum = 0;
+        for (int dir = 0; dir < 4; dir++) {
+            int nr = r + dr[dir];
+            int nc = c + dc[dir];
+
+            if (nr < 0 || nr >= M || nc < 0 || nc >= N) continue;
+            if (map[nr][nc] >= map[r][c]) continue;
+
+            sum += dfs(nr, nc);
+        }
+
+        return dp[r][c] = sum;
+    }
+}
