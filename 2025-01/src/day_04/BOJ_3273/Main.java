7e2dadbf7440426a6336d38bb2b302bb90327ab4
FickleBoBo <yukmw0704@gmail.com>
2025-01-04 03:14:44 +0900

    BOJ 3273 두 수의 합

---
+++ 2025-01/src/day_04/BOJ_3273/Main.java
@@ -0,0 +1,42 @@
+package day_04.BOJ_3273;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        int X = Integer.parseInt(br.readLine());
+
+        int left = 0;
+        int right = N - 1;
+        int cnt = 0;
+
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+
+            if (sum < X) {
+                left++;
+            } else if (sum > X) {
+                right--;
+            } else {
+                left++;
+                right--;
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
