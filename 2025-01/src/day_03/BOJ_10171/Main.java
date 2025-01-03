4ec92dd8d74623507bb98af129692f3a9191620b
FickleBoBo <yukmw0704@gmail.com>
2025-01-03 22:18:57 +0900

    BOJ 10171 고양이

---
+++ 2025-01/src/day_03/BOJ_10171/Main.java
@@ -0,0 +1,10 @@
+package day_03.BOJ_10171;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\\n" +
+                " )  ( ')\n" +
+                "(  /  )\n" +
+                " \\(__)|");
+    }
+}
