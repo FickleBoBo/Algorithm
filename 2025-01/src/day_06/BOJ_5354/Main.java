22e35234502fc4fd19d62f6ede142698c93fefa2
FickleBoBo <yukmw0704@gmail.com>
2025-01-06 02:35:45 +0900

    BOJ 5354 J박스

---
+++ 2025-01/src/day_06/BOJ_5354/Main.java
@@ -0,0 +1,28 @@
+package day_06.BOJ_5354;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int tc = 1; tc <= T; tc++) {
+            int N = Integer.parseInt(br.readLine());
+
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (i == 0 || i == N - 1 || j == 0 || j == N - 1) bw.write("#");
+                    else bw.write("J");
+                }
+                bw.newLine();
+            }
+
+            bw.newLine();
+        }
+
+        bw.flush();
+    }
+}
