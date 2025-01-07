e2f90e366f531adc332ee3a5637137cce4466d09
FickleBoBo <yukmw0704@gmail.com>
2025-01-08 02:51:07 +0900

    BOJ 1546 평균

---
+++ 2025-01/src/day_08/BOJ_1546/Main.java
@@ -0,0 +1,24 @@
+package day_08.BOJ_1546;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        int sum = 0;
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            sum += arr[i];
+        }
+        Arrays.sort(arr);
+
+        System.out.println((double) sum / arr[N - 1] * 100 / arr.length);
+    }
+}
