ca04cdfee72277c73c8c6211dfd057dd0e104305
FickleBoBo <yukmw0704@gmail.com>
2025-01-08 02:50:54 +0900

    BOJ 2738 행렬 덧셈

---
+++ 2025-01/src/day_08/BOJ_2738/Main.java
@@ -0,0 +1,37 @@
+package day_08.BOJ_2738;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] matrix = new int[N][M];
+
+        for (int k = 0; k < 2; k++) {
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < M; j++) {
+                    matrix[i][j] += Integer.parseInt(st.nextToken());
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                sb.append(matrix[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
