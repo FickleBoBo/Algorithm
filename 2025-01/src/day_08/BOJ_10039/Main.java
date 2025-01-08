df05aa02925902e7e200c0bfcd80238692fa30fb
FickleBoBo <yukmw0704@gmail.com>
2025-01-08 22:09:42 +0900

    BOJ 10039 평균 점수

---
+++ 2025-01/src/day_08/BOJ_10039/Main.java
@@ -0,0 +1,17 @@
+package day_08.BOJ_10039;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            int score = Integer.parseInt(br.readLine());
+            sum += Math.max(score, 40);
+        }
+
+        System.out.println(sum / 5);
+    }
+}
