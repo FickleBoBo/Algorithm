0183c15d9914b8a8ce3adfc77bdc36280a048357
FickleBoBo <yukmw0704@gmail.com>
2025-01-10 00:31:46 +0900

    BOJ 1463 1로 만들기

--- 2024-12/src/day_27/BOJ_1463/Main_DP2.java
+++ 2024-12/src/day_27/BOJ_1463/Main_DP2.java
@@ -3,7 +3,7 @@ package day_27.BOJ_1463;
 import java.io.*;
 import java.util.*;
 
-public class Main_DFS {
+public class Main_DP2 {
 
     private static final Map<Integer, Integer> dp = new HashMap<>();
 
@@ -13,16 +13,16 @@ public class Main_DFS {
         int N = Integer.parseInt(br.readLine());
         dp.put(1, 0);
 
-        System.out.println(dfs(N));
+        System.out.println(recur(N));
     }
 
-    private static int dfs(int N) {
+    private static int recur(int N) {
         if (dp.containsKey(N)) return dp.get(N);
 
-        if (N % 6 == 0) dp.put(N, Math.min(dfs(N - 1), Math.min(dfs(N / 3), dfs(N / 2))) + 1);
-        else if (N % 3 == 0) dp.put(N, Math.min(dfs(N / 3), dfs(N - 1)) + 1);
-        else if (N % 2 == 0) dp.put(N, Math.min(dfs(N / 2), dfs(N - 1)) + 1);
-        else dp.put(N, dfs(N - 1) + 1);
+        if (N % 6 == 0) dp.put(N, Math.min(recur(N - 1), Math.min(recur(N / 3), recur(N / 2))) + 1);
+        else if (N % 3 == 0) dp.put(N, Math.min(recur(N / 3), recur(N - 1)) + 1);
+        else if (N % 2 == 0) dp.put(N, Math.min(recur(N / 2), recur(N - 1)) + 1);
+        else dp.put(N, recur(N - 1) + 1);
 
         return dp.get(N);
     }
