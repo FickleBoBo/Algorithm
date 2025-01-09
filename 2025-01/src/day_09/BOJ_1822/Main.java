4551bc9f7d636f5243fcebb0fa55a3897be22581
FickleBoBo <yukmw0704@gmail.com>
2025-01-10 00:51:46 +0900

    BOJ 1822 차집합

---
+++ 2025-01/src/day_09/BOJ_1822/Main.java
@@ -0,0 +1,35 @@
+package day_09.BOJ_1822;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        Set<Integer> A = new TreeSet<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A.add(Integer.parseInt(st.nextToken()));
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            A.remove(Integer.parseInt(st.nextToken()));
+        }
+
+        sb.append(A.size()).append("\n");
+        for (int n : A) {
+            sb.append(n).append(" ");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
