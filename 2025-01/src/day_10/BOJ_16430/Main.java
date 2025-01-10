9a3c0fcfd9e420ace0bdcd8fd7d7c42faa3e5106
FickleBoBo <yukmw0704@gmail.com>
2025-01-11 06:02:48 +0900

    BOJ 16430 제리와 톰

---
+++ 2025-01/src/day_10/BOJ_16430/Main.java
@@ -0,0 +1,21 @@
+package day_10.BOJ_16430;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        sb.append(B - A).append(" ").append(B);
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
