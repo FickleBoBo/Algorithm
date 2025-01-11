06d6e73568d46e8e4d29a18e0cd356027815065f
FickleBoBo <yukmw0704@gmail.com>
2025-01-11 17:46:35 +0900

    BOJ 15651 N과 M (3)

---
+++ 2025-01/src/day_11/BOJ_15651/Main.java
@@ -0,0 +1,46 @@
+package day_11.BOJ_15651;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static final StringBuilder sb = new StringBuilder();
+
+    private static int[] arr;
+    private static int[] sel;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        sel = new int[M];
+
+        permutation(0);
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+    // 중복 순열
+    private static void permutation(int selIdx) {
+        if (selIdx == sel.length) {
+            for (int n : sel) {
+                sb.append(n).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            sel[selIdx] = i + 1;
+            permutation(selIdx + 1);
+        }
+    }
+
+}
