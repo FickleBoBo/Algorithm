e4b0a618fef1cd613c801655569f6057104935c4
FickleBoBo <yukmw0704@gmail.com>
2025-01-12 13:52:14 +0900

    BOJ 1550 16진수

---
+++ 2025-01/src/day_12/BOJ_1550/Main.java
@@ -0,0 +1,13 @@
+package day_12.BOJ_1550;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine(), 16);
+
+        System.out.println(n);
+    }
+}
