f6f770e8d22d2b520a3414f438a07834af6ee218
FickleBoBo <yukmw0704@gmail.com>
2025-01-12 13:52:04 +0900

    BOJ 1920 수 찾기

---
+++ 2025-01/src/day_12/BOJ_1920/Main.java
@@ -0,0 +1,33 @@
+package day_12.BOJ_1920;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        Set<Integer> set = new HashSet<>();
+        st = new StringTokenizer(br.readLine());
+        while (st.hasMoreTokens()) {
+            set.add(Integer.parseInt(st.nextToken()));
+        }
+
+        int M = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
+        while (st.hasMoreTokens()) {
+            if (set.contains(Integer.parseInt(st.nextToken()))) {
+                bw.write("1\n");
+            } else {
+                bw.write("0\n");
+            }
+        }
+
+        bw.flush();
+    }
+}
