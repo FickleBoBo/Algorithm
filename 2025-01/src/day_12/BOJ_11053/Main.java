0a56cd773692b06fb4336ad1b1181222e28f8c59
FickleBoBo <yukmw0704@gmail.com>
2025-01-13 09:19:02 +0900

    BOJ 11053 가장 긴 증가하는 부분 수열

---
+++ 2025-01/src/day_12/BOJ_11053/Main.java
@@ -0,0 +1,32 @@
+package day_12.BOJ_11053;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[N];
+        Arrays.fill(dp, 1);
+        int max = 0;
+
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < k; i++) {
+                if (arr[i] < arr[k]) dp[k] = Math.max(dp[k], dp[i] + 1);
+            }
+            max = Math.max(max, dp[k]);
+        }
+
+        System.out.println(max);
+    }
+}
