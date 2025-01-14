bf32a302c711c93c24fced5ae6b82196b448f3e0
FickleBoBo <yukmw0704@gmail.com>
2025-01-14 11:06:30 +0900

    BOJ 12348 분해합 2

---
+++ 2025-01/src/day_14/BOJ_12348/Main.java
@@ -0,0 +1,38 @@
+package day_14.BOJ_12348;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = br.readLine();
+        long len = input.length();
+
+        long N = Long.parseLong(input);
+        long ans = 0;
+
+        for (long i = N - 9 * len; i <= N; i++) {
+            long sum = solve(i);
+
+            if (sum == N) {
+                ans = i;
+                break;
+            }
+        }
+
+        System.out.println(ans);
+    }
+
+    private static long solve(long N) {
+        long sum = N;
+
+        while (N != 0) {
+            sum += N % 10;
+            N /= 10;
+        }
+
+        return sum;
+    }
+
+}
