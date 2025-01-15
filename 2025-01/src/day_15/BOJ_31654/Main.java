9190260a31cdcc4ea3cb785d1b21a2ee1a791e4d
FickleBoBo <yukmw0704@gmail.com>
2025-01-15 11:20:48 +0900

    BOJ 31654 Adding Trouble

---
+++ 2025-01/src/day_15/BOJ_31654/Main.java
@@ -0,0 +1,18 @@
+package day_15.BOJ_31654;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        if (A + B == C) System.out.println("correct!");
+        else System.out.println("wrong!");
+    }
+}
