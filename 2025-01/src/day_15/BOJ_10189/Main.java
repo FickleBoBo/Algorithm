f4681e9cea1e278818fa01da0a57a152ab82173d
FickleBoBo <yukmw0704@gmail.com>
2025-01-15 11:18:48 +0900

    BOJ 10189 Hook

---
+++ 2025-01/src/day_15/BOJ_10189/Main.java
@@ -0,0 +1,10 @@
+package day_15.BOJ_10189;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("#  # #### #### #  #\n" +
+                "#### #  # #  # # #\n" +
+                "#### #  # #  # # #\n" +
+                "#  # #### #### #  #");
+    }
+}
