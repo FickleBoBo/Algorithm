ba2c14435464479491515ff03c096c99e33c8708
FickleBoBo <yukmw0704@gmail.com>
2025-01-15 13:09:56 +0900

    BOJ 4562 No Brainer

---
+++ 2025-01/src/day_15/BOJ_4562/Main.java
@@ -0,0 +1,25 @@
+package day_15.BOJ_4562;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            if (x < y) bw.write("NO BRAINS\n");
+            else bw.write("MMM BRAINS\n");
+        }
+
+        bw.flush();
+    }
+}
