87c6b554ec30be4d5712af438d425f4d73e199f8
FickleBoBo <yukmw0704@gmail.com>
2025-01-15 13:09:33 +0900

    BOJ 2798 블랙잭

---
+++ 2025-01/src/day_15/BOJ_2798/Main.java
@@ -0,0 +1,34 @@
+package day_15.BOJ_2798;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        int ans = 0;
+        for (int i = 0; i < N - 2; i++) {
+            for (int j = i + 1; j < N - 1; j++) {
+                for (int k = j + 1; k < N; k++) {
+                    int sum = arr[i] + arr[j] + arr[k];
+                    if (sum <= M) ans = Math.max(ans, sum);
+                    else break;
+                }
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
