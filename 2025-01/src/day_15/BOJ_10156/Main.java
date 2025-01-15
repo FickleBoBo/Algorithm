e69d353e94ac0d1ffa552a8fb6ff9c3da2c3d78f
FickleBoBo <yukmw0704@gmail.com>
2025-01-15 13:37:32 +0900

    BOJ 10156 과자

---
+++ 2025-01/src/day_15/BOJ_10156/Main.java
@@ -0,0 +1,17 @@
+package day_15.BOJ_10156;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        System.out.println(Math.max(K * N - M, 0));
+    }
+}
