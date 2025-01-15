00f7f8111c272fd0e7853eef9450c82300254d08
FickleBoBo <yukmw0704@gmail.com>
2025-01-15 11:21:32 +0900

    BOJ 31611 火曜日 (Tuesday)

---
+++ 2025-01/src/day_15/BOJ_31611/Main.java
@@ -0,0 +1,14 @@
+package day_15.BOJ_31611;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int x = Integer.parseInt(br.readLine());
+
+        if (x % 7 == 2) System.out.println(1);
+        else System.out.println(0);
+    }
+}
