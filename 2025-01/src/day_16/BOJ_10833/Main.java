3a3b22b00198ffe522051cf0e5b8b1127651c66f
FickleBoBo <yukmw0704@gmail.com>
2025-01-16 19:19:54 +0900

    BOJ 10833 사과

---
+++ 2025-01/src/day_16/BOJ_10833/Main.java
@@ -0,0 +1,24 @@
+package day_16.BOJ_10833;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            sum += B % A;
+        }
+
+        System.out.println(sum);
+    }
+}
