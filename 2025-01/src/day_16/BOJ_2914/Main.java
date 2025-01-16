d37ec95ded5aebb66680ba2879a88a834401b0b4
FickleBoBo <yukmw0704@gmail.com>
2025-01-16 16:37:20 +0900

    BOJ 2914 저작권

---
+++ 2025-01/src/day_16/BOJ_2914/Main.java
@@ -0,0 +1,16 @@
+package day_16.BOJ_2914;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int I = Integer.parseInt(st.nextToken());
+
+        System.out.println(A * I - A + 1);
+    }
+}
