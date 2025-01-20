dba99416b003741a23f9ec9da285d56388a15bb7
FickleBoBo <yukmw0704@gmail.com>
2025-01-21 08:53:34 +0900

    BOJ 11727 2×n 타일링 2

---
+++ 2025-01/src/day_21/BOJ_11727/Main.java
@@ -0,0 +1,23 @@
+package day_21.BOJ_11727;
+
+import java.io.*;
+
+public class Main {
+
+    private static final int MOD = 10_007;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[1 + N];
+        dp[0] = dp[1] = 1;
+
+        for (int i = 2; i <= N; i++) {
+            dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % MOD;
+        }
+
+        System.out.println(dp[N]);
+    }
+}
