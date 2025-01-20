34223caf7bf777aff95514232e0585e71391a539
FickleBoBo <yukmw0704@gmail.com>
2025-01-20 21:53:47 +0900

    BOJ 10821 정수의 개수

---
+++ 2025-01/src/day_18/BOJ_10821/Main.java
@@ -0,0 +1,13 @@
+package day_18.BOJ_10821;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), ",");
+
+        System.out.println(st.countTokens());
+    }
+}
