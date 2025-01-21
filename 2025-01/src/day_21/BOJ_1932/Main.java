dfbe33e5d5939c4e019d8a47482cb86c5f40daa8
FickleBoBo <yukmw0704@gmail.com>
2025-01-21 10:29:04 +0900

    BOJ 1932 정수 삼각형

---
+++ 2025-01/src/day_21/BOJ_1932/Main.java
@@ -0,0 +1,36 @@
+package day_21.BOJ_1932;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] map = new int[1 + N][1 + N];
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            for (int j = 1; j <= i; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int[][] dp = new int[1 + N][1 + N];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]) + map[i][j];
+            }
+        }
+
+        int max = 0;
+        for (int i = 1; i <= N; i++) {
+            max = Math.max(max, dp[N][i]);
+        }
+
+        System.out.println(max);
+    }
+}
