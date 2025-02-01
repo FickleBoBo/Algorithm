9e074f9c466f917511ddb34e9b1bc69effc698c7
FickleBoBo <yukmw0704@gmail.com>
2025-02-01 21:07:02 +0900

    BOJ 1753 최단경로

---
+++ 2025-02/src/day_01/BOJ_1753/Main.java
@@ -0,0 +1,88 @@
+package day_01.BOJ_1753;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static class Node implements Comparable<Node> {
+        int v;
+        int w;
+
+        public Node(int v, int w) {
+            this.v = v;
+            this.w = w;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return Integer.compare(this.w, o.w);
+        }
+    }
+
+    private static final int INF = 1_000_000_000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(br.readLine());
+
+        List<List<Node>> adj = new ArrayList<>();
+        for (int i = 0; i <= V; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+
+            adj.get(u).add(new Node(v, w));
+        }
+
+        int[] dist = dijkstra(K, V, adj);
+        for (int i = 1; i <= V; i++) {
+            if (dist[i] == INF) sb.append("INF\n");
+            else sb.append(dist[i]).append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+    private static int[] dijkstra(int start, int N, List<List<Node>> adj) {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(start, 0));
+
+        boolean[] visited = new boolean[1 + N];
+
+        int[] dist = new int[1 + N];
+        Arrays.fill(dist, INF);
+        dist[start] = 0;
+
+        while (!pq.isEmpty()) {
+            Node node = pq.remove();
+
+            if (visited[node.v]) continue;
+            visited[node.v] = true;
+
+            for (Node next : adj.get(node.v)) {
+                if (visited[next.v]) continue;
+
+                if (dist[next.v] > dist[node.v] + next.w) {
+                    dist[next.v] = dist[node.v] + next.w;
+                    pq.add(new Node(next.v, dist[next.v]));
+                }
+            }
+        }
+
+        return dist;
+    }
+
+}
