032da3d7ad4a481a2527aa8e923a44bb89d7fa93
FickleBoBo <yukmw0704@gmail.com>
2025-02-01 21:06:12 +0900

    BOJ 1916 최소비용 구하기

---
+++ 2025-02/src/day_01/BOJ_1916/Main.java
@@ -0,0 +1,84 @@
+package day_01.BOJ_1916;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static class Node implements Comparable<Node> {
+        int v;
+        int w;
+
+        public Node(int v, int w) {
+            this.v = v;
+            this.w = w;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return Integer.compare(this.w, o.w);
+        }
+    }
+
+    private static final int INF = 1_000_000_000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+        List<List<Node>> adj = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int W = Integer.parseInt(st.nextToken());
+
+            adj.get(A).add(new Node(B, W));
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+
+        System.out.println(dijkstra(start, end, N, adj));
+    }
+
+    private static int dijkstra(int start, int end, int N, List<List<Node>> adj) {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(start, 0));
+
+        boolean[] visited = new boolean[1 + N];
+
+        int[] dist = new int[1 + N];
+        Arrays.fill(dist, INF);
+        dist[start] = 0;
+
+        while (!pq.isEmpty()) {
+            Node node = pq.remove();
+
+            if (node.v == end) return dist[node.v];
+
+            if (visited[node.v]) continue;
+            visited[node.v] = true;
+
+            for (Node next : adj.get(node.v)) {
+                if (visited[next.v]) continue;
+
+                if (dist[next.v] > dist[node.v] + next.w) {
+                    dist[next.v] = dist[node.v] + next.w;
+                    pq.add(new Node(next.v, dist[next.v]));
+                }
+            }
+        }
+
+        return -1;
+    }
+
+}
