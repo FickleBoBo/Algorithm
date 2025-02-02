dc1b202d589df6979df29a39f65e505ec13c8f23
FickleBoBo <yukmw0704@gmail.com>
2025-02-02 10:52:11 +0900

    BOJ 17252 삼삼한 수

---
+++ 2025-02/src/day_02/BOJ_17252/Main.java
@@ -0,0 +1,22 @@
+package day_02.BOJ_17252;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        if (N == 0) {
+            System.out.println("NO");
+            return;
+        }
+
+        for (int i = 1162261467; i > 0; i /= 3) {
+            if (N >= i) N -= i;
+        }
+
+        if (N == 0) System.out.println("YES");
+        else System.out.println("NO");
+    }
+}
