7c3c5be396a7020c003383ca25090f28c5793ef5
FickleBoBo <yukmw0704@gmail.com>
2025-02-03 15:32:51 +0900

    BOJ 11725 트리의 부모 찾기

---
+++ 2025-02/src/day_03/BOJ_11725/Main_DFS.java
@@ -0,0 +1,56 @@
+package day_03.BOJ_11725;
+
+import java.io.*;
+import java.util.*;
+
+public class Main_DFS {
+
+    private static final List<List<Integer>> adj = new ArrayList<>();
+    private static boolean[] visited;
+    private static int[] p;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i <= N; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < N - 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+
+        visited = new boolean[1 + N];
+        p = new int[1 + N];
+
+        dfs(1);
+
+        for (int i = 2; i <= N; i++) {
+            sb.append(p[i]).append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+    private static void dfs(int node) {
+        visited[node] = true;
+
+        for (int next : adj.get(node)) {
+            if (visited[next]) continue;
+
+            p[next] = node;
+            dfs(next);
+        }
+    }
+
+}
