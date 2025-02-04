cb0d52206ac2c2743cce96af97dd7a9a3701b930
FickleBoBo <yukmw0704@gmail.com>
2025-02-04 19:06:02 +0900

    BOJ 16504 종이접기

---
+++ 2025-02/src/day_04/BOJ_16504/Main.java
@@ -0,0 +1,25 @@
+package day_04.BOJ_16504;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        long sum = 0;
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            for (int j = 0; j < N; j++) {
+                sum += Integer.parseInt(st.nextToken());
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
