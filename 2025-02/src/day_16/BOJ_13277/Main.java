4b0e4ecc4d22290c3fb88f590a47791040affeef
FickleBoBo <yukmw0704@gmail.com>
2025-02-17 10:42:42 +0900

    BOJ 13277 큰 수 곱셈

---
+++ 2025-02/src/day_16/BOJ_13277/Main.java
@@ -0,0 +1,17 @@
+package day_16.BOJ_13277;
+
+import java.io.*;
+import java.math.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        BigInteger A = new BigInteger(st.nextToken());
+        BigInteger B = new BigInteger(st.nextToken());
+
+        System.out.println(A.multiply(B));
+    }
+}
