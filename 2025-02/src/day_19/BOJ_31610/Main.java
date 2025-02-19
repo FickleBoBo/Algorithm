632b08c29e4e47f7556922dc65ebff85acba3fbb
FickleBoBo <yukmw0704@gmail.com>
2025-02-19 09:23:25 +0900

    BOJ 31610 飴の袋詰め (Drops Packing)

---
+++ 2025-02/src/day_19/BOJ_31610/Main.java
@@ -0,0 +1,15 @@
+package day_19.BOJ_31610;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
+
+        System.out.println(A * B + C);
+    }
+}
