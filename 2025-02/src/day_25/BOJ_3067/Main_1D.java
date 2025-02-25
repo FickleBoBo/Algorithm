477b4484c3007918f5b4d34866f1ea8d1c56c7de
FickleBoBo <yukmw0704@gmail.com>
2025-02-26 01:36:58 +0900

    BOJ 3067 Coins

---
+++ 2025-02/src/day_25/BOJ_3067/Main_1D.java
@@ -0,0 +1,43 @@
+package day_25.BOJ_3067;
+
+import java.io.*;
+import java.util.*;
+
+public class Main_1D {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= T; tc++) {
+            int N = Integer.parseInt(br.readLine());
+
+            int[] coins = new int[1 + N];
+            st = new StringTokenizer(br.readLine());
+            for (int i = 1; i <= N; i++) {
+                coins[i] = Integer.parseInt(st.nextToken());
+            }
+
+            int M = Integer.parseInt(br.readLine());
+
+            int[] dp = new int[1 + M];
+
+            // 0원을 만들 수 있는 경우는 1가지
+            dp[0] = 1;
+
+            for (int i = 1; i <= N; i++) {
+                // 중복 선택을 위해 앞에서부터 갱신
+                for (int j = coins[i]; j <= M; j++) {
+                    dp[j] += dp[j - coins[i]];
+                }
+            }
+
+            sb.append(dp[M]).append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
