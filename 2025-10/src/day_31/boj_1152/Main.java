3852c4cdbb227344ff2dbe94874dd8980eb3ad7c
FickleBoBo <yukmw0704@gmail.com>
2025-11-01 01:09:05 +0900

    BOJ 1152 단어의 개수

---
+++ 2025-10/src/day_31/boj_1152/Main.java
@@ -0,0 +1,12 @@
+package day_31.boj_1152;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(new StringTokenizer(br.readLine()).countTokens());
+    }
+}
