5e2d5a782dc56c1e89e59591e4e65a4620457577
FickleBoBo <yukmw0704@gmail.com>
2025-11-05 17:49:13 +0900

    BOJ 11050 이항 계수 1

---
+++ 2025-11/src/day_05/boj_11050/Main.java
@@ -0,0 +1,23 @@
+package day_05.boj_11050;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        System.out.println(nCr(N, K));
+    }
+
+    private static int nCr(int n, int r) {
+        if (r == 0 || n == r) return 1;
+        if (r == 1) return n;
+
+        return nCr(n - 1, r) + nCr(n - 1, r - 1);
+    }
+}
