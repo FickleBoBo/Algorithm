98601497fc71d2fdf6fb46ac720b157cd76a7d36
FickleBoBo <yukmw0704@gmail.com>
2025-11-05 17:49:02 +0900

    BOJ 15482 한글 LCS

---
+++ 2025-11/src/day_04/boj_15482/Main2.java
@@ -0,0 +1,36 @@
+package day_04.boj_15482;
+
+import java.io.*;
+
+public class Main2 {
+
+    private static int[][] dp;
+    private static boolean[][] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        char[] str1 = br.readLine().toCharArray();
+        char[] str2 = br.readLine().toCharArray();
+        int N = str1.length;
+        int M = str2.length;
+
+        dp = new int[N][M];
+        visited = new boolean[N][M];
+
+        System.out.println(lcs(str1, N - 1, str2, M - 1));
+    }
+
+    private static int lcs(char[] str1, int len1, char[] str2, int len2) {
+        if (len1 == -1 || len2 == -1) return 0;
+
+        if (visited[len1][len2]) return dp[len1][len2];
+
+        visited[len1][len2] = true;
+        if (str1[len1] == str2[len2]) {
+            return dp[len1][len2] = lcs(str1, len1 - 1, str2, len2 - 1) + 1;
+        } else {
+            return dp[len1][len2] = Math.max(lcs(str1, len1 - 1, str2, len2), lcs(str1, len1, str2, len2 - 1));
+        }
+    }
+}
