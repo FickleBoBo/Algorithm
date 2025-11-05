0a322c40511ad06596d1c31aaedc5b9f7dfcf1c8
FickleBoBo <yukmw0704@gmail.com>
2025-11-05 17:49:20 +0900

    BOJ 11051 이항 계수 2

---
+++ 2025-11/src/day_05/boj_11051/Main2.java
@@ -0,0 +1,28 @@
+package day_05.boj_11051;
+
+import java.io.*;
+import java.util.*;
+
+public class Main2 {
+
+    private static final int MOD = 10_007;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[][] dp = new int[1 + N][1 + K];
+        for (int n = 0; n <= N; n++) {
+            dp[n][0] = 1;
+
+            for (int r = 1; r <= Math.min(n, K); r++) {
+                dp[n][r] = (dp[n - 1][r] + dp[n - 1][r - 1]) % MOD;
+            }
+        }
+
+        System.out.println(dp[N][K]);
+    }
+}
