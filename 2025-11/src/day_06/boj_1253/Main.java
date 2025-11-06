d7b913817e56bef61a8b423c64e772b2f8810789
FickleBoBo <yukmw0704@gmail.com>
2025-11-06 23:16:47 +0900

    BOJ 1253 좋다

---
+++ 2025-11/src/day_06/boj_1253/Main.java
@@ -0,0 +1,42 @@
+package day_06.boj_1253;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);  // 이분 탐색을 위해 정렬
+
+        int cnt = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (i == j) continue;
+
+                // 이분 탐색으로 찾으면 좋은 수
+                if (binarySearch(arr, Math.min(i, j), Math.max(i, j), arr[i] - arr[j])) {
+                    cnt++;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(cnt);
+    }
+
+    // 배열을 pivot1, pivot2로 분할한 세 구간 중 key가 존재하는지 리턴
+    private static boolean binarySearch(int[] arr, int pivot1, int pivot2, int key) {
+        return Arrays.binarySearch(arr, 0, pivot1, key) >= 0 ||
+                Arrays.binarySearch(arr, pivot1 + 1, pivot2, key) >= 0 ||
+                Arrays.binarySearch(arr, pivot2 + 1, arr.length, key) >= 0;
+    }
+}
---
+++ 2025-11/src/day_06/boj_1253/Main2.java
@@ -0,0 +1,51 @@
+package day_06.boj_1253;
+
+import java.io.*;
+import java.util.*;
+
+public class Main2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        int cnt = 0;
+        for (int i = 0; i < N; i++) {
+            int left = 0;
+            int right = N - 1;
+
+            while (left < right) {
+                // 왼쪽 포인터가 i면 넘어감
+                if (left == i) {
+                    left++;
+                    continue;
+                }
+
+                // 오른쪽 포인터가 i면 넘어감
+                if (right == i) {
+                    right--;
+                    continue;
+                }
+
+                if (arr[left] + arr[right] > arr[i]) {
+                    right--;
+                } else if (arr[left] + arr[right] < arr[i]) {
+                    left++;
+                } else {
+                    cnt++;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
