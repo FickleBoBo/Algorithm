e50a50aa1ca4e9b3630c597f10ab2c9901c3ab97
FickleBoBo <yukmw0704@gmail.com>
2025-11-07 00:31:40 +0900

    BOJ 1629 곱셈

---
+++ 2025-11/src/day_06/boj_1629/Main.java
@@ -0,0 +1,29 @@
+package day_06.boj_1629;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        System.out.println(modPow(A, B, C));
+    }
+
+    public static long modPow(long A, long B, long C) {
+        if (B == 0) return 1;
+
+        long half = modPow(A, B / 2, C);
+
+        if (B % 2 == 0) {
+            return half * half % C;
+        } else {
+            return half * half % C * A % C;  // half * half * A가 long형 범위를 넘어갈 수 있어서 미리 mod 수행
+        }
+    }
+}
