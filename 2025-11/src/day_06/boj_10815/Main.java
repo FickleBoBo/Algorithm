56f67875ecb0f0930fc95d304542dab31d47af5c
FickleBoBo <yukmw0704@gmail.com>
2025-11-06 20:44:55 +0900

    BOJ 10815 숫자 카드

---
+++ 2025-11/src/day_06/boj_10815/Main.java
@@ -0,0 +1,37 @@
+package day_06.boj_10815;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);  // 이분 탐색을 위해 정렬
+
+        br.readLine();  // M 입력값은 버림
+
+        st = new StringTokenizer(br.readLine());
+        while (st.hasMoreTokens()) {
+            // 이분 탐색 값이 0 이상이면 배열에 값이 존재하는 상황
+            if (Arrays.binarySearch(arr, Integer.parseInt(st.nextToken())) >= 0) {
+                sb.append("1 ");
+            } else {
+                sb.append("0 ");
+            }
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
