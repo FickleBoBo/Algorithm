69caa44a820d5958a1f614b56ce686b4ab9473a3
FickleBoBo <yukmw0704@gmail.com>
2025-11-15 01:16:54 +0900

    BOJ 2293 동전 1

---
+++ 2025-11/src/day_15/boj_2293/Main2.java
@@ -0,0 +1,29 @@
+package day_15.boj_2293;
+
+import java.io.*;
+import java.util.*;
+
+public class Main2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] coins = new int[n];
+        for (int i = 0; i < n; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
+        }
+
+        int[] dp = new int[1 + k];
+        dp[0] = 1;  // 어떤 동전도 사용하지 않았을 때 0원을 만드는 경우는 1가지
+        for (int coin : coins) {
+            for (int j = coin; j <= k; j++) {
+                dp[j] += dp[j - coin];
+            }
+        }
+
+        System.out.println(dp[k]);
+    }
+}
