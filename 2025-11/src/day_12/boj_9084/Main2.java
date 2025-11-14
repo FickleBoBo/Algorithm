f83aba614652b365c6f078679c1ef2a257e94cb2
FickleBoBo <yukmw0704@gmail.com>
2025-11-15 01:55:23 +0900

    BOJ 9084 동전

---
+++ 2025-11/src/day_12/boj_9084/Main2.java
@@ -0,0 +1,39 @@
+package day_12.boj_9084;
+
+import java.io.*;
+import java.util.*;
+
+public class Main2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= T; tc++) {
+            int N = Integer.parseInt(br.readLine());
+
+            int[] coins = new int[N];
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) {
+                coins[i] = Integer.parseInt(st.nextToken());
+            }
+
+            int M = Integer.parseInt(br.readLine());
+
+            int[] dp = new int[1 + M];
+            dp[0] = 1;  // 어떤 동전도 사용하지 않았을 때 0원을 만드는 경우는 1가지
+            for (int coin : coins) {
+                for (int j = coin; j <= M; j++) {
+                    dp[j] += dp[j - coin];
+                }
+            }
+
+            sb.append(dp[M]).append('\n');
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
