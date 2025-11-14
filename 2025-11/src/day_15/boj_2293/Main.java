053c3e136fce5e8582f78232ea666d4a7e4419b0
FickleBoBo <yukmw0704@gmail.com>
2025-11-15 01:32:43 +0900

    BOJ 2293 동전 1

--- 2025-11/src/day_15/boj_2293/Main.java
+++ 2025-11/src/day_15/boj_2293/Main.java
@@ -19,14 +19,11 @@ public class Main {
         int[][] dp = new int[1 + n][1 + k];
         dp[0][0] = 1;  // 어떤 동전도 사용하지 않았을 때 0원을 만드는 경우는 1가지
         for (int i = 1; i <= n; i++) {
+            System.arraycopy(dp[i - 1], 0, dp[i], 0, 1 + k);
+
             int coin = coins[i];
-
-            for (int j = 0; j <= k; j++) {
-                dp[i][j] = dp[i - 1][j];
-
-                if (j >= coin) {
-                    dp[i][j] += dp[i][j - coin];
-                }
+            for (int j = coin; j <= k; j++) {
+                dp[i][j] += dp[i][j - coin];
             }
         }
 
