2605d73089e57abcc185d0d8fe5c7fb42e8720a6
FickleBoBo <yukmw0704@gmail.com>
2025-11-16 10:10:18 +0900

    BOJ 2433 The Sound of Silence

---
+++ 2025-11/src/day_15/boj_2433/Main.java
@@ -0,0 +1,61 @@
+package day_15.boj_2433;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[1 + n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Deque<Integer> minDq = new ArrayDeque<>();  // 단조 증가 덱
+        Deque<Integer> maxDq = new ArrayDeque<>();  // 단조 감소 덱
+        for (int i = 1; i <= n; i++) {
+            // 구간에서 나가는 인덱스 처리
+            if (!minDq.isEmpty() && minDq.peekFirst() < (i - m + 1)) {
+                minDq.pollFirst();
+            }
+
+            // 구간으로 들어오는 인덱스 처리
+            while (!minDq.isEmpty() && arr[minDq.peekLast()] >= arr[i]) {
+                minDq.pollLast();
+            }
+            minDq.offerLast(i);
+
+            // 구간에서 나가는 인덱스 처리
+            if (!maxDq.isEmpty() && maxDq.peekFirst() < (i - m + 1)) {
+                maxDq.pollFirst();
+            }
+
+            // 구간으로 들어오는 인덱스 처리
+            while (!maxDq.isEmpty() && arr[maxDq.peekLast()] <= arr[i]) {
+                maxDq.pollLast();
+            }
+            maxDq.offerLast(i);
+
+            // 윈도우 크기가 확보됐을 때부터 비교
+            if ((i > m - 1) && (arr[maxDq.peekFirst()] - arr[minDq.peekFirst()] <= c)) {
+                sb.append(i - m + 1).append('\n');
+            }
+        }
+
+        if (sb.length() == 0) {
+            bw.write("NONE");
+        } else {
+            bw.write(sb.toString());
+        }
+        bw.flush();
+    }
+}
