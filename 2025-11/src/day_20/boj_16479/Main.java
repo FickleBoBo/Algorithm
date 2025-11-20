7e194cd2bf7b00589b101b3647c4b0294bcaeb8e
FickleBoBo <yukmw0704@gmail.com>
2025-11-20 23:47:38 +0900

    BOJ 16479 컵라면 측정하기

---
+++ 2025-11/src/day_20/boj_16479/Main.java
@@ -0,0 +1,19 @@
+package day_20.boj_16479;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int K = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
+        int D1 = Integer.parseInt(st.nextToken());
+        int D2 = Integer.parseInt(st.nextToken());
+
+        System.out.println(K * K - Math.pow((D1 - D2), 2) / 4);
+    }
+}
