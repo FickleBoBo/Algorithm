6935c2a195254523976db7c1cdab52de54736d26
FickleBoBo <yukmw0704@gmail.com>
2025-11-21 23:39:38 +0900

    BOJ 11659 구간 합 구하기 4

---
+++ 2025-11/src/day_21/boj_11659/Main.java
@@ -0,0 +1,38 @@
+package day_21.boj_11659;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[1 + N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] prefixSum = new int[1 + N];
+        for (int i = 1; i <= N; i++) {
+            prefixSum[i] = prefixSum[i - 1] + arr[i];
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            sb.append(prefixSum[end] - prefixSum[start - 1]).append('\n');
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
