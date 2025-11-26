d944e35ba4f09a6471d2ef673c9d4e1ecede0d30
FickleBoBo <yukmw0704@gmail.com>
2025-11-27 03:53:25 +0900

    BOJ 2565 전깃줄

---
+++ 2025-11/src/day_26/boj_2565/Main.java
@@ -0,0 +1,35 @@
+package day_26.boj_2565;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] map = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            map[i][0] = Integer.parseInt(st.nextToken());
+            map[i][1] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(map, (o1, o2) -> Integer.compare(o1[0], o2[0]));
+
+        int[] dp = new int[N];
+        Arrays.fill(dp, 1);
+        int max = 0;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                if (map[i][1] > map[j][1]) dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+
+            max = Math.max(max, dp[i]);
+        }
+
+        System.out.println(N - max);
+    }
+}
