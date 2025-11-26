da15638e145c8402f7c4f54c39867e3ba1015f31
FickleBoBo <yukmw0704@gmail.com>
2025-11-27 03:57:05 +0900

    BOJ 11722 가장 긴 감소하는 부분 수열

---
+++ 2025-11/src/day_26/boj_11722/Main.java
@@ -0,0 +1,33 @@
+package day_26.boj_11722;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[N];
+        Arrays.fill(dp, 1);  // 자기 자신만으로 LIS 가능
+        int max = 0;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] < arr[j]) dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+
+            max = Math.max(max, dp[i]);
+        }
+
+        System.out.println(max);
+    }
+}
