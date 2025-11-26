69bf763cc90ee764d2add5886c9c1a4585106c0b
FickleBoBo <yukmw0704@gmail.com>
2025-11-27 08:23:18 +0900

    BOJ 1929 소수 구하기

---
+++ 2025-11/src/day_27/boj_1929/Main.java
@@ -0,0 +1,41 @@
+package day_27.boj_1929;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        boolean[] isPrime = sieve(N);
+
+        for (int i = M; i <= N; i++) {
+            if (isPrime[i]) sb.append(i).append('\n');
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+    // 에라토스테네스의 체
+    private static boolean[] sieve(int N) {
+        boolean[] isPrime = new boolean[1 + N];
+        Arrays.fill(isPrime, 2, N + 1, true);
+
+        for (int i = 2; i * i <= N; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= N; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        return isPrime;
+    }
+}
