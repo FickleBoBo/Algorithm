a29921bc484008e2393d906cc07a4ceda7e687c9
FickleBoBo <yukmw0704@gmail.com>
2025-11-28 11:38:27 +0900

    BOJ 17087 숨바꼭질 6

---
+++ 2025-11/src/day_28/boj_17087/Main.java
@@ -0,0 +1,32 @@
+package day_28.boj_17087;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int gcd = Math.abs(S - arr[0]);
+        for (int i = 1; i < N; i++) {
+            gcd = gcd(gcd, Math.abs(S - arr[i]));
+        }
+
+        System.out.println(gcd);
+    }
+
+    private static int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+}
