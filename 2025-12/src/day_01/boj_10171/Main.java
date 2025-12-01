ebe0b7c61a4d21cefaf30d02254f577dd3e88db4
FickleBoBo <yukmw0704@gmail.com>
2025-12-01 23:06:44 +0900

    feat: 백준 10171 고양이

---
+++ 2025-12/src/day_01/boj_10171/Main.java
@@ -0,0 +1,10 @@
+package day_01.boj_10171;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
