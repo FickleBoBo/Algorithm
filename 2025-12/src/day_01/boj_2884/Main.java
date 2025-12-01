20cc9125380e2a0dce7d596dff25fd51a3929292
FickleBoBo <yukmw0704@gmail.com>
2025-12-01 23:06:10 +0900

    feat: 백준 2884 알람 시계

---
+++ 2025-12/src/day_01/boj_2884/Main.java
@@ -0,0 +1,24 @@
+package day_01.boj_2884;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int H = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        if (M >= 45) {
+            System.out.println(H + " " + (M - 45));
+        } else {
+            if (H > 0) {
+                System.out.println((H - 1) + " " + (M + 15));
+            } else {
+                System.out.println("23 " + (M + 15));
+            }
+        }
+    }
+}
