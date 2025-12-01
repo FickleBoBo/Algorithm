707cb510f574daeadba530145dfd5f333d475eb9
FickleBoBo <yukmw0704@gmail.com>
2025-12-01 23:02:29 +0900

    feat: 백준 1008 A/B

---
+++ 2025-12/src/day_01/boj_1008/Main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int A, B;
+    cin >> A >> B;
+    cout << fixed << setprecision(9) << (double)A / B;
+}
