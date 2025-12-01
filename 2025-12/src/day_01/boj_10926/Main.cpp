5bb6498f930fb7d3f8561746790f468a31d7e523
FickleBoBo <yukmw0704@gmail.com>
2025-12-01 23:07:55 +0900

    feat: 백준 10926 ??!

---
+++ 2025-12/src/day_01/boj_10926/Main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string id;
+    cin >> id;
+    cout << id << "??!";
+}
---
+++ 2025-12/src/day_01/boj_10926/Main.java
@@ -0,0 +1,11 @@
+package day_01.boj_10926;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(br.readLine() + "??!");
+    }
+}
