da00cc43e465b3b523151efa2bb92d5f2dc6a511
FickleBoBo <yukmw0704@gmail.com>
2025-12-01 23:08:17 +0900

    feat: 백준 11382 꼬마 정민

---
+++ 2025-12/src/day_01/boj_11382/Main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long long A, B, C;
+    cin >> A >> B >> C;
+    cout << A + B + C;
+}
