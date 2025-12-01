f05eb3c10b317ecd032946a3032769b03c270939
FickleBoBo <yukmw0704@gmail.com>
2025-12-01 23:08:54 +0900

    feat: 백준 18108 1998년생인 내가 태국에서는 2541년생?!

---
+++ 2025-12/src/day_01/boj_18108/Main.java
@@ -0,0 +1,12 @@
+package day_01.boj_18108;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int y = Integer.parseInt(br.readLine());
+        System.out.println(y - 543);
+    }
+}
