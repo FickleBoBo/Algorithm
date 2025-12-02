98fe96e5de22fcc3ddbbaebf93bafa63026268ce
FickleBoBo <yukmw0704@gmail.com>
2025-12-03 02:40:17 +0900

    feat: 백준 10811 바구니 뒤집기

---
+++ 2025-12/src/day_02/boj_10811/Main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N, M;
+    cin >> N >> M;
+
+    vector<int> v(1 + N);
+    iota(v.begin(), v.end(), 0);
+
+    for (int i = 0; i < M; i++) {
+        int s, e;
+        cin >> s >> e;
+
+        reverse(v.begin() + s, v.begin() + e + 1);
+    }
+
+    for (int i = 1; i <= N; i++) {
+        cout << v[i] << ' ';
+    }
+}
