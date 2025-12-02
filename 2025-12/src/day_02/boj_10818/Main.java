51dad3569e4d9d182c7bf10223392bfa08a3aa9e
FickleBoBo <yukmw0704@gmail.com>
2025-12-03 02:40:55 +0900

    feat: 백준 10818 최소, 최대

---
+++ 2025-12/src/day_02/boj_10818/Main.java
@@ -0,0 +1,28 @@
+package day_02.boj_10818;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int min = 1_000_000;
+        int max = -1_000_000;
+        for (int num : arr) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+
+        System.out.println(min + " " + max);
+    }
+}
