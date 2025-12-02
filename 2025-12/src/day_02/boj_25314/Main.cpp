9f5162c6419baf59f831167be63ca3e08c3bbd18
FickleBoBo <yukmw0704@gmail.com>
2025-12-03 02:37:08 +0900

    feat: 백준 25314 코딩은 체육과목 입니다

---
+++ 2025-12/src/day_02/boj_25314/Main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    for (int i = 0; i < N / 4; i++) {
+        cout << "long ";
+    }
+    cout << "int";
+}
