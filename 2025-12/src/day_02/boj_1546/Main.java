ed0d74feb3295eb6e1da8d7f28959331c0b00e82
FickleBoBo <yukmw0704@gmail.com>
2025-12-03 02:35:19 +0900

    feat: 백준 1546 평균

---
+++ 2025-12/src/day_02/boj_1546/Main.java
@@ -0,0 +1,27 @@
+package day_02.boj_1546;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        int max = 0;
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            max = Math.max(max, arr[i]);
+        }
+
+        double sum = 0;
+        for (int num : arr) {
+            sum += (double) num / max * 100;
+        }
+        System.out.println(sum / N);
+    }
+}
