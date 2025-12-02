e99c0a9a418dc7f7257f4f2be00ee2dda1aca7c7
FickleBoBo <yukmw0704@gmail.com>
2025-12-03 02:42:41 +0900

    feat: 백준 10952 A+B - 5

---
+++ 2025-12/src/day_02/boj_10952/Main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    while (true) {
+        int A, B;
+        cin >> A >> B;
+
+        if (A == 0) break;
+
+        cout << A + B << '\n';
+    }
+}
