991c3e7a0d31a7446fd6bb4fde7f7772e5e587ee
FickleBoBo <yukmw0704@gmail.com>
2025-12-03 02:39:58 +0900

    feat: 백준 10810 공 넣기

---
+++ 2025-12/src/day_02/boj_10810/Main.java
@@ -0,0 +1,33 @@
+package day_02.boj_10810;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int num = Integer.parseInt(st.nextToken());
+
+            Arrays.fill(arr, start - 1, end, num);
+        }
+
+        for (int num : arr) {
+            sb.append(num).append(" ");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
