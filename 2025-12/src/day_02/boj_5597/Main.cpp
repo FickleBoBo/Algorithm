801efdd95650b82ba56990e179f5354d5cafcc0c
FickleBoBo <yukmw0704@gmail.com>
2025-12-03 02:39:13 +0900

    feat: 백준 5597 과제 안 내신 분..?

---
+++ 2025-12/src/day_02/boj_5597/Main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    bool visited[1 + 30] = {};
+
+    for (int i = 0; i < 28; i++) {
+        int n;
+        cin >> n;
+        visited[n] = true;
+    }
+
+    for (int i = 1; i <= 30; i++) {
+        if (visited[i]) continue;
+
+        cout << i << '\n';
+    }
+}
