40fa730474de7af5f157779b0da6148e116fe38d
FickleBoBo <yukmw0704@gmail.com>
2025-12-04 05:45:38 +0900

    feat: 백준 10991 별 찍기 - 16

---
+++ 2025-12/src/day_03/boj_10991/Main.java
@@ -0,0 +1,31 @@
+package day_03.boj_10991;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N - i; j++) {
+                sb.append(" ");
+            }
+
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                if (j % 2 == 1) {
+                    sb.append("*");
+                } else {
+                    sb.append(" ");
+                }
+            }
+            sb.append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
