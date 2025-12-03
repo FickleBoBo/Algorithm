57552f8e52fd945f3ea6b7ba8e3ea659817ea8e0
FickleBoBo <yukmw0704@gmail.com>
2025-12-04 05:41:53 +0900

    feat: 백준 2438 별 찍기 - 1

---
+++ 2025-12/src/day_03/boj_2438/Main.java
@@ -0,0 +1,23 @@
+package day_03.boj_2438;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= i; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
