7d2e89cad140134231e7fa2b04281d67b9a55653
FickleBoBo <yukmw0704@gmail.com>
2025-12-04 05:47:02 +0900

    feat: 백준 1735 분수 합

---
+++ 2025-12/src/day_03/boj_1735/Main.java
@@ -0,0 +1,29 @@
+package day_03.boj_1735;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int C = Integer.parseInt(st.nextToken());
+        int D = Integer.parseInt(st.nextToken());
+
+        int numerator = A * D + B * C;  // 분자
+        int denominator = B * D;  // 분모
+        int gcd = gcd(numerator, denominator);
+        System.out.printf("%d %d", numerator / gcd, denominator / gcd);
+    }
+
+    static int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+}
