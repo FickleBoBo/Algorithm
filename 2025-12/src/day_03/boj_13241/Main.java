7768c18aa3a1c04632c37968d8b7b7258bd0ec41
FickleBoBo <yukmw0704@gmail.com>
2025-12-04 05:48:09 +0900

    feat: 백준 13241 최소공배수

---
+++ 2025-12/src/day_03/boj_13241/Main.java
@@ -0,0 +1,25 @@
+package day_03.boj_13241;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        System.out.println(lcm(A, B));
+    }
+
+    static int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+    static long lcm(int a, int b) {
+        return (long) a * b / gcd(a, b);
+    }
+}
