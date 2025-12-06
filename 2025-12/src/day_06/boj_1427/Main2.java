494083c4c8960f20d8e844ca6d85be7998d5cce1
FickleBoBo <yukmw0704@gmail.com>
2025-12-07 08:08:10 +0900

    feat: 백준 1427 소트인사이드

---
+++ 2025-12/src/day_06/boj_1427/Main2.java
@@ -0,0 +1,25 @@
+package day_06.boj_1427;
+
+import java.io.*;
+
+public class Main2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        String[] input = br.readLine().split("");
+        int[] cntArr = new int[10];
+
+        for (String str : input) {
+            cntArr[Integer.parseInt(str)]++;
+        }
+
+        for (int i = 9; i >= 0; i--) {
+            sb.append(String.valueOf(i).repeat(cntArr[i]));
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
