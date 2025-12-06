dae32e242b55e17fc019a3150be9a35766d2428a
FickleBoBo <yukmw0704@gmail.com>
2025-12-07 08:02:01 +0900

    feat: 백준 2485 가로수

--- 2025-12/src/day_03/boj_2485/Main.cpp
+++ 2025-12/src/day_03/boj_2485/Main.cpp
@@ -5,16 +5,16 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int N;
-    cin >> N;
+    int n;
+    cin >> n;
 
-    vector<int> v(N);
+    vector<int> v(n);
     for (auto& num : v) cin >> num;
 
     int g = gcd(v[1] - v[0], v[2] - v[1]);
-    for (int i = 3; i < N; i++) {
+    for (int i = 3; i < n; i++) {
         g = gcd(g, v[i] - v[i - 1]);
     }
 
-    cout << (v[N - 1] - v[0]) / g + 1 - N;
+    cout << (v[n - 1] - v[0]) / g + 1 - n;
 }
