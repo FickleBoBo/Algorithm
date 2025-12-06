754788ea3c01d83a9fd7eb0dbde0047ae7c824f2
FickleBoBo <yukmw0704@gmail.com>
2025-12-07 08:19:19 +0900

    feat: 백준 15894 수학은 체육과목 입니다

---
+++ 2025-12/src/day_06/boj_15894/Main.java
@@ -0,0 +1,12 @@
+package day_06.boj_15894;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(4L * n);
+    }
+}
