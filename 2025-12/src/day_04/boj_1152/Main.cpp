0cb0a7f456bac2da03f63db80d2d903983e34c02
FickleBoBo <yukmw0704@gmail.com>
2025-12-07 08:07:36 +0900

    feat: 백준 1152 단어의 개수

---
+++ 2025-12/src/day_04/boj_1152/Main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int cnt = 0;
+    string s;
+
+    while (cin >> s) {
+        cnt++;
+    }
+
+    cout << cnt;
+}
