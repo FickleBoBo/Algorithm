6a1cbc50b1cb001839db61b1a27f19f2ed2d7fac
FickleBoBo <yukmw0704@gmail.com>
2025-12-07 08:25:30 +0900

    feat: 백준 11720 숫자의 합

---
+++ 2025-12/src/day_04/boj_11720/Main.java
@@ -0,0 +1,19 @@
+package day_04.boj_11720;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        br.readLine();
+        String str = br.readLine();
+
+        int sum = 0;
+        for (char c : str.toCharArray()) {
+            sum += c - '0';
+        }
+
+        System.out.println(sum);
+    }
+}
