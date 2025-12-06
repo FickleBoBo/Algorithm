4e468b6dc77a75499d87c05338201d93886d8563
FickleBoBo <yukmw0704@gmail.com>
2025-12-07 08:22:01 +0900

    feat: 백준 2743 단어 길이 재기

---
+++ 2025-12/src/day_04/boj_2743/Main.java
@@ -0,0 +1,12 @@
+package day_04.boj_2743;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        System.out.println(str.length());
+    }
+}
