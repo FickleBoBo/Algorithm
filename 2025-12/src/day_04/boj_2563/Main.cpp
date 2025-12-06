066c3188a6be4b5c40390dc2ab447c3b0ed03848
FickleBoBo <yukmw0704@gmail.com>
2025-12-07 08:18:16 +0900

    feat: 백준 2563 색종이

---
+++ 2025-12/src/day_04/boj_2563/Main.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    bool visited[100][100] = {};
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        int row, col;
+        cin >> row >> col;
+
+        for (int r = row; r < row + 10; r++) {
+            for (int c = col; c < col + 10; c++) {
+                visited[r][c] = true;
+            }
+        }
+    }
+
+    int sum = 0;
+    for (int i = 0; i < 100; i++) {
+        for (int j = 0; j < 100; j++) {
+            if (visited[i][j]) sum++;
+        }
+    }
+
+    cout << sum;
+}
---
+++ 2025-12/src/day_04/boj_2563/Main.java
@@ -0,0 +1,35 @@
+package day_04.boj_2563;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        boolean[][] visited = new boolean[100][100];
+
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int row = Integer.parseInt(st.nextToken());
+            int col = Integer.parseInt(st.nextToken());
+
+            for (int r = row; r < row + 10; r++) {
+                for (int c = col; c < col + 10; c++) {
+                    visited[r][c] = true;
+                }
+            }
+        }
+
+        int sum = 0;
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                if (visited[i][j]) sum++;
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
