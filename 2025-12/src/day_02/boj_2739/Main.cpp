dd9eb5acb7621e46d05c9fa601fe07fa11e62b23
FickleBoBo <yukmw0704@gmail.com>
2025-12-07 07:56:46 +0900

    feat: 백준 2739 구구단

--- 2025-12/src/day_02/boj_2739/Main.cpp
+++ 2025-12/src/day_02/boj_2739/Main.cpp
@@ -5,10 +5,10 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int N;
-    cin >> N;
+    int n;
+    cin >> n;
 
     for (int i = 1; i <= 9; i++) {
-        cout << N << " * " << i << " = " << N * i << '\n';
+        cout << n << " * " << i << " = " << n * i << '\n';
     }
 }
