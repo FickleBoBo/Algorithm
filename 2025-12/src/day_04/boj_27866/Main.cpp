228f35f3726bc5ccd396d86a92dcb647293836be
FickleBoBo <yukmw0704@gmail.com>
2025-12-07 08:25:47 +0900

    feat: 백준 27866 문자와 문자열

---
+++ 2025-12/src/day_04/boj_27866/Main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string s;
+    int i;
+    cin >> s >> i;
+    cout << s[i - 1];
+}
