443f52fcb9db597f2929a561ee3b55296ce82746
FickleBoBo <yukmw0704@gmail.com>
2025-12-07 08:10:40 +0900

    feat: 백준 1978 소수 찾기

---
+++ 2025-12/src/day_06/boj_1978/Main2.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<bool> sieve(int n) {
+    vector<bool> isPrime(1 + n);
+    fill(isPrime.begin() + 2, isPrime.end(), true);
+
+    for (int i = 2; i * i <= n; i++) {
+        if (isPrime[i]) {
+            for (int j = i * i; j <= n; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+
+    return isPrime;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    vector<bool> isPrime = sieve(1000);
+    int cnt = 0;
+
+    for (int i = 0; i < n; i++) {
+        int n;
+        cin >> n;
+
+        if (isPrime[n]) cnt++;
+    }
+
+    cout << cnt;
+}
