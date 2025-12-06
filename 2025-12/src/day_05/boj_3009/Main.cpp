cbc734a151d4fef43de395f68a4ba7eaa0f051f1
FickleBoBo <yukmw0704@gmail.com>
2025-12-07 08:26:56 +0900

    feat: 백준 3009 네 번째 점

---
+++ 2025-12/src/day_05/boj_3009/Main.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int x1, y1, x2, y2, x3, y3;
+    cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+
+    int x4;
+    if (x1 == x2) {
+        x4 = x3;
+    } else if (x2 == x3) {
+        x4 = x1;
+    } else {
+        x4 = x2;
+    }
+
+    int y4;
+    if (y1 == y2) {
+        y4 = y3;
+    } else if (y2 == y3) {
+        y4 = y1;
+    } else {
+        y4 = y2;
+    }
+
+    cout << x4 << ' ' << y4;
+}
---
+++ 2025-12/src/day_05/boj_3009/Main2.java
@@ -0,0 +1,28 @@
+package day_05.boj_3009;
+
+import java.io.*;
+import java.util.*;
+
+public class Main2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int x1 = Integer.parseInt(st.nextToken());
+        int y1 = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int x2 = Integer.parseInt(st.nextToken());
+        int y2 = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int x3 = Integer.parseInt(st.nextToken());
+        int y3 = Integer.parseInt(st.nextToken());
+
+        int x4 = x1 ^ x2 ^ x3;
+        int y4 = y1 ^ y2 ^ y3;
+
+        System.out.println(x4 + " " + y4);
+    }
+}
