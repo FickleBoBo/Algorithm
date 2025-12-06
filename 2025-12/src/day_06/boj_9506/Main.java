839c453051b61b69052ddf0cf0be0a2b275074f5
FickleBoBo <yukmw0704@gmail.com>
2025-12-07 08:30:30 +0900

    feat: 백준 9506 약수들의 합

---
+++ 2025-12/src/day_06/boj_9506/Main.java
@@ -0,0 +1,36 @@
+package day_06.boj_9506;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+            int n = Integer.parseInt(br.readLine());
+
+            if (n == -1) break;
+
+            List<String> list = new ArrayList<>();
+            int sum = 0;
+            for (int i = 1; i < n; i++) {
+                if (n % i == 0) {
+                    list.add(String.valueOf(i));
+                    sum += i;
+                }
+            }
+
+            if (sum == n) {
+                sb.append(n).append(" = ").append(String.join(" + ", list)).append("\n");
+            } else {
+                sb.append(n).append(" is NOT perfect.\n");
+            }
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
