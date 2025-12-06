4a6fc88ee5dda36b8da567a615911bd65e33d68e
FickleBoBo <yukmw0704@gmail.com>
2025-12-07 08:08:10 +0900

    feat: 백준 1427 소트인사이드

---
+++ 2025-12/src/day_06/boj_1427/Main.java
@@ -0,0 +1,22 @@
+package day_06.boj_1427;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        char[] arr = br.readLine().toCharArray();
+        Arrays.sort(arr);
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            sb.append(arr[i]);
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
