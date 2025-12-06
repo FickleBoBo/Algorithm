e60dc0d2c4bdd3b59a4706b91aa1b1c0f2a75aed
FickleBoBo <yukmw0704@gmail.com>
2025-12-07 08:16:58 +0900

    feat: 백준 7977 크리스 마틴

---
+++ 2025-12/src/day_06/boj_7977/Main.java
@@ -0,0 +1,29 @@
+package day_06.boj_7977;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        char[] str = br.readLine().toCharArray();
+
+        int[] cntArr = new int[26];
+        for (char c : str) {
+            cntArr[c - 'A']++;
+        }
+
+        int min = Math.min(Math.min(cntArr[0], cntArr[2]), Math.min(cntArr[6], cntArr[19]));
+
+        System.out.println(min);
+        System.out.println(getDNA(N, cntArr, min));
+    }
+
+    static String getDNA(int N, int[] cntArr, int min) {
+        if (cntArr[0] == min) return "A".repeat(N);
+        if (cntArr[2] == min) return "C".repeat(N);
+        if (cntArr[6] == min) return "G".repeat(N);
+        return "T".repeat(N);
+    }
+}
