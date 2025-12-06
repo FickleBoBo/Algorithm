9b482efd7f5718bdf3afe5aeff53003b17bc4020
FickleBoBo <yukmw0704@gmail.com>
2025-12-07 08:23:08 +0900

    feat: 백준 9086 문자열

---
+++ 2025-12/src/day_04/boj_9086/Main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    for (int tc = 1; tc <= t; tc++) {
+        string s;
+        cin >> s;
+
+        cout << s.front() << s.back() << '\n';
+    }
+}
