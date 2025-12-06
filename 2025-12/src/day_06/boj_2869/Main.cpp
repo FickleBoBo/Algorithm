48d6bb12d34506415f766ce1d50503a0e3371090
FickleBoBo <yukmw0704@gmail.com>
2025-12-07 08:29:44 +0900

    feat: 백준 2869 달팽이는 올라가고 싶다

---
+++ 2025-12/src/day_06/boj_2869/Main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int a, b, v;
+    cin >> a >> b >> v;
+    cout << (v - b - 1) / (a - b) + 1;
+}
---
+++ 2025-12/src/day_06/boj_2869/Main.java
@@ -0,0 +1,17 @@
+package day_06.boj_2869;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+
+        System.out.println((V - B - 1) / (A - B) + 1);
+    }
+}
