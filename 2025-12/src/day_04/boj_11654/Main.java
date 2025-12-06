7e0f7b74cb0037f5b3114f8598a319285d006366
FickleBoBo <yukmw0704@gmail.com>
2025-12-07 08:25:00 +0900

    feat: 백준 11654 아스키 코드

---
+++ 2025-12/src/day_04/boj_11654/Main.java
@@ -0,0 +1,12 @@
+package day_04.boj_11654;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        char c = br.readLine().charAt(0);
+        System.out.println((int) c);
+    }
+}
