92c25987429e233d9e7bce101616d5796263cf1f
FickleBoBo <yukmw0704@gmail.com>
2025-12-07 08:21:35 +0900

    feat: 백준 2738 덧셈 행렬

---
+++ 2025-12/src/day_04/boj_2738/Main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> mat(n, vector<int>(m));
+    for (int k = 0; k < 2; k++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int num;
+                cin >> num;
+                mat[i][j] += num;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << mat[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+}
