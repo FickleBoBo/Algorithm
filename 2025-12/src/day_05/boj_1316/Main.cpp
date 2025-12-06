3e948e59fa510677ba61ce3be8b36855d1af2ba8
FickleBoBo <yukmw0704@gmail.com>
2025-12-07 08:26:26 +0900

    feat: 백준 1316 그룹 단어 체커

---
+++ 2025-12/src/day_05/boj_1316/Main.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    int cnt = 0;
+
+    for (int i = 0; i < n; i++) {
+        string s;
+        cin >> s;
+
+        bool visited[26] = {};
+        char prev = '\0';
+        bool flag = true;
+
+        for (char c : s) {
+            if (visited[c - 'a'] && c != prev) {
+                flag = false;
+                break;
+            }
+
+            visited[c - 'a'] = true;
+            prev = c;
+        }
+
+        if (flag) cnt++;
+    }
+
+    cout << cnt;
+}
---
+++ 2025-12/src/day_05/boj_1316/Main.java
@@ -0,0 +1,30 @@
+package day_05.boj_1316;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int cnt = 0;
+
+        out:
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            boolean[] visited = new boolean[26];
+            char prev = '\u0000';
+
+            for (char c : str.toCharArray()) {
+                if (visited[c - 'a'] && c != prev) continue out;
+
+                visited[c - 'a'] = true;
+                prev = c;
+            }
+
+            cnt++;
+        }
+
+        System.out.println(cnt);
+    }
+}
