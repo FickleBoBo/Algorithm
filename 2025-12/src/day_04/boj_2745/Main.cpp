9b741f4f2cf8fcd0ed77934070a7b3452a7d59d3
FickleBoBo <yukmw0704@gmail.com>
2025-12-07 08:22:16 +0900

    feat: 백준 2745 진법 변환

---
+++ 2025-12/src/day_04/boj_2745/Main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string n;
+    int b;
+    cin >> n >> b;
+    cout << stoi(n, nullptr, b);
+}
