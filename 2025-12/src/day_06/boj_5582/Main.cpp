5f5ebd10ce44bab58f2627cee3d2e49e74edfcb3
FickleBoBo <yukmw0704@gmail.com>
2025-12-07 08:15:51 +0900

    feat: 백준 5582 공통 부분 문자열

---
+++ 2025-12/src/day_06/boj_5582/Main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string s1, s2;
+    cin >> s1 >> s2;
+
+    int n = s1.size();
+    int m = s2.size();
+
+    vector<vector<int>> dp(1 + n, vector<int>(1 + m));
+    int mx = 0;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            if (s1[i - 1] == s2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+                mx = max(mx, dp[i][j]);
+            }
+        }
+    }
+
+    cout << mx;
+}
