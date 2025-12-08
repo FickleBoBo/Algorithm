43e693bb3a8b0d96a9c5ab47dcf0eadd8c872c4e
FickleBoBo <yukmw0704@gmail.com>
2025-12-08 19:26:54 +0900

    feat: 백준 11650 좌표 정렬하기

---
+++ 2025-12/src/day_08/boj_11650/Main.java
@@ -0,0 +1,33 @@
+package day_08.boj_11650;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr, (o1, o2) -> {
+            if (o1[0] == o2[0]) return Integer.compare(o1[1], o2[1]);
+            return Integer.compare(o1[0], o2[0]);
+        });
+
+        for (int[] pos : arr) {
+            sb.append(pos[0]).append(" ").append(pos[1]).append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
