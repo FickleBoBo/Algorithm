23c010d87c8bf75958c8a068271cc089963dad52
FickleBoBo <yukmw0704@gmail.com>
2025-12-08 19:24:02 +0900

    feat: 백준 11053 가장 긴 증가하는 부분 수열

---
+++ 2025-12/src/day_07/boj_11053/Main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+    for (auto& num : v) cin >> num;
+
+    vector<int> dp(n, 1);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            if (v[i] > v[j]) dp[i] = max(dp[i], dp[j] + 1);
+        }
+    }
+
+    cout << *max_element(dp.begin(), dp.end());
+}
