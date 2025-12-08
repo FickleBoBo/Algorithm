f0c6d444ca36af6c5c35a505ce81ef8464730695
FickleBoBo <yukmw0704@gmail.com>
2025-12-08 19:22:54 +0900

    feat: 백준 15965 K번째 소수

---
+++ 2025-12/src/day_08/boj_15965/Main.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+constexpr int MAX = 7368787;
+
+vector<bool> sieve(int n) {
+    vector<bool> isPrime(1 + n);
+    fill(isPrime.begin() + 2, isPrime.end(), true);
+
+    for (int i = 2; i * i <= n; i++) {
+        if (isPrime[i]) {
+            for (int j = i * i; j <= n; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+
+    return isPrime;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int k;
+    cin >> k;
+
+    vector<bool> isPrime = sieve(MAX);
+    int cnt = 0;
+
+    for (int i = 2; i <= MAX; i++) {
+        if (isPrime[i]) cnt++;
+
+        if (cnt == k) {
+            cout << i;
+            break;
+        }
+    }
+}
