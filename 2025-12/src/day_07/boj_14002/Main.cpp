2d7a5bad1caa798798f8ef7d2b470794d67f0ab5
FickleBoBo <yukmw0704@gmail.com>
2025-12-08 19:25:03 +0900

    feat: 백준 14002 가장 긴 증가하는 부분 수열 4

---
+++ 2025-12/src/day_07/boj_14002/Main.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> traceback(const vector<int>& v, const vector<int>& prev, int pos) {
+    vector<int> lis;
+
+    while (pos != -1) {
+        lis.push_back(v[pos]);
+        pos = prev[pos];
+    }
+
+    reverse(lis.begin(), lis.end());
+    return lis;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+    for (auto& num : v) cin >> num;
+
+    vector<int> dp(n, 1);
+    vector<int> prev(n, -1);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            if (v[i] > v[j] && dp[j] + 1 > dp[i]) {
+                dp[i] = dp[j] + 1;
+                prev[i] = j;
+            }
+        }
+    }
+
+    int mx = 0;
+    int pos = 0;
+    for (int i = 0; i < n; i++) {
+        if (dp[i] > mx) {
+            mx = dp[i];
+            pos = i;
+        }
+    }
+
+    cout << mx << '\n';
+
+    vector<int> lis = traceback(v, prev, pos);
+    for (auto num : lis) {
+        cout << num << ' ';
+    }
+}
---
+++ 2025-12/src/day_07/boj_14002/Main.java
@@ -0,0 +1,66 @@
+package day_07.boj_14002;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[N];
+        int[] prev = new int[N];
+        Arrays.fill(dp, 1);
+        Arrays.fill(prev, -1);
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j] && dp[j] + 1 > dp[i]) {
+                    dp[i] = dp[j] + 1;
+                    prev[i] = j;
+                }
+            }
+        }
+
+        int max = 0;
+        int pos = 0;
+        for (int i = 0; i < N; i++) {
+            if (dp[i] > max) {
+                max = dp[i];
+                pos = i;
+            }
+        }
+
+        sb.append(max).append("\n");
+        sb.append(traceback(arr, prev, pos));
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+    static String traceback(int[] arr, int[] prev, int pos) {
+        StringBuilder sb = new StringBuilder();
+
+        Deque<Integer> stack = new ArrayDeque<>();
+        while (pos != -1) {
+            stack.push(arr[pos]);
+            pos = prev[pos];
+        }
+
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop()).append(" ");
+        }
+
+        return sb.toString();
+    }
+}
