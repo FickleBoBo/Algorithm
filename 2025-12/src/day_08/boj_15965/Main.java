88a8b80e392f0487990dc25ccd5bff1e3f8a6df3
FickleBoBo <yukmw0704@gmail.com>
2025-12-08 19:22:54 +0900

    feat: 백준 15965 K번째 소수

---
+++ 2025-12/src/day_08/boj_15965/Main.java
@@ -0,0 +1,41 @@
+package day_08.boj_15965;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static final int MAX = 7_368_787;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int K = Integer.parseInt(br.readLine());
+        boolean[] isPrime = sieve(MAX);
+        int cnt = 0;
+
+        for (int i = 2; i <= MAX; i++) {
+            if (isPrime[i]) cnt++;
+
+            if (cnt == K) {
+                System.out.println(i);
+                break;
+            }
+        }
+    }
+
+    static boolean[] sieve(int N) {
+        boolean[] isPrime = new boolean[1 + N];
+        Arrays.fill(isPrime, 2, N + 1, true);
+
+        for (int i = 2; i * i <= N; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= N; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        return isPrime;
+    }
+}
