7c5006a35741902345afbb808cc4b3e34991184c
FickleBoBo <yukmw0704@gmail.com>
2025-12-08 19:23:38 +0900

    feat: 백준 2960 에라토스테네스의 체

---
+++ 2025-12/src/day_08/boj_2960/Main.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int sieve(int n, int k) {
+    vector<bool> isPrime(1 + n);
+    fill(isPrime.begin() + 2, isPrime.end(), true);
+    int cnt = 0;
+
+    for (int i = 2; i <= n; i++) {
+        if (isPrime[i]) {
+            cnt++;
+            if (cnt == k) return i;
+
+            for (int j = i * i; j <= n; j += i) {
+                if (isPrime[j]) {
+                    isPrime[j] = false;
+
+                    cnt++;
+                    if (cnt == k) return j;
+                }
+            }
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, k;
+    cin >> n >> k;
+    cout << sieve(n, k);
+}
