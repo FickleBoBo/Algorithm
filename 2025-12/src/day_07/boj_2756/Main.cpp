1824af16fb63f356f842382b433b85679770e5c6
FickleBoBo <yukmw0704@gmail.com>
2025-12-08 19:20:31 +0900

    feat: 백준 2756 다트

---
+++ 2025-12/src/day_07/boj_2756/Main.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int point(double dist) {
+    if (dist <= 3) return 100;
+    if (dist <= 6) return 80;
+    if (dist <= 9) return 60;
+    if (dist <= 12) return 40;
+    if (dist <= 15) return 20;
+    return 0;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    for (int tc = 1; tc <= t; tc++) {
+        int p1 = 0;
+        int p2 = 0;
+
+        double x, y;
+        for (int i = 0; i < 3; i++) {
+            cin >> x >> y;
+            p1 += point(sqrt(x * x + y * y));
+        }
+        for (int i = 0; i < 3; i++) {
+            cin >> x >> y;
+            p2 += point(sqrt(x * x + y * y));
+        }
+
+        if (p1 > p2) {
+            cout << "SCORE: " << p1 << " to " << p2 << ", PLAYER 1 WINS.\n";
+        } else if (p1 < p2) {
+            cout << "SCORE: " << p1 << " to " << p2 << ", PLAYER 2 WINS.\n";
+        } else {
+            cout << "SCORE: " << p1 << " to " << p2 << ", TIE.\n";
+        }
+    }
+}
