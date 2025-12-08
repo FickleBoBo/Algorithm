6ae4c9c6e59644bae4ec169415ad6b88a0c61f20
FickleBoBo <yukmw0704@gmail.com>
2025-12-08 19:22:18 +0900

    feat: 백준 11660 구간 합 구하기 5

---
+++ 2025-12/src/day_08/boj_11660/Main.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> map(n, vector<int>(n));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> map[i][j];
+        }
+    }
+
+    vector<vector<int>> psum(1 + n, vector<int>(1 + n));
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            psum[i][j] = psum[i - 1][j] + psum[i][j - 1] - psum[i - 1][j - 1] + map[i - 1][j - 1];
+        }
+    }
+
+    for (int i = 0; i < m; i++) {
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+        cout << psum[x2][y2] - psum[x2][y1 - 1] - psum[x1 - 1][y2] + psum[x1 - 1][y1 - 1] << '\n';
+    }
+}
