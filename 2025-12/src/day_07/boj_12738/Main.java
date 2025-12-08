b0b8e68c183c973aca6c45eace0a3d02cf5ceda0
FickleBoBo <yukmw0704@gmail.com>
2025-12-08 19:24:46 +0900

    feat: 백준 12738 가장 긴 증가하는 부분 수열 3

---
+++ 2025-12/src/day_07/boj_12738/Main.java
@@ -0,0 +1,49 @@
+package day_07.boj_12738;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        List<Integer> dp = new ArrayList<>();
+        for (int num : arr) {
+            int idx = lowerBound(dp, num);
+
+            if (idx == dp.size()) {
+                dp.add(num);
+            } else {
+                dp.set(idx, num);
+            }
+        }
+
+        System.out.println(dp.size());
+    }
+
+    static int lowerBound(List<Integer> list, int key) {
+        int left = 0;
+        int right = list.size();
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+
+            if (list.get(mid) < key) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return right;
+    }
+}
