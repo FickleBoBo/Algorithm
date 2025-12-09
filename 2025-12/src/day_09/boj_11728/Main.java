9a1f60af930705bb2b5024c5451716ddd1276d8c
FickleBoBo <yukmw0704@gmail.com>
2025-12-09 18:59:27 +0900

    feat: 백준 11728 배열 합치기

---
+++ 2025-12/src/day_09/boj_11728/Main.java
@@ -0,0 +1,34 @@
+package day_09.boj_11728;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N + M];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = N; i < N + M; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        for (int n : arr) {
+            sb.append(n).append(" ");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
