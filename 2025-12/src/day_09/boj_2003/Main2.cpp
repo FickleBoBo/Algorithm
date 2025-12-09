9bb6e823acd792852d0af15a24b7f89d1c1fddb8
FickleBoBo <yukmw0704@gmail.com>
2025-12-09 18:57:24 +0900

    feat: 백준 2003 수들의 합 2

---
+++ 2025-12/src/day_09/boj_2003/Main2.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> v(n);
+    for (auto& num : v) cin >> num;
+
+    int l = 0;
+    int r = 0;
+    int sum = 0;
+    int cnt = 0;
+    while (true) {
+        if (sum < m) {
+            sum += v[r++];
+        } else {
+            if (sum == m) cnt++;
+
+            sum -= v[l++];
+        }
+
+        if (r == n && sum < m) break;
+    }
+
+    cout << cnt;
+}
