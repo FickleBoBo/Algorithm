74c48324d0c73486bcf5bc4ca72b7485a8d89cf0
FickleBoBo <yukmw0704@gmail.com>
2025-12-09 18:49:41 +0900

    feat: 백준 27436 벌집 2

---
+++ 2025-12/src/day_09/boj_27436/Main.java
@@ -0,0 +1,30 @@
+package day_09.boj_27436;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        long N = Long.parseLong(br.readLine());
+
+        System.out.println(lowerBound(N));
+    }
+
+    static long lowerBound(long key) {
+        long left = 1;
+        long right = 2_000_000_000;
+
+        while (left < right) {
+            long mid = (left + right) / 2;
+
+            if ((3 * mid * (mid - 1) + 1) < key) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return right;
+    }
+}
