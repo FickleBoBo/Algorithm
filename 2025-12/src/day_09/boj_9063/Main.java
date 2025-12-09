3eea031b3c8b5ea67fea1d5bff666245aebacf9c
FickleBoBo <yukmw0704@gmail.com>
2025-12-09 18:52:04 +0900

    feat: 백준 9063 대지

---
+++ 2025-12/src/day_09/boj_9063/Main.java
@@ -0,0 +1,30 @@
+package day_09.boj_9063;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int minX = 10000;
+        int minY = 10000;
+        int maxX = -10000;
+        int maxY = -10000;
+
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            minX = Math.min(minX, x);
+            minY = Math.min(minY, y);
+            maxX = Math.max(maxX, x);
+            maxY = Math.max(maxY, y);
+        }
+
+        System.out.println((maxX - minX) * (maxY - minY));
+    }
+}
