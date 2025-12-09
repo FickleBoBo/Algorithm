cd56cbeb746bcfc01e1a576a4c53846025daccf4
FickleBoBo <yukmw0704@gmail.com>
2025-12-09 18:50:05 +0900

    feat: 백준 21921 블로그

---
+++ 2025-12/src/day_09/boj_21921/Main.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, x;
+    cin >> n >> x;
+
+    vector<int> v(n);
+    for (auto& num : v) cin >> num;
+
+    vector<int> psum(1 + n);
+    for (int i = 1; i <= n; i++) {
+        psum[i] = psum[i - 1] + v[i - 1];
+    }
+
+    int mx = 0;
+    int cnt = 0;
+    for (int i = 0; i <= n - x; i++) {
+        int diff = psum[i + x] - psum[i];
+
+        if (diff > mx) {
+            mx = diff;
+            cnt = 1;
+        } else if (diff == mx) {
+            cnt++;
+        }
+    }
+
+    if (mx == 0) {
+        cout << "SAD";
+    } else {
+        cout << mx << '\n';
+        cout << cnt << '\n';
+    }
+}
---
+++ 2025-12/src/day_09/boj_21921/Main2.java
@@ -0,0 +1,48 @@
+package day_09.boj_21921;
+
+import java.io.*;
+import java.util.*;
+
+public class Main2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // 초기 윈도우의 방문자 수 합 계산
+        int sum = 0;
+        for (int i = 0; i < X; i++) {
+            sum += arr[i];
+        }
+
+        // 윈도우를 이동하며 최대 방문자수 계산
+        int max = sum;
+        int cnt = 1;
+        for (int i = 0; i < N - X; i++) {
+            sum = sum - arr[i] + arr[i + X];
+
+            // 이동한 구간의 최대 방문자 수가 더 많으면 최댓값과 개수를 갱신하고 동일하면 개수만 갱신
+            if (sum > max) {
+                max = sum;
+                cnt = 1;
+            } else if (sum == max) {
+                cnt++;
+            }
+        }
+
+        if (max == 0) {
+            System.out.println("SAD");
+        } else {
+            System.out.println(max);
+            System.out.println(cnt);
+        }
+    }
+}
