6cd218fe1a71c8c445fab4c8249a0cc0d65490d3
FickleBoBo <yukmw0704@gmail.com>
2025-12-09 18:50:05 +0900

    feat: 백준 21921 블로그

---
+++ 2025-12/src/day_09/boj_21921/Main2.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, x;
+    cin >> n >> x;
+
+    vector<int> v(n);
+    for (auto& num : v) cin >> num;
+
+    // 초기 윈도우의 방문자 수 합 계산
+    int sum = 0;
+    for (int i = 0; i < x; i++) {
+        sum += v[i];
+    }
+
+    // 윈도우를 이동하며 최대 방문자수 계산
+    int mx = sum;
+    int cnt = 1;
+    for (int i = 0; i < n - x; i++) {
+        sum = sum - v[i] + v[i + x];
+
+        // 이동한 구간의 최대 방문자 수가 더 많으면 최댓값과 개수를 갱신하고 동일하면 개수만 갱신
+        if (sum > mx) {
+            mx = sum;
+            cnt = 1;
+        } else if (sum == mx) {
+            cnt++;
+        }
+    }
+
+    if (mx == 0) {
+        cout << "SAD";
+    } else {
+        cout << mx << '\n';
+        cout << cnt << '\n';
+    }
+}
