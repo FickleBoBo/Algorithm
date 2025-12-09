445b2f136f3932fe5bb7e9f5866a5b72123d1951
FickleBoBo <yukmw0704@gmail.com>
2025-12-09 18:59:27 +0900

    feat: 백준 11728 배열 합치기

---
+++ 2025-12/src/day_09/boj_11728/Main2.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> v1(n);
+    for (auto& num : v1) cin >> num;
+    sort(v1.begin(), v1.end());
+
+    vector<int> v2(m);
+    for (auto& num : v2) cin >> num;
+    sort(v2.begin(), v2.end());
+
+    vector<int> ans;
+    int p1 = 0;  // v1 포인터
+    int p2 = 0;  // v2 포인터
+
+    while (p1 < n && p2 < m) {
+        if (v1[p1] <= v2[p2]) {
+            ans.push_back(v1[p1++]);
+        } else {
+            ans.push_back(v2[p2++]);
+        }
+    }
+
+    ans.insert(ans.end(), v1.begin() + p1, v1.end());  // 남은 수 처리
+    ans.insert(ans.end(), v2.begin() + p2, v2.end());  // 남은 수 처리
+
+    for (auto num : ans) {
+        cout << num << ' ';
+    }
+}
