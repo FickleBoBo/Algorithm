671d633d5f06be02488c1bd434158d98ae24b278
FickleBoBo <yukmw0704@gmail.com>
2025-12-09 18:53:47 +0900

    feat: 백준 10821 정수의 개수

---
+++ 2025-12/src/day_09/boj_10821/Main.java
@@ -0,0 +1,12 @@
+package day_09.boj_10821;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] line = br.readLine().split(",");
+        System.out.println(line.length);
+    }
+}
