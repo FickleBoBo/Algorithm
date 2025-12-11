845556951090effd6cd92e3889adcf4392a71633
FickleBoBo <yukmw0704@gmail.com>
2025-12-12 01:39:52 +0900

    feat: 백준 1920 수 찾기

---
+++ 2025-12/src/day_10/boj_1920/Main.java
@@ -0,0 +1,35 @@
+package day_10.boj_1920;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        br.readLine();
+        st = new StringTokenizer(br.readLine());
+        while (st.hasMoreTokens()) {
+            if (Arrays.binarySearch(arr, Integer.parseInt(st.nextToken())) >= 0) {
+                sb.append("1\n");
+            } else {
+                sb.append("0\n");
+            }
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
