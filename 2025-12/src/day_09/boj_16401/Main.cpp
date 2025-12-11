81259cdffd72bb88e693aad446bc71811a94e39c
FickleBoBo <yukmw0704@gmail.com>
2025-12-12 02:07:16 +0900

    feat: 백준 16401 과자 나눠주기

--- 2025-12/src/day_09/boj_16401/Main.cpp
+++ 2025-12/src/day_09/boj_16401/Main.cpp
@@ -1,20 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int upper_bound_param(const vector<int>& v, int m) {
-    int left = 0;
+int upper_bound_param(const vector<int>& v, int key) {
+    int left = 1;
     int right = 1000000001;  // 과자의 길이가 최대 10^9까지라서 right의 상한을 10^9+1로 설정
 
     while (left < right) {
         int mid = (left + right) / 2;
-        if (mid == 0) return 1;  // mid가 0이면 과자를 나누어줄 수 없는 경우로 1 반환
 
         int cnt = 0;
         for (int n : v) {
             cnt += n / mid;
         }
 
-        if (cnt >= m) {
+        if (cnt >= key) {
             left = mid + 1;
         } else {
             right = mid;
@@ -32,7 +31,7 @@ int main() {
     cin >> m >> n;
 
     vector<int> v(n);
-    for (auto& num : v) cin >> num;
+    for (int& x : v) cin >> x;
 
     cout << upper_bound_param(v, m) - 1;
 }
--- 2025-12/src/day_09/boj_16401/Main.java
+++ 2025-12/src/day_09/boj_16401/Main.java
@@ -20,20 +20,19 @@ public class Main {
         System.out.println(upperBound(arr, M) - 1);
     }
 
-    static int upperBound(int[] arr, int M) {
-        int left = 0;
+    static int upperBound(int[] arr, int key) {
+        int left = 1;
         int right = 1_000_000_001;  // 과자의 길이가 최대 10^9까지라서 right의 상한을 10^9+1로 설정
 
         while (left < right) {
             int mid = (left + right) / 2;
-            if (mid == 0) return 1;  // mid가 0이면 과자를 나누어줄 수 없는 경우로 1 반환
 
             int cnt = 0;
             for (int n : arr) {
                 cnt += n / mid;
             }
 
-            if (cnt >= M) {
+            if (cnt >= key) {
                 left = mid + 1;
             } else {
                 right = mid;
