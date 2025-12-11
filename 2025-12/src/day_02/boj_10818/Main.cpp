51632165c2b7949ce52002fb7da9761a1993df43
FickleBoBo <yukmw0704@gmail.com>
2025-12-12 01:47:40 +0900

    feat: 백준 10818 최소, 최대

--- 2025-12/src/day_02/boj_10818/Main.cpp
+++ 2025-12/src/day_02/boj_10818/Main.cpp
@@ -9,7 +9,7 @@ int main() {
     cin >> n;
 
     vector<int> v(n);
-    for (auto& num : v) cin >> num;
+    for (int& x : v) cin >> x;
 
     auto [mn_it, mx_it] = minmax_element(v.begin(), v.end());
     cout << *mn_it << ' ' << *mx_it;
