b834d78433a362c6eb20a1f355e50ef09f14d3d8
FickleBoBo <yukmw0704@gmail.com>
2025-12-12 22:46:25 +0900

    feat: 백준 1106 호텔

---
+++ 2025-12/src/day_12/boj_1106/Main.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+constexpr int MAX = 100001;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int c, n;
+    cin >> c >> n;
+
+    vector<pair<int, int>> a(n);
+    for (auto& p : a) cin >> p.first >> p.second;
+
+    vector<vector<int>> dp(1 + n, vector<int>(1 + c + 100));
+    fill(dp[0].begin() + 1, dp[0].end(), MAX);
+    for (int i = 1; i <= n; i++) {
+        int cost = a[i - 1].first;
+        int person = a[i - 1].second;
+
+        for (int j = 1; j <= c + 100; j++) {
+            if (j < person) {
+                dp[i][j] = dp[i - 1][j];
+            } else {
+                dp[i][j] = min(dp[i][j - person] + cost, dp[i - 1][j]);
+            }
+        }
+    }
+
+    cout << *min_element(dp[n].begin() + c, dp[n].end());
+}
---
+++ 2025-12/src/day_12/boj_1106/Main2.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+constexpr int MAX = 100001;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int c, n;
+    cin >> c >> n;
+
+    vector<pair<int, int>> a(n);
+    for (auto& p : a) cin >> p.first >> p.second;
+
+    vector<int> dp(1 + c + 100);
+    fill(dp.begin() + 1, dp.end(), MAX);
+    for (int i = 0; i < n; i++) {
+        int cost = a[i].first;
+        int person = a[i].second;
+
+        for (int j = person; j <= c + 100; j++) {
+            dp[j] = min(dp[j - person] + cost, dp[j]);
+        }
+    }
+
+    cout << *min_element(dp.begin() + c, dp.end());
+}
