f4ecd960a2966567dc23e38315a40869ee713baa
FickleBoBo <yukmw0704@gmail.com>
2025-12-12 22:50:16 +0900

    feat: 프로그래머스 42748 K번째수

---
+++ 2025-12/src/day_12/prms_42748/Solution.java
@@ -0,0 +1,27 @@
+package day_12.prms_42748;
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for (int i = 0; i < commands.length; i++) {
+            int[] command = commands[i];
+            int start = command[0] - 1;  // array에서 구간 시작 인덱스(포함)
+            int end = command[1];  // array에서 구간 끝 인덱스(미포함)
+            int k = command[2] - 1;
+            int len = end - start;  // 구간의 길이
+
+            int[] tmp = new int[len];
+
+            // 임시 배열에 고속 복사
+            System.arraycopy(array, start, tmp, 0, len);
+            Arrays.sort(tmp);
+
+            answer[i] = tmp[k];
+        }
+
+        return answer;
+    }
+}
