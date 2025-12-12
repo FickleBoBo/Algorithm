19d1bd0b903c576293c4aaeb407d8e5af8f690a6
FickleBoBo <yukmw0704@gmail.com>
2025-12-12 22:46:25 +0900

    feat: 백준 1106 호텔

---
+++ 2025-12/src/day_12/boj_1106/Main2.java
@@ -0,0 +1,43 @@
+package day_12.boj_1106;
+
+import java.io.*;
+import java.util.*;
+
+public class Main2 {
+
+    static final int MAX = 100_001;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int C = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        int[] costs = new int[N];
+        int[] people = new int[N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            costs[i] = Integer.parseInt(st.nextToken());
+            people[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[1 + C + 100];
+        Arrays.fill(dp, 1, dp.length, MAX);
+        for (int i = 0; i < N; i++) {
+            int cost = costs[i];
+            int person = people[i];
+
+            for (int j = person; j <= C + 100; j++) {
+                dp[j] = Math.min(dp[j - person] + cost, dp[j]);
+            }
+        }
+
+        int min = MAX;
+        for (int j = C; j <= C + 100; j++) {
+            min = Math.min(min, dp[j]);
+        }
+
+        System.out.println(min);
+    }
+}
