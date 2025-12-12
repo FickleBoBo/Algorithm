0b643ac73586aa12e42934e00cbe1772c97cbbbc
FickleBoBo <yukmw0704@gmail.com>
2025-12-12 22:45:21 +0900

    feat: 백준 1629 곱셈

---
+++ 2025-12/src/day_08/boj_1629/Main2.java
@@ -0,0 +1,28 @@
+package day_08.boj_1629;
+
+import java.io.*;
+import java.util.*;
+
+public class Main2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        System.out.println(modPow(A, B, C));
+    }
+
+    static long modPow(long A, long B, long C) {
+        if (B == 0) return 1;
+
+        long half = modPow(A, B / 2, C);
+        if (B % 2 == 1) {
+            return half * half % C * A % C;
+        } else {
+            return half * half % C;
+        }
+    }
+}
