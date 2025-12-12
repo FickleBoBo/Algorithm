24e36603cd1a1ff55718679bcf482cde5484fe7e
FickleBoBo <yukmw0704@gmail.com>
2025-12-12 22:48:04 +0900

    feat: 백준 2629 양팔저울

---
+++ 2025-12/src/day_12/boj_2629/Main.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+constexpr int MAX = 100001;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    vector<int> wa(n);
+    for (int& x : wa) cin >> x;
+
+    int sum = 0;
+    for (int x : wa) sum += x;
+
+    int m;
+    cin >> m;
+
+    vector<int> ba(m);
+    for (int& x : ba) cin >> x;
+
+    vector<vector<bool>> dp(1 + n, vector<bool>(1 + sum));
+    dp[0][0] = true;  // 추가 주어지지 않아도 구슬의 무게가 0이면 측정 가능
+    for (int i = 1; i <= n; i++) {
+        int w = wa[i - 1];
+
+        for (int j = 0; j <= sum; j++) {
+            if (!dp[i - 1][j]) continue;
+
+            dp[i][j] = true;           // 현재 추를 사용하지 않는 경우
+            dp[i][j + w] = true;       // 현재 추를 사용하는 경우
+            dp[i][abs(j - w)] = true;  // 현재 추를 사용하는 경우
+        }
+    }
+
+    for (int b : ba) {
+        // 구슬의 무게가 모든 추의 무게보다 크거나 추의 조합으로 측정 불가능하면 N
+        if (b > sum || !dp[n][b]) {
+            cout << "N ";
+        } else {
+            cout << "Y ";
+        }
+    }
+}
---
+++ 2025-12/src/day_12/boj_2629/Main.java
@@ -0,0 +1,56 @@
+package day_12.boj_2629;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] W = new int[1 + N];
+        int sum = 0;
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            sum += W[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int M = Integer.parseInt(br.readLine());
+
+        int[] beads = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            beads[i] = Integer.parseInt(st.nextToken());
+        }
+
+        boolean[][] dp = new boolean[1 + N][1 + sum];
+        dp[0][0] = true;  // 추가 주어지지 않아도 구슬의 무게가 0이면 측정 가능
+        for (int i = 1; i <= N; i++) {
+            int w = W[i];
+
+            for (int j = 0; j <= sum; j++) {
+                if (!dp[i - 1][j]) continue;
+
+                dp[i][j] = true;  // 현재 추를 사용하지 않는 경우
+                dp[i][j + w] = true;  // 현재 추를 사용하는 경우
+                dp[i][Math.abs(j - w)] = true;  // 현재 추를 사용하는 경우
+            }
+        }
+
+        for (int bead : beads) {
+            // 구슬의 무게가 모든 추의 무게보다 크거나 추의 조합으로 측정 불가능하면 N
+            if (bead > sum || !dp[N][bead]) {
+                sb.append("N ");
+            } else {
+                sb.append("Y ");
+            }
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
