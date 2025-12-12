3f9620b5289502d66357655128c002b5b2f3672b
FickleBoBo <yukmw0704@gmail.com>
2025-12-12 22:46:07 +0900

    feat: 백준 7579 앱

---
+++ 2025-12/src/day_12/boj_7579/Main.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> ma(n);
+    for (int& x : ma) cin >> x;
+
+    vector<int> ca(n);
+    for (int& x : ca) cin >> x;
+
+    int sum = 0;
+    for (int x : ca) sum += x;
+
+    vector<vector<int>> dp(1 + n, vector<int>(1 + sum));
+    for (int i = 1; i <= n; i++) {
+        int mi = ma[i - 1];
+        int ci = ca[i - 1];
+
+        for (int j = 0; j <= sum; j++) {
+            if (j < ci) {
+                dp[i][j] = dp[i - 1][j];
+            } else {
+                dp[i][j] = max(dp[i - 1][j - ci] + mi, dp[i - 1][j]);
+            }
+        }
+    }
+
+    for (int j = 0; j <= sum; j++) {
+        if (dp[n][j] >= m) {
+            cout << j;
+            return 0;
+        }
+    }
+}
---
+++ 2025-12/src/day_12/boj_7579/Main2.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> ma(n);
+    for (int& x : ma) cin >> x;
+
+    vector<int> ca(n);
+    for (int& x : ca) cin >> x;
+
+    int sum = 0;
+    for (int x : ca) sum += x;
+
+    vector<int> dp(1 + sum);
+    for (int i = 0; i < n; i++) {
+        int mi = ma[i];
+        int ci = ca[i];
+
+        for (int j = sum; j >= ci; j--) {
+            dp[j] = max(dp[j - ci] + mi, dp[j]);
+        }
+    }
+
+    for (int j = 0; j <= sum; j++) {
+        if (dp[j] >= m) {
+            cout << j;
+            return 0;
+        }
+    }
+}
