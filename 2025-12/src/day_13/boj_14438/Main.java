17b4109b58d27f3af46c366808499524da26f279
FickleBoBo <yukmw0704@gmail.com>
2025-12-17 03:25:19 +0900

    feat: 백준 14438 수열과 쿼리 17

--- 2025-12/src/day_13/boj_14438/Main.java
+++ 2025-12/src/day_13/boj_14438/Main.java
@@ -68,7 +68,6 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
@@ -96,7 +95,6 @@ public class Main {
             }
         }
 
-        bw.write(sb.toString());
-        bw.flush();
+        System.out.println(sb);
     }
 }
