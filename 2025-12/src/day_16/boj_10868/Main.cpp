78f09d6daae97d4ae9f3afb707439d99c50cc260
FickleBoBo <yukmw0704@gmail.com>
2025-12-17 03:07:02 +0900

    feat: 백준 10868 최솟값

---
+++ 2025-12/src/day_16/boj_10868/Main.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+constexpr int MAX = 1000000001;
+
+struct SegTree {
+    int n;
+    vector<int> tree;
+
+    SegTree(int n) : n(n), tree(4 * n) {
+    }
+
+    void init(const vector<int>& arr, int node, int start, int end) {
+        if (start == end) {
+            tree[node] = arr[start];
+            return;
+        }
+
+        int mid = (start + end) / 2;
+
+        init(arr, node * 2, start, mid);
+        init(arr, node * 2 + 1, mid + 1, end);
+        tree[node] = min(tree[node * 2], tree[node * 2 + 1]);
+    }
+
+    int queryMin(int node, int start, int end, int left, int right) {
+        if (left > end || right < start) return MAX;
+        if (left <= start && end <= right) return tree[node];
+
+        int mid = (start + end) / 2;
+
+        int leftMin = queryMin(node * 2, start, mid, left, right);
+        int rightMin = queryMin(node * 2 + 1, mid + 1, end, left, right);
+        return min(leftMin, rightMin);
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> v(n);
+    for (int& x : v) cin >> x;
+
+    SegTree tree(n);
+    tree.init(v, 1, 0, n - 1);
+
+    for (int i = 0; i < m; i++) {
+        int a, b;
+        cin >> a >> b;
+        cout << tree.queryMin(1, 0, n - 1, a - 1, b - 1) << '\n';
+    }
+}
