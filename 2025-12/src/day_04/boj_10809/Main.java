ce5b65463877f90107fea0ff6fa3f3aa89505871
FickleBoBo <yukmw0704@gmail.com>
2025-12-17 03:12:47 +0900

    feat: 백준 10809 알파벳 찾기

--- 2025-12/src/day_04/boj_10809/Main.java
+++ 2025-12/src/day_04/boj_10809/Main.java
@@ -5,7 +5,6 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
 
         String S = br.readLine();
@@ -14,7 +13,6 @@ public class Main {
             sb.append(S.indexOf((char) (i + 'a'))).append(" ");
         }
 
-        bw.write(sb.toString());
-        bw.flush();
+        System.out.println(sb);
     }
 }
