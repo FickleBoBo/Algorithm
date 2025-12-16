9db0b84332e80f276f71e705b08d5fb30d849f9e
FickleBoBo <yukmw0704@gmail.com>
2025-12-17 02:58:57 +0900

    feat: 백준 10871 X보다 작은 수

--- 2025-12/src/day_02/boj_10871/Main.java
+++ 2025-12/src/day_02/boj_10871/Main.java
@@ -6,7 +6,6 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -23,7 +22,6 @@ public class Main {
             if (n < X) sb.append(n).append(" ");
         }
 
-        bw.write(sb.toString());
-        bw.flush();
+        System.out.println(sb);
     }
 }
