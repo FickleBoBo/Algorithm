35f270075652be180738acc6430b6df26a27772b
FickleBoBo <yukmw0704@gmail.com>
2025-12-17 03:23:58 +0900

    feat: 백준 5676 음주 코딩

--- 2025-12/src/day_13/boj_5676/Main.java
+++ 2025-12/src/day_13/boj_5676/Main.java
@@ -66,7 +66,6 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
@@ -106,7 +105,6 @@ public class Main {
             sb.append("\n");
         }
 
-        bw.write(sb.toString());
-        bw.flush();
+        System.out.println(sb);
     }
 }
