b9846775908c4fd5ad6c890d8857e7c5cc4d5d75
FickleBoBo <yukmw0704@gmail.com>
2025-12-17 03:10:36 +0900

    feat: 백준 10814 나이순 정렬

---
+++ 2025-12/src/day_17/boj_10814/Main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    vector<pair<int, string>> v(n);
+    for (auto& p : v) cin >> p.first >> p.second;
+    stable_sort(v.begin(), v.end(), [](const pair<int, string>& a, const pair<int, string>& b) {
+        return a.first < b.first;
+    });
+
+    for (auto& p : v) {
+        cout << p.first << ' ' << p.second << '\n';
+    }
+}
