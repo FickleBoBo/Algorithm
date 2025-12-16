ecffa1a7bdaf6eae015131df06505657874e31c3
FickleBoBo <yukmw0704@gmail.com>
2025-12-17 02:52:52 +0900

    feat: 백준 10950 A+B - 3

--- 2025-12/src/day_02/boj_10950/Main.java
+++ 2025-12/src/day_02/boj_10950/Main.java
@@ -6,7 +6,6 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
@@ -19,7 +18,6 @@ public class Main {
             sb.append(A + B).append("\n");
         }
 
-        bw.write(sb.toString());
-        bw.flush();
+        System.out.println(sb);
     }
 }
