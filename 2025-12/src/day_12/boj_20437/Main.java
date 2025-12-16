e06274919bbcdae58361dfb046e7076b058988cc
FickleBoBo <yukmw0704@gmail.com>
2025-12-17 03:22:40 +0900

    feat: 백준 20437 문자열 게임 2

--- 2025-12/src/day_12/boj_20437/Main.java
+++ 2025-12/src/day_12/boj_20437/Main.java
@@ -6,7 +6,6 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
 
         int T = Integer.parseInt(br.readLine());
@@ -42,7 +41,6 @@ public class Main {
             }
         }
 
-        bw.write(sb.toString());
-        bw.flush();
+        System.out.println(sb);
     }
 }
