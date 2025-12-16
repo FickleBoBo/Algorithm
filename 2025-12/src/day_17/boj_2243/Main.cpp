ce686393b1223cda346391ec328066563c81a0a5
FickleBoBo <yukmw0704@gmail.com>
2025-12-17 08:15:26 +0900

    feat: 백준 2243 사탕상자

---
+++ 2025-12/src/day_17/boj_2243/Main.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+constexpr int MAX = 1000000;
+
+struct SegTree {
+    int n;
+    vector<int> tree;
+
+    SegTree(int n) : n(n), tree(4 * n) {
+    }
+
+    void update(int node, int start, int end, int idx, int delta) {
+        if (start == end) {
+            tree[node] += delta;
+            return;
+        }
+
+        int mid = (start + end) / 2;
+
+        if (idx <= mid) {
+            update(node * 2, start, mid, idx, delta);
+        } else {
+            update(node * 2 + 1, mid + 1, end, idx, delta);
+        }
+        tree[node] = tree[node * 2] + tree[node * 2 + 1];
+    }
+
+    int queryKth(int node, int start, int end, int k) {
+        if (start == end) return start;
+
+        int mid = (start + end) / 2;
+
+        if (k <= tree[node * 2]) {
+            return queryKth(node * 2, start, mid, k);
+        } else {
+            return queryKth(node * 2 + 1, mid + 1, end, k - tree[node * 2]);
+        }
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    SegTree tree(MAX);
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        int a, b;
+        cin >> a >> b;
+
+        if (a == 1) {
+            int k = tree.queryKth(1, 1, MAX, b);
+            tree.update(1, 1, MAX, k, -1);
+            cout << k << '\n';
+        } else {
+            int c;
+            cin >> c;
+            tree.update(1, 1, MAX, b, c);
+        }
+    }
+}
