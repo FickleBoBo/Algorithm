1f4c20a9eca91ec149dd86f41930781293c05ffb
FickleBoBo <yukmw0704@gmail.com>
2025-12-17 03:17:07 +0900

    feat: 백준 2756 다트

--- 2025-12/src/day_07/boj_2756/Main.java
+++ 2025-12/src/day_07/boj_2756/Main.java
@@ -6,7 +6,6 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
@@ -32,8 +31,7 @@ public class Main {
             }
         }
 
-        bw.write(sb.toString());
-        bw.flush();
+        System.out.println(sb);
     }
 
     static double dist(double x, double y) {
