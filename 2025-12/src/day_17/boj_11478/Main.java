465232e84147c923fd6e694dbd81be002f9473e3
FickleBoBo <yukmw0704@gmail.com>
2025-12-17 03:10:55 +0900

    feat: 백준 11478 서로 다른 부분 문자열의 개수

---
+++ 2025-12/src/day_17/boj_11478/Main.java
@@ -0,0 +1,21 @@
+package day_17.boj_11478;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Set<String> set = new HashSet<>();
+        String str = br.readLine();
+
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i; j < str.length(); j++) {
+                set.add(str.substring(i, j + 1));
+            }
+        }
+
+        System.out.println(set.size());
+    }
+}
