78277287984cd207799a766db4788a218145d48a
FickleBoBo <yukmw0704@gmail.com>
2025-12-17 03:21:07 +0900

    feat: 백준 2720 세탁소 사장 동혁

--- 2025-12/src/day_11/boj_2720/Main.java
+++ 2025-12/src/day_11/boj_2720/Main.java
@@ -5,7 +5,6 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
 
         int T = Integer.parseInt(br.readLine());
@@ -24,7 +23,6 @@ public class Main {
             sb.append(C).append("\n");
         }
 
-        bw.write(sb.toString());
-        bw.flush();
+        System.out.println(sb);
     }
 }
