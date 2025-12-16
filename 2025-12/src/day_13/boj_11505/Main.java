63ba2c8ea2de39af4ba7657b09217f0f6ba23f5a
FickleBoBo <yukmw0704@gmail.com>
2025-12-17 03:25:03 +0900

    feat: 백준 11505 구간 곱 구하기

--- 2025-12/src/day_13/boj_11505/Main.java
+++ 2025-12/src/day_13/boj_11505/Main.java
@@ -68,7 +68,6 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -95,7 +94,6 @@ public class Main {
             }
         }
 
-        bw.write(sb.toString());
-        bw.flush();
+        System.out.println(sb);
     }
 }
