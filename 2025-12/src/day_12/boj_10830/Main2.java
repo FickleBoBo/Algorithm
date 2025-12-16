35eeb721a41bf360e053f06d762286d8f3ed1d69
FickleBoBo <yukmw0704@gmail.com>
2025-12-17 03:22:24 +0900

    feat: 백준 10830 행렬 제곱

--- 2025-12/src/day_12/boj_10830/Main2.java
+++ 2025-12/src/day_12/boj_10830/Main2.java
@@ -9,7 +9,6 @@ public class Main2 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -33,8 +32,7 @@ public class Main2 {
             sb.append("\n");
         }
 
-        bw.write(sb.toString());
-        bw.flush();
+        System.out.println(sb);
     }
 
     static int[][] binPow(int[][] mat, long n) {
