5c491c53997f8e4fcddfa54168b3a82e98da678d
FickleBoBo <yukmw0704@gmail.com>
2025-12-17 03:14:42 +0900

    feat: 백준 1929 소수 구하기

--- 2025-12/src/day_06/boj_1929/Main.java
+++ 2025-12/src/day_06/boj_1929/Main.java
@@ -6,7 +6,6 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -18,8 +17,7 @@ public class Main {
             if (isPrime[i]) sb.append(i).append("\n");
         }
 
-        bw.write(sb.toString());
-        bw.flush();
+        System.out.println(sb);
     }
 
     static boolean[] sieve(int N) {
