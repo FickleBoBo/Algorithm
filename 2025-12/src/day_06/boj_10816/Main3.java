f323451e394c6486cda5ad5adcb4d59f1258a5c9
FickleBoBo <yukmw0704@gmail.com>
2025-12-17 03:16:04 +0900

    feat: 백준 10816 숫자 카드 2

--- 2025-12/src/day_06/boj_10816/Main3.java
+++ 2025-12/src/day_06/boj_10816/Main3.java
@@ -6,7 +6,6 @@ import java.util.*;
 public class Main3 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
@@ -25,7 +24,6 @@ public class Main3 {
             sb.append(map.getOrDefault(Integer.parseInt(st.nextToken()), 0)).append(" ");
         }
 
-        bw.write(sb.toString());
-        bw.flush();
+        System.out.println(sb);
     }
 }
