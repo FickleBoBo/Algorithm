6ae62c22d5596cb8afc190f4793775f85dca348a
FickleBoBo <yukmw0704@gmail.com>
2025-12-17 03:15:10 +0900

    feat: 백준 2751 수 정렬하기 2

--- 2025-12/src/day_06/boj_2751/Main.java
+++ 2025-12/src/day_06/boj_2751/Main.java
@@ -6,7 +6,6 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
@@ -21,7 +20,6 @@ public class Main {
             sb.append(n).append("\n");
         }
 
-        bw.write(sb.toString());
-        bw.flush();
+        System.out.println(sb);
     }
 }
