1e2fe0f136a00a0cff707000ff15f743f770e9c4
FickleBoBo <yukmw0704@gmail.com>
2025-12-17 03:12:29 +0900

    feat: 백준 9086 문자열

--- 2025-12/src/day_04/boj_9086/Main.java
+++ 2025-12/src/day_04/boj_9086/Main.java
@@ -5,7 +5,6 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
 
         int T = Integer.parseInt(br.readLine());
@@ -15,7 +14,6 @@ public class Main {
             sb.append(str.charAt(0)).append(str.charAt(str.length() - 1)).append("\n");
         }
 
-        bw.write(sb.toString());
-        bw.flush();
+        System.out.println(sb);
     }
 }
