a50c1867ba0474cf9c6d89796a0b6b3d3e63b0ec
FickleBoBo <yukmw0704@gmail.com>
2025-12-17 03:24:50 +0900

    feat: 백준 2042 구간 합 구하기

--- 2025-12/src/day_13/boj_2042/Main.java
+++ 2025-12/src/day_13/boj_2042/Main.java
@@ -66,7 +66,6 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -93,7 +92,6 @@ public class Main {
             }
         }
 
-        bw.write(sb.toString());
-        bw.flush();
+        System.out.println(sb);
     }
 }
