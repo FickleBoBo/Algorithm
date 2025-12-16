084fe91a01303cd72cbef7b14d30e4c6c17d5bcc
FickleBoBo <yukmw0704@gmail.com>
2025-12-17 03:16:22 +0900

    feat: 백준 10989 수 정렬하기 3

--- 2025-12/src/day_06/boj_10989/Main.java
+++ 2025-12/src/day_06/boj_10989/Main.java
@@ -8,7 +8,6 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
@@ -24,7 +23,6 @@ public class Main {
             }
         }
 
-        bw.write(sb.toString());
-        bw.flush();
+        System.out.println(sb);
     }
 }
