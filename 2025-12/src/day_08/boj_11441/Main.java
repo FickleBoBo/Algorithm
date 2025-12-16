d6b407fb6e7ad244580c9e42db032e467b04c18a
FickleBoBo <yukmw0704@gmail.com>
2025-12-17 03:18:16 +0900

    feat: 백준 11441 합 구하기

--- 2025-12/src/day_08/boj_11441/Main.java
+++ 2025-12/src/day_08/boj_11441/Main.java
@@ -6,7 +6,6 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
@@ -32,7 +31,6 @@ public class Main {
             sb.append(pSum[end] - pSum[start - 1]).append("\n");
         }
 
-        bw.write(sb.toString());
-        bw.flush();
+        System.out.println(sb);
     }
 }
