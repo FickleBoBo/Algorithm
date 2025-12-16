b32934d1d06826fbab60aad83ba79e6c9223f409
FickleBoBo <yukmw0704@gmail.com>
2025-12-17 03:15:55 +0900

    feat: 백준 10815 숫자 카드

--- 2025-12/src/day_06/boj_10815/Main3.java
+++ 2025-12/src/day_06/boj_10815/Main3.java
@@ -6,7 +6,6 @@ import java.util.*;
 public class Main3 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
@@ -28,7 +27,6 @@ public class Main3 {
             }
         }
 
-        bw.write(sb.toString());
-        bw.flush();
+        System.out.println(sb);
     }
 }
