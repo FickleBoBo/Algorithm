8e6e476543630f38ab3ba7d778bd4e68c4d862da
FickleBoBo <yukmw0704@gmail.com>
2025-12-17 03:19:34 +0900

    feat: 백준 1963 소수 경로

--- 2025-12/src/day_09/boj_1963/Main.java
+++ 2025-12/src/day_09/boj_1963/Main.java
@@ -9,7 +9,6 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
@@ -29,8 +28,7 @@ public class Main {
             }
         }
 
-        bw.write(sb.toString());
-        bw.flush();
+        System.out.println(sb);
     }
 
     static boolean[] sieve(int N) {
