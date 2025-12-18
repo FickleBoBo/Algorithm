5d09a03896f073efde336a85ab62d29a81413455
FickleBoBo <yukmw0704@gmail.com>
2025-12-19 08:37:17 +0900

    feat: 백준 7785 회사에 있는 사람

---
+++ 2025-12/src/day_18/boj_7785/Main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    set<string, greater<string>> st;
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        string name, type;
+        cin >> name >> type;
+
+        if (type == "enter") {
+            st.insert(name);
+        } else {
+            st.erase(name);
+        }
+    }
+
+    for (const string& s : st) {
+        cout << s << '\n';
+    }
+}
