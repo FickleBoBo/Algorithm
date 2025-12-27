e2e52f4ec14b952ffc5d5edfd49287a61c1730b6
FickleBoBo <yukmw0704@gmail.com>
2025-12-27 14:07:23 +0900

    feat: [BaekJoon] 2433번 - The Sound of Silence [Java][C++]

--- 2025-12/src/day_20/boj_2433/Main.cpp
+++ 2025-12/src/day_20/boj_2433/Main.cpp
@@ -5,27 +5,47 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n, l;
-    cin >> n >> l;
+    int n, m, c;
+    cin >> n >> m >> c;
 
     vector<int> v(n);
     for (int& x : v) cin >> x;
 
-    // 단조 덱
-    deque<int> dq;
+    deque<int> minDq;  // 단조 증가 큐
+    deque<int> maxDq;  // 단조 감소 큐
+    bool hasSilence = false;
+
     for (int i = 0; i < n; i++) {
         // 구간에서 나가는 인덱스 처리
-        if (!dq.empty() && dq.front() == (i - l)) {
-            dq.pop_front();
+        if (!minDq.empty() && minDq.front() == (i - m)) {
+            minDq.pop_front();
         }
 
         // 구간으로 들어오는 인덱스 처리
-        while (!dq.empty() && v[i] <= v[dq.back()]) {
-            dq.pop_back();
+        while (!minDq.empty() && v[i] <= v[minDq.back()]) {
+            minDq.pop_back();
+        }
+        minDq.push_back(i);
+
+        // 구간에서 나가는 인덱스 처리
+        if (!maxDq.empty() && maxDq.front() == (i - m)) {
+            maxDq.pop_front();
         }
 
-        dq.push_back(i);
+        // 구간으로 들어오는 인덱스 처리
+        while (!maxDq.empty() && v[i] >= v[maxDq.back()]) {
+            maxDq.pop_back();
+        }
+        maxDq.push_back(i);
 
-        cout << v[dq.front()] << ' ';
+        // 윈도우 크기가 확보됐을 때부터 비교
+        if ((i > m - 2) && (v[maxDq.front()] - v[minDq.front()] <= c)) {
+            cout << i - m + 2 << '\n';
+            hasSilence = true;
+        }
+    }
+
+    if (!hasSilence) {
+        cout << "NONE";
     }
 }
--- 2025-12/src/day_20/boj_2433/Main2.cpp
+++ 2025-12/src/day_20/boj_2433/Main2.cpp
@@ -5,21 +5,35 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n, l;
-    cin >> n >> l;
+    int n, m, c;
+    cin >> n >> m >> c;
 
     vector<int> v(n);
     for (int& x : v) cin >> x;
 
-    // 우선순위 큐
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> minPq;  // 최소힙
+    priority_queue<pair<int, int>> maxPq;                                     // 최대힙
+    bool hasSilence = false;
+
     for (int i = 0; i < n; i++) {
-        while (!pq.empty() && pq.top().second <= (i - l)) {
-            pq.pop();
+        while (!minPq.empty() && minPq.top().second <= (i - m)) {
+            minPq.pop();
         }
+        minPq.push({v[i], i});
 
-        pq.push({v[i], i});
+        while (!maxPq.empty() && maxPq.top().second <= (i - m)) {
+            maxPq.pop();
+        }
+        maxPq.push({v[i], i});
 
-        cout << pq.top().first << ' ';
+        // 윈도우 크기가 확보됐을 때부터 비교
+        if ((i > m - 2) && (maxPq.top().first - minPq.top().first <= c)) {
+            cout << i - m + 2 << '\n';
+            hasSilence = true;
+        }
+    }
+
+    if (!hasSilence) {
+        cout << "NONE";
     }
 }
