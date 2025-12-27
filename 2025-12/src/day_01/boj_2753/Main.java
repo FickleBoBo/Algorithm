d8977af0bc3a47fe9fb468b754417860b34ee296
FickleBoBo <yukmw0704@gmail.com>
2025-12-27 12:54:19 +0900

    feat: [BaekJoon] 2753번 - 윤년 [Java][C++]

--- 2025-12/src/day_01/boj_2753/Main.java
+++ 2025-12/src/day_01/boj_2753/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
         int year = Integer.parseInt(br.readLine());
 
-        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+        if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
             System.out.println(1);
         } else {
             System.out.println(0);
