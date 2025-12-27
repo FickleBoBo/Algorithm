4412297e973173facace979a197219db64bbc412
FickleBoBo <yukmw0704@gmail.com>
2025-12-27 14:09:43 +0900

    feat: [BaekJoon] 10814번 - 나이순 정렬 [Java][C++]

--- 2025-12/src/day_17/boj_10814/Main.cpp
+++ 2025-12/src/day_17/boj_10814/Main.cpp
@@ -10,11 +10,11 @@ int main() {
 
     vector<pair<int, string>> v(n);
     for (auto& p : v) cin >> p.first >> p.second;
-    stable_sort(v.begin(), v.end(), [](const pair<int, string>& a, const pair<int, string>& b) {
+    stable_sort(v.begin(), v.end(), [](const auto& a, const auto& b) {
         return a.first < b.first;
     });
 
-    for (auto p : v) {
+    for (auto& p : v) {
         cout << p.first << ' ' << p.second << '\n';
     }
 }
