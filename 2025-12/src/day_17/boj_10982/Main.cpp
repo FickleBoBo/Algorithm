637c738de4c2b13395ab34f6235795193a24a072
FickleBoBo <yukmw0704@gmail.com>
2025-12-27 14:10:04 +0900

    feat: [BaekJoon] 10982번 - 행운쿠키 제작소 [Java][C++]

--- 2025-12/src/day_17/boj_10982/Main.cpp
+++ 2025-12/src/day_17/boj_10982/Main.cpp
@@ -18,7 +18,7 @@ int main() {
         int sum = 0;
         for (auto p : v) sum += p.first;
 
-        vector<vector<int>> dp(2, vector<int>(1 + sum));  // 토글링 dp
+        vector<vector<int>> dp(2, vector<int>(1 + sum));  // 롤링 배열
         int prev = 0;
         int cur = 1;
         for (int i = 0; i < n; i++) {
@@ -33,14 +33,14 @@ int main() {
                 }
             }
 
-            // 토글링
+            // 롤링
             prev ^= 1;
             cur ^= 1;
         }
 
         int mn = INT_MAX;
         for (int j = 0; j <= sum; j++) {
-            mn = min(mn, max(j, dp[prev][j]));  // 토글링 때문에 최신값이 prev에 있음
+            mn = min(mn, max(j, dp[prev][j]));  // 롤링 때문에 최신값이 prev에 있음
         }
 
         cout << mn << '\n';
