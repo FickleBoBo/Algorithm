5ef6d1814b5c32e2576ff3756ac8c2cd2c1d18c6
FickleBoBo <yukmw0704@gmail.com>
2025-12-27 14:08:30 +0900

    feat: [Programmers] 1845번 - 폰켓몬 [Java][C++]

--- 2025-12/src/day_18/prms_1845/Solution.cpp
+++ 2025-12/src/day_18/prms_1845/Solution.cpp
@@ -5,8 +5,6 @@
 using namespace std;
 
 int solution(vector<int> nums) {
-    unordered_set<int> st;
-    for (int n : nums) st.insert(n);
-
+    unordered_set<int> st(nums.begin(), nums.end());
     return min(st.size(), nums.size() / 2);
 }
