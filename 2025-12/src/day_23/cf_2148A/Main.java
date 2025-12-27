5677f7e332201854768f6c615976f0a3111c0626
FickleBoBo <yukmw0704@gmail.com>
2025-12-27 14:05:36 +0900

    feat: [Codeforces] #2148A - Sublime Sequence [Java][C++]

---
+++ 2025-12/src/day_23/cf_2148A/Main.java
@@ -0,0 +1,27 @@
+package day_23.cf_2148A;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int t = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= t; tc++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int n = Integer.parseInt(st.nextToken());
+
+            if (n % 2 == 0) {
+                sb.append("0\n");
+            } else {
+                sb.append(x).append("\n");
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
