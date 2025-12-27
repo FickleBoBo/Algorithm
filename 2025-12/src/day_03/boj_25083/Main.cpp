4eeb4875ffbfccd9ee0d017b5653d4015da8993a
FickleBoBo <yukmw0704@gmail.com>
2025-12-27 12:57:24 +0900

    feat: [BaekJoon] 25083번 - 새싹 [Java][C++]

--- 2025-12/src/day_03/boj_25083/Main.cpp
+++ 2025-12/src/day_03/boj_25083/Main.cpp
@@ -5,10 +5,10 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    cout << "         ,r'\"7\n";
-    cout << "r`-_   ,'  ,/\n";
-    cout << " \\. \". L_r'\n";
-    cout << "   `~\\/\n";
-    cout << "      |\n";
-    cout << "      |\n";
+    cout << "         ,r'\"7" << '\n';
+    cout << "r`-_   ,'  ,/" << '\n';
+    cout << " \\. \". L_r'" << '\n';
+    cout << "   `~\\/" << '\n';
+    cout << "      |" << '\n';
+    cout << "      |" << '\n';
 }
