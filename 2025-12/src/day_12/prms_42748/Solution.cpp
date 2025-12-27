e413ba4e8ec057e3c2326d1610977e7f3a61e7d6
FickleBoBo <yukmw0704@gmail.com>
2025-12-28 07:32:00 +0900

    feat: [Programmers] 42748번 - K번째수 [Java][C++]

--- 2025-12/src/day_12/prms_42748/Solution.cpp
+++ 2025-12/src/day_12/prms_42748/Solution.cpp
@@ -7,12 +7,12 @@ using namespace std;
 vector<int> solution(vector<int> array, vector<vector<int>> commands) {
     vector<int> ans;
 
-    for (int i = 0; i < commands.size(); i++) {
-        int s = commands[i][0];
-        int e = commands[i][1];
-        int k = commands[i][2];
+    for (auto& c : commands) {
+        int s = c[0];
+        int e = c[1];
+        int k = c[2];
 
-        vector<int> tmp(array.begin() + s - 1, array.begin() + e);  // 범위 생성자 활용
+        vector<int> tmp(array.begin() + s - 1, array.begin() + e);
         sort(tmp.begin(), tmp.end());
         ans.push_back(tmp[k - 1]);
     }
