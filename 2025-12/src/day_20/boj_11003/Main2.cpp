1210f062cd8ab38496e7295838b3234e59deb8b0
FickleBoBo <yukmw0704@gmail.com>
2025-12-27 14:07:36 +0900

    feat: [BaekJoon] 11003번 - 최솟값 찾기 [Java][C++]

--- 2025-12/src/day_20/boj_11003/Main2.cpp
+++ 2025-12/src/day_20/boj_11003/Main2.cpp
@@ -5,35 +5,21 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n, m, c;
-    cin >> n >> m >> c;
+    int n, l;
+    cin >> n >> l;
 
     vector<int> v(n);
     for (int& x : v) cin >> x;
 
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> minPq;  // 최소힙
-    priority_queue<pair<int, int>> maxPq;                                     // 최대힙
-    bool hasSilence = false;
-
+    // 우선순위 큐
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
     for (int i = 0; i < n; i++) {
-        while (!minPq.empty() && minPq.top().second <= (i - m)) {
-            minPq.pop();
+        while (!pq.empty() && pq.top().second <= (i - l)) {
+            pq.pop();
         }
-        minPq.push({v[i], i});
 
-        while (!maxPq.empty() && maxPq.top().second <= (i - m)) {
-            maxPq.pop();
-        }
-        maxPq.push({v[i], i});
+        pq.push({v[i], i});
 
-        // 윈도우 크기가 확보됐을 때부터 비교
-        if ((i > m - 2) && (maxPq.top().first - minPq.top().first <= c)) {
-            cout << i - m + 2 << '\n';
-            hasSilence = true;
-        }
-    }
-
-    if (!hasSilence) {
-        cout << "NONE";
+        cout << pq.top().first << ' ';
     }
 }
