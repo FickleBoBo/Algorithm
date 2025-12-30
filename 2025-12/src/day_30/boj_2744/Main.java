e3b9170ffb4fb174f931dc92b472b2c3cf97805c
FickleBoBo <yukmw0704@gmail.com>
2025-12-30 21:02:34 +0900

    feat: [BaekJoon] 2744번 - 대소문자 바꾸기 [Java][C++]

---
+++ 2025-12/src/day_30/boj_2744/Main.java
@@ -0,0 +1,18 @@
+package day_30.boj_2744;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        for (char c : str.toCharArray()) {
+            if (Character.isLowerCase(c)) {
+                System.out.print(Character.toUpperCase(c));
+            } else {
+                System.out.print(Character.toLowerCase(c));
+            }
+        }
+    }
+}
