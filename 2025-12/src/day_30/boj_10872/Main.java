1fb53f8814dad2bcaec79862e7b9c6d7c344591a
FickleBoBo <yukmw0704@gmail.com>
2025-12-30 21:03:28 +0900

    feat: [BaekJoon] 10872번 - 팩토리얼 [Java][C++]

---
+++ 2025-12/src/day_30/boj_10872/Main.java
@@ -0,0 +1,18 @@
+package day_30.boj_10872;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int fact = 1;
+        for (int i = 1; i <= N; i++) {
+            fact *= i;
+        }
+
+        System.out.println(fact);
+    }
+}
