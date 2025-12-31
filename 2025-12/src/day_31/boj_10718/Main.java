e3d5f3b5e7884eb6d56a3a63edc15ccb9c6fea58
FickleBoBo <yukmw0704@gmail.com>
2026-01-01 00:16:14 +0900

    feat: [BaekJoon] 10718번 - We love kriii [Java][C++]

---
+++ 2025-12/src/day_31/boj_10718/Main.java
@@ -0,0 +1,8 @@
+package day_31.boj_10718;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }
+}
