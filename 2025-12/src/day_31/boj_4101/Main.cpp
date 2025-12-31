927da0a75d56487daac50ecfef2cfa53030fec97
FickleBoBo <yukmw0704@gmail.com>
2026-01-01 00:14:35 +0900

    feat: [BaekJoon] 4101번 - 크냐? [Java][C++]

---
+++ 2025-12/src/day_31/boj_4101/Main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    while (true) {
+        int a, b;
+        cin >> a >> b;
+
+        if (a == 0) break;
+
+        if (a > b) {
+            cout << "Yes\n";
+        } else {
+            cout << "No\n";
+        }
+    }
+}
