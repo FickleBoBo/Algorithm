33c5383817ce8be70654cc879e141c9d564faccf
FickleBoBo <yukmw0704@gmail.com>
2026-01-01 00:16:53 +0900

    feat: [BaekJoon] 15680번 - 연세대학교 [Java][C++]

---
+++ 2025-12/src/day_31/boj_15680/Main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    if (n == 0) {
+        cout << "YONSEI";
+    } else {
+        cout << "Leading the Way to the Future";
+    }
+}
