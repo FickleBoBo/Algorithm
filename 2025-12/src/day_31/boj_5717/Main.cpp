8e70660ddeaff7371bc7037955323b97d5f1a362
FickleBoBo <yukmw0704@gmail.com>
2026-01-01 00:15:07 +0900

    feat: [BaekJoon] 5717번 - 상근이의 친구들 [Java][C++]

---
+++ 2025-12/src/day_31/boj_5717/Main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    while (true) {
+        int a, b;
+        cin >> a >> b;
+
+        if (a == 0) break;
+
+        cout << a + b << '\n';
+    }
+}
---
+++ 2025-12/src/day_31/boj_5717/Main.java
@@ -0,0 +1,24 @@
+package day_31.boj_5717;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        while (true) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            if (A == 0) break;
+
+            sb.append(A + B).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
