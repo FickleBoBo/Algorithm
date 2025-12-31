83f27f385be4973ad28c828171735080242aea88
FickleBoBo <yukmw0704@gmail.com>
2026-01-01 00:15:58 +0900

    [BaekJoon] 10189번 - Hook [Java][C++]

---
+++ 2025-12/src/day_31/boj_10189/Main.java
@@ -0,0 +1,10 @@
+package day_31.boj_10189;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("#  # #### #### #  #");
+        System.out.println("#### #  # #  # # #");
+        System.out.println("#### #  # #  # # #");
+        System.out.println("#  # #### #### #  #");
+    }
+}
