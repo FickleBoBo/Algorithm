55273e891632179cd082adcc5ae684269f737b45
FickleBoBo <yukmw0704@gmail.com>
2026-01-01 00:15:24 +0900

    feat: [BaekJoon] 9316번 - Hello Judge [Java][C++]

---
+++ 2025-12/src/day_31/boj_9316/Main.java
@@ -0,0 +1,18 @@
+package day_31.boj_9316;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            sb.append("Hello World, Judge ").append(i).append("!\n");
+        }
+
+        System.out.println(sb);
+    }
+}
