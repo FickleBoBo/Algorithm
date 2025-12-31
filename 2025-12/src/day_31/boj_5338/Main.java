6627f9a0ff0042c4cbaa9c5af67cc22b87a62012
FickleBoBo <yukmw0704@gmail.com>
2026-01-01 00:14:53 +0900

    feat: [BaekJoon] 5338번 - 마이크로소프트 로고 [Java][C++]

---
+++ 2025-12/src/day_31/boj_5338/Main.java
@@ -0,0 +1,11 @@
+package day_31.boj_5338;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("       _.-;;-._");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-;;-._|");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-''-._|");
+    }
+}
