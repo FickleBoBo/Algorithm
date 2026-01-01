64267e85e478b1fe2e89dc7d6a168d8ef2463821
FickleBoBo <yukmw0704@gmail.com>
2026-01-01 23:45:39 +0900

    feat: [BaekJoon] 2161번 - 카드1 [Java][C++]

---
+++ 2026-01/src/day_01/boj_2161/Main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    deque<int> q(n);
+    iota(q.begin(), q.end(), 1);
+
+    while (q.size() > 1) {
+        cout << q.front() << ' ';
+        q.pop_front();
+
+        q.push_back(q.front());
+        q.pop_front();
+    }
+    cout << q.front();
+}
