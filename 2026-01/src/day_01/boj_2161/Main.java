0621530373798e05f34f683b07f0a255d89fac2a
FickleBoBo <yukmw0704@gmail.com>
2026-01-01 23:45:39 +0900

    feat: [BaekJoon] 2161번 - 카드1 [Java][C++]

---
+++ 2026-01/src/day_01/boj_2161/Main.java
@@ -0,0 +1,26 @@
+package day_01.boj_2161;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        Queue<Integer> q = new ArrayDeque<>();
+        for (int i = 1; i <= N; i++) {
+            q.offer(i);
+        }
+
+        while (q.size() > 1) {
+            sb.append(q.poll()).append(" ");
+            q.offer(q.poll());
+        }
+        sb.append(q.poll());
+
+        System.out.println(sb);
+    }
+}
