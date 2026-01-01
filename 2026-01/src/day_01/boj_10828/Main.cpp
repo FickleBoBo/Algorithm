5e0d76070c7a35e8eb2e45b3ec9a8400f9ae10e4
FickleBoBo <yukmw0704@gmail.com>
2026-01-01 23:47:38 +0900

    feat: [BaekJoon] 10828번 - 스택 [Java][C++]

---
+++ 2026-01/src/day_01/boj_10828/Main.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int stack[10000];
+    int peek = -1;
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        string command;
+        cin >> command;
+
+        if (command == "push") {
+            int x;
+            cin >> x;
+            stack[++peek] = x;
+        } else if (command == "pop") {
+            if (peek == -1) {
+                cout << -1 << '\n';
+            } else {
+                cout << stack[peek--] << '\n';
+            }
+        } else if (command == "size") {
+            cout << peek + 1 << '\n';
+        } else if (command == "empty") {
+            if (peek == -1) {
+                cout << 1 << '\n';
+            } else {
+                cout << 0 << '\n';
+            }
+        } else {
+            if (peek == -1) {
+                cout << -1 << '\n';
+            } else {
+                cout << stack[peek] << '\n';
+            }
+        }
+    }
+}
