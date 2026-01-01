30453f76855656b95c284efdd7d51344e20b80ac
FickleBoBo <yukmw0704@gmail.com>
2026-01-01 23:46:07 +0900

    feat: [BaekJoon] 5337번 - 웰컴 [Java][C++]

---
+++ 2026-01/src/day_01/boj_5337/Main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cout << ".  .   ." << '\n';
+    cout << "|  | _ | _. _ ._ _  _" << '\n';
+    cout << "|/\\|(/.|(_.(_)[ | )(/." << '\n';
+}
