757930d633681c0b2c20cfaf6470c1d8c9f63eac
FickleBoBo <yukmw0704@gmail.com>
2026-01-01 23:49:59 +0900

    feat: [BaekJoon] 10845번 - 큐 [Java][C++]

---
+++ 2026-01/src/day_01/boj_10845/Main.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int queue[10000];
+    int front = 0;
+    int rear = 0;
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        string command;
+        cin >> command;
+
+        if (command == "push") {
+            int x;
+            cin >> x;
+            queue[rear++] = x;
+        } else if (command == "pop") {
+            if (front == rear) {
+                cout << -1 << '\n';
+            } else {
+                cout << queue[front++] << '\n';
+            }
+        } else if (command == "size") {
+            cout << rear - front << '\n';
+        } else if (command == "empty") {
+            if (front == rear) {
+                cout << 1 << '\n';
+            } else {
+                cout << 0 << '\n';
+            }
+        } else if (command == "front") {
+            if (front == rear) {
+                cout << -1 << '\n';
+            } else {
+                cout << queue[front] << '\n';
+            }
+        } else {
+            if (front == rear) {
+                cout << -1 << '\n';
+            } else {
+                cout << queue[rear - 1] << '\n';
+            }
+        }
+    }
+}
---
+++ 2026-01/src/day_01/boj_10845/Main.java
@@ -0,0 +1,55 @@
+package day_01.boj_10845;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int[] queue = new int[10000];
+        int front = 0;
+        int rear = 0;
+
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
+
+            if (command.equals("push")) {
+                int x = Integer.parseInt(st.nextToken());
+                queue[rear++] = x;
+            } else if (command.equals("pop")) {
+                if (front == rear) {
+                    sb.append("-1\n");
+                } else {
+                    sb.append(queue[front++]).append("\n");
+                }
+            } else if (command.equals("size")) {
+                sb.append(rear - front).append("\n");
+            } else if (command.equals("empty")) {
+                if (front == rear) {
+                    sb.append("1\n");
+                } else {
+                    sb.append("0\n");
+                }
+            } else if (command.equals("front")) {
+                if (front == rear) {
+                    sb.append("-1\n");
+                } else {
+                    sb.append(queue[front]).append("\n");
+                }
+            } else {
+                if (front == rear) {
+                    sb.append("-1\n");
+                } else {
+                    sb.append(queue[rear - 1]).append("\n");
+                }
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
