5c47141b903837c22e908d5cc0d43ae794152cc0
FickleBoBo <yukmw0704@gmail.com>
2026-01-02 16:16:07 +0900

    feat: [BaekJoon] 27327번 - 時間 (Hour) [Java][C++]

---
+++ 2026-01/src/day_02/boj_27327/Main.java
@@ -0,0 +1,12 @@
+package day_02.boj_27327;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int X = Integer.parseInt(br.readLine());
+        System.out.println(X * 24);
+    }
+}
