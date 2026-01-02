f5b71f432e9f79c830ba8584c2d75dec4d19717c
FickleBoBo <yukmw0704@gmail.com>
2026-01-02 16:15:39 +0900

    feat: [BaekJoon] 16394번 - 홍익대학교 [Java][C++]

---
+++ 2026-01/src/day_02/boj_16394/Main.java
@@ -0,0 +1,12 @@
+package day_02.boj_16394;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        System.out.println(N - 1946);
+    }
+}
