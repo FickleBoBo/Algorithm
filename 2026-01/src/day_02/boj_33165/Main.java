5f4bcdf1626ec2890c0b61b1c1836704ef98bc4b
FickleBoBo <yukmw0704@gmail.com>
2026-01-02 16:18:21 +0900

    feat: [BaekJoon] 33165번 - 徒競走 (Footrace) [Java][C++]

---
+++ 2026-01/src/day_02/boj_33165/Main.java
@@ -0,0 +1,14 @@
+package day_02.boj_33165;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+        int V = Integer.parseInt(br.readLine());
+
+        System.out.println(T * V);
+    }
+}
