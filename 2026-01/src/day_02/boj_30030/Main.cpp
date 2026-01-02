1a6632203638709fe523ec87fa5ad03ef5d7927d
FickleBoBo <yukmw0704@gmail.com>
2026-01-02 16:16:59 +0900

    feat: [BaekJoon] 30030번 - 스위트콘 가격 구하기 [Java][C++]

---
+++ 2026-01/src/day_02/boj_30030/Main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int b;
+    cin >> b;
+    cout << b * 10 / 11;
+}
---
+++ 2026-01/src/day_02/boj_30030/Main.java
@@ -0,0 +1,12 @@
+package day_02.boj_30030;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int B = Integer.parseInt(br.readLine());
+        System.out.println(B * 10 / 11);
+    }
+}
