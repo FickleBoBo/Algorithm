f463ae53ba8ce198f77be6371304b6f0f7419978
FickleBoBo <yukmw0704@gmail.com>
2026-01-02 16:18:07 +0900

    feat: [BaekJoon] 32951번 - AI 선도대학 [Java][C++]

---
+++ 2026-01/src/day_02/boj_32951/Main.java
@@ -0,0 +1,12 @@
+package day_02.boj_32951;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(N - 2024);
+    }
+}
