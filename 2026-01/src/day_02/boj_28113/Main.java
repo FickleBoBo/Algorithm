ccc5aba221fcf86cdaafc571072724fa5b887d11
FickleBoBo <yukmw0704@gmail.com>
2026-01-02 16:16:20 +0900

    feat: [BaekJoon] 28113번 - 정보섬의 대중교통 [Java][C++]

---
+++ 2026-01/src/day_02/boj_28113/Main.java
@@ -0,0 +1,23 @@
+package day_02.boj_28113;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        if (A < Math.max(N, B)) {
+            System.out.println("Bus");
+        } else if (A > Math.max(N, B)) {
+            System.out.println("Subway");
+        } else {
+            System.out.println("Anything");
+        }
+    }
+}
