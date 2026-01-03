d5f5ccf75eecc4b02316ce8cd665abf6e489bcb2
FickleBoBo <yukmw0704@gmail.com>
2026-01-04 01:50:43 +0900

    feat: [BaekJoon] 31611번 - 火曜日 (Tuesday) [Java][C++]

---
+++ 2026-01/src/day_03/boj_31611/Main.java
@@ -0,0 +1,17 @@
+package day_03.boj_31611;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int X = Integer.parseInt(br.readLine());
+
+        if (X % 7 == 2) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
