3cd793635f233ce70f93fa29c3bb9d6029c1965e
FickleBoBo <yukmw0704@gmail.com>
2026-01-04 01:50:07 +0900

    feat: [BaekJoon] 15439번 - 베라의 패션 [Java][C++]

---
+++ 2026-01/src/day_03/boj_15439/Main.java
@@ -0,0 +1,12 @@
+package day_03.boj_15439;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(N * N - N);
+    }
+}
