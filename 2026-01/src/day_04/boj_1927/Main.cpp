b383b359de86093a98de60785f00d5789980b326
FickleBoBo <yukmw0704@gmail.com>
2026-01-05 01:49:34 +0900

    feat: [BaekJoon] 1927번 - 최소 힙 [Java][C++]

---
+++ 2026-01/src/day_04/boj_1927/Main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    priority_queue<int, vector<int>, greater<>> pq;
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+
+        if (x) {
+            pq.push(x);
+        } else {
+            if (pq.empty()) {
+                cout << 0 << '\n';
+            } else {
+                cout << pq.top() << '\n';
+                pq.pop();
+            }
+        }
+    }
+}
