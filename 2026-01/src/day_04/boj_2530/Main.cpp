a465430f323d2a0272f9c6c43f12337c6f313508
FickleBoBo <yukmw0704@gmail.com>
2026-01-05 01:50:01 +0900

    feat: [BaekJoon] 2530번 - 인공지능 시계 [Java][C++]

---
+++ 2026-01/src/day_04/boj_2530/Main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    c += d;
+
+    b += c / 60;
+    c %= 60;
+
+    a += b / 60;
+    b %= 60;
+
+    a %= 24;
+
+    cout << a << ' ' << b << ' ' << c;
+}
