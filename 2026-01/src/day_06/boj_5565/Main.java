9b51f38463a58b37f1539d7e9896067949718729
FickleBoBo <yukmw0704@gmail.com>
2026-01-06 23:50:12 +0900

    feat: [BaekJoon] 5565번 - 영수증 [Java][C++]

---
+++ 2026-01/src/day_06/boj_5565/Main.java
@@ -0,0 +1,16 @@
+package day_06.boj_5565;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int sum = Integer.parseInt(br.readLine());
+        for (int i = 1; i < 10; i++) {
+            sum -= Integer.parseInt(br.readLine());
+        }
+
+        System.out.println(sum);
+    }
+}
