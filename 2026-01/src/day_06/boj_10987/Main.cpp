f6fbe80abbef943cf6b9cd9262f464dacc5d35a4
FickleBoBo <yukmw0704@gmail.com>
2026-01-06 23:52:03 +0900

    feat: [BaekJoon] 10987번 - 모음의 개수 [Java][C++]

---
+++ 2026-01/src/day_06/boj_10987/Main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string s;
+    cin >> s;
+
+    int cnt = 0;
+    for (char c : s) {
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') cnt++;
+    }
+
+    cout << cnt;
+}
