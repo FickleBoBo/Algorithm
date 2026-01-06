65b9bdd03f052bbf08c532c5da0877684d7952ec
FickleBoBo <yukmw0704@gmail.com>
2026-01-06 23:51:16 +0900

    feat: [BaekJoon] 10039번 - 평균 점수 [Java][C++]

---
+++ 2026-01/src/day_06/boj_10039/Main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int sum = 0;
+    for (int i = 0; i < 5; i++) {
+        int x;
+        cin >> x;
+        sum += max(x, 40);
+    }
+
+    cout << sum / 5;
+}
