066b5a6e6798ed41b0039401e13c68d1ff37eebb
FickleBoBo <yukmw0704@gmail.com>
2026-01-06 23:49:43 +0900

    feat: [BaekJoon] 3046번 - R2 [Java][C++]

---
+++ 2026-01/src/day_06/boj_3046/Main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int r1, s;
+    cin >> r1 >> s;
+    cout << 2 * s - r1;
+}
