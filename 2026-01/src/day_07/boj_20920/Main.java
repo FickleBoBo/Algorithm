ca1402659ea35f25a1849887ed21b7edbb3cdab3
FickleBoBo <yukmw0704@gmail.com>
2026-01-08 00:44:40 +0900

    feat: [BaekJoon] 20920번 - 영단어 암기는 괴로워 [Java][C++]

---
+++ 2026-01/src/day_07/boj_20920/Main.java
@@ -0,0 +1,53 @@
+package day_07.boj_20920;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static class Node implements Comparable<Node> {
+        String str;
+        int cnt;
+
+        public Node(String str, int cnt) {
+            this.str = str;
+            this.cnt = cnt;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            if (this.cnt != o.cnt) return Integer.compare(o.cnt, this.cnt);
+            if (this.str.length() != o.str.length()) return Integer.compare(o.str.length(), this.str.length());
+            return this.str.compareTo(o.str);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            if (str.length() < M) continue;
+
+            map.put(str, map.getOrDefault(str, 0) + 1);
+        }
+
+        List<Node> list = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            list.add(new Node(entry.getKey(), entry.getValue()));
+        }
+        list.sort(Comparator.naturalOrder());
+
+        for (Node node : list) {
+            bw.write(node.str);
+            bw.newLine();
+        }
+        bw.flush();
+    }
+}
