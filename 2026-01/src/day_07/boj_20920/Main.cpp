3a75e6c3acb567c681a65f3ea666b7b81d3ba480
FickleBoBo <yukmw0704@gmail.com>
2026-01-08 00:44:40 +0900

    feat: [BaekJoon] 20920번 - 영단어 암기는 괴로워 [Java][C++]

---
+++ 2026-01/src/day_07/boj_20920/Main.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+
+    unordered_map<string, int> mp;
+    for (int i = 0; i < n; i++) {
+        string s;
+        cin >> s;
+
+        if (s.size() < m) continue;
+        mp[s]++;
+    }
+
+    vector<pair<string, int>> v;
+    for (auto& [key, value] : mp) {
+        v.emplace_back(key, value);
+    }
+    sort(v.begin(), v.end(), [](auto& a, auto& b) {
+        if (a.second != b.second) return a.second > b.second;
+        if (a.first.size() != b.first.size()) return a.first.size() > b.first.size();
+        return a.first < b.first;
+    });
+
+    for (auto& x : v) {
+        cout << x.first << '\n';
+    }
+}
