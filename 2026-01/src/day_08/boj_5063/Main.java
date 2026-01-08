18f7eca6bdd23788be6242db1ce1dea963c33546
FickleBoBo <yukmw0704@gmail.com>
2026-01-09 04:48:47 +0900

    feat: [BaekJoon] 5063번 - TGN [Java][C++]

---
+++ 2026-01/src/day_08/boj_5063/Main.java
@@ -0,0 +1,30 @@
+package day_08.boj_5063;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int r = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            if (r > e - c) {
+                bw.write("do not advertise\n");
+            } else if (r < e - c) {
+                bw.write("advertise\n");
+            } else {
+                bw.write("does not matter\n");
+            }
+        }
+
+        bw.flush();
+    }
+}
