7e31d2b6594da6c96c7fde34d6e49015d3570ca3
FickleBoBo <yukmw0704@gmail.com>
2026-01-09 04:48:22 +0900

    feat: [BaekJoon] 2839번 - 설탕 배달 [Java][C++]

---
+++ 2026-01/src/day_08/boj_2839/Main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    if (n >= 0 && n % 5 == 0) {
+        cout << n / 5;
+    } else if (n >= 3 && (n - 3) % 5 == 0) {
+        cout << (n - 3) / 5 + 1;
+    } else if (n >= 6 && (n - 6) % 5 == 0) {
+        cout << (n - 6) / 5 + 2;
+    } else if (n >= 9 && (n - 9) % 5 == 0) {
+        cout << (n - 9) / 5 + 3;
+    } else if (n >= 12 && (n - 12) % 5 == 0) {
+        cout << (n - 12) / 5 + 4;
+    } else {
+        cout << -1;
+    }
+}
