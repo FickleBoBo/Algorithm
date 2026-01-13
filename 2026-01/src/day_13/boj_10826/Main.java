93d4eb8935133eace60b9187243313a935331bb6
FickleBoBo <yukmw0704@gmail.com>
2026-01-13 11:43:51 +0900

    feat: [BaekJoon] 10826번 - 피보나치 수 4 [Java]

---
+++ 2026-01/src/day_13/boj_10826/Main.java
@@ -0,0 +1,25 @@
+package day_13.boj_10826;
+
+import java.io.*;
+import java.math.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        if (N == 0) {
+            System.out.println(0);
+            return;
+        }
+
+        BigInteger[] dp = new BigInteger[1 + N];
+        dp[0] = BigInteger.ZERO;
+        dp[1] = BigInteger.ONE;
+        for (int i = 2; i <= N; i++) {
+            dp[i] = dp[i - 1].add(dp[i - 2]);
+        }
+
+        System.out.println(dp[N]);
+    }
+}
