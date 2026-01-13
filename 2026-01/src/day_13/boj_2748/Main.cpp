148414bd3bcff8787a5cbbf8b60bf0bf6d81197a
FickleBoBo <yukmw0704@gmail.com>
2026-01-13 11:43:23 +0900

    feat: [BaekJoon] 2748번 - 피보나치 수 2 [Java][C++]

---
+++ 2026-01/src/day_13/boj_2748/Main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+constexpr int MAXN = 90;
+
+long long dp[1 + MAXN] = {0, 1};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    for (int i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    cout << dp[n];
+}
