cc53608fb19b63ec16ce8bdb5de3743b16835141
FickleBoBo <yukmw0704@gmail.com>
2026-01-13 11:44:33 +0900

    feat: [BaekJoon] 15624번 - 피보나치 수 7 [Java][C++]

---
+++ 2026-01/src/day_13/boj_15624/Main.java
@@ -0,0 +1,26 @@
+package day_13.boj_15624;
+
+import java.io.*;
+
+public class Main {
+
+    static final int MOD = 1_000_000_007;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        if (N == 0) {
+            System.out.println(0);
+            return;
+        }
+
+        int[] dp = new int[1 + N];
+        dp[1] = 1;
+        for (int i = 2; i <= N; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+        }
+
+        System.out.println(dp[N]);
+    }
+}
