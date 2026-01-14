867fe53a2c699981e9e17452914e881826deb777
FickleBoBo <yukmw0704@gmail.com>
2026-01-14 14:02:35 +0900

    feat: [BaekJoon] 11004번 - K번째 수 [Java][C++]

---
+++ 2026-01/src/day_14/boj_11004/Main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> v(n);
+    for (int& x : v) cin >> x;
+    sort(v.begin(), v.end());
+
+    cout << v[k - 1];
+}
