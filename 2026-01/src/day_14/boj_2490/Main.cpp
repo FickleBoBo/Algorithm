d5fc5d30f59a10b2045828be9a18a2c873d54f3c
FickleBoBo <yukmw0704@gmail.com>
2026-01-14 14:01:17 +0900

    feat: [BaekJoon] 2490번 - 윷놀이 [Java][C++]

---
+++ 2026-01/src/day_14/boj_2490/Main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    for (int i = 0; i < 3; i++) {
+        int cnt = 0;
+        for (int j = 0; j < 4; j++) {
+            int x;
+            cin >> x;
+            cnt += x;
+        }
+
+        if (cnt == 0) {
+            cout << 'D' << '\n';
+        } else if (cnt == 1) {
+            cout << 'C' << '\n';
+        } else if (cnt == 2) {
+            cout << 'B' << '\n';
+        } else if (cnt == 3) {
+            cout << 'A' << '\n';
+        } else {
+            cout << 'E' << '\n';
+        }
+    }
+}
---
+++ 2026-01/src/day_14/boj_2490/Main.java
@@ -0,0 +1,31 @@
+package day_14.boj_2490;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        for (int i = 0; i < 3; i++) {
+            int cnt = 0;
+            st = new StringTokenizer(br.readLine());
+            while (st.hasMoreTokens()) {
+                cnt += Integer.parseInt(st.nextToken());
+            }
+
+            if (cnt == 0) {
+                System.out.println("D");
+            } else if (cnt == 1) {
+                System.out.println("C");
+            } else if (cnt == 2) {
+                System.out.println("B");
+            } else if (cnt == 3) {
+                System.out.println("A");
+            } else {
+                System.out.println("E");
+            }
+        }
+    }
+}
