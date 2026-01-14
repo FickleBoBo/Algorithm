09bb1f950f90e9d0f069c167ad0dcb2b5738e9dc
FickleBoBo <yukmw0704@gmail.com>
2026-01-14 14:00:18 +0900

    feat: [BaekJoon] 1550번 - 16진수 [Java][C++]

---
+++ 2026-01/src/day_14/boj_1550/Main.java
@@ -0,0 +1,12 @@
+package day_14.boj_1550;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        System.out.println(Integer.parseInt(str, 16));
+    }
+}
