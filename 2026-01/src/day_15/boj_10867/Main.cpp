eb1ebc1bfe8df4580334edbff82cf866d4f07383
FickleBoBo <yukmw0704@gmail.com>
2026-01-15 09:07:45 +0900

    feat: [BaekJoon] 10867번 - 중복 빼고 정렬하기 [Java][C++]

---
+++ 2026-01/src/day_15/boj_10867/Main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+constexpr int MAX = 1000;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    vector<bool> visited(MAX + 1 + MAX);
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        visited[MAX + x] = true;
+    }
+
+    for (int i = 0; i < visited.size(); i++) {
+        if (visited[i]) {
+            cout << i - MAX << ' ';
+        }
+    }
+}
