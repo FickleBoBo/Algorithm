480aa5b90ea2fccc10facfd5def3391fc14ea86d
FickleBoBo <yukmw0704@gmail.com>
2026-01-15 09:09:34 +0900

    feat: [BaekJoon] 2947번 - 나무 조각 [Java][C++]

---
+++ 2026-01/src/day_15/boj_2947/Main.java
@@ -0,0 +1,34 @@
+package day_15.boj_2947;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[5];
+        for (int i = 0; i < 5; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+
+                    for (int n : arr) {
+                        sb.append(n).append(" ");
+                    }
+                    sb.append("\n");
+                }
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
