01b38644c1f79909f195b09fee2a7ce4c8d36045
FickleBoBo <yukmw0704@gmail.com>
2026-01-15 09:07:45 +0900

    feat: [BaekJoon] 10867번 - 중복 빼고 정렬하기 [Java][C++]

---
+++ 2026-01/src/day_15/boj_10867/Main.java
@@ -0,0 +1,32 @@
+package day_15.boj_10867;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static final int MAX = 1000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        boolean[] visited = new boolean[MAX + 1 + MAX];
+
+        int N = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(st.nextToken());
+            visited[MAX + x] = true;
+        }
+
+        for (int i = 0; i < visited.length; i++) {
+            if (visited[i]) {
+                sb.append(i - MAX).append(" ");
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
