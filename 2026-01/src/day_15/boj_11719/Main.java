f392b8539f80f3f020615d402e73ff79a8074f98
FickleBoBo <yukmw0704@gmail.com>
2026-01-15 09:08:00 +0900

    feat: [BaekJoon] 11719번 - 그대로 출력하기 2 [Java][C++]

---
+++ 2026-01/src/day_15/boj_11719/Main.java
@@ -0,0 +1,18 @@
+package day_15.boj_11719;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String line;
+        while ((line = br.readLine()) != null) {
+            bw.write(line);
+            bw.newLine();
+        }
+
+        bw.flush();
+    }
+}
