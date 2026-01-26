3026b06340107f24f2efe378df1069c66f41ee63
FickleBoBo <yukmw0704@gmail.com>
2026-01-26 22:38:15 +0900

    feat: [BaekJoon] 2083번 - 럭비 클럽 [Java][C++]

---
+++ 2026-01/src/day_18/boj_2083/Main.java
@@ -0,0 +1,29 @@
+package day_18.boj_2083;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        while (true) {
+            st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            int age = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+
+            if (name.equals("#") && age == 0 && weight == 0) break;
+
+            if (age > 17 || weight >= 80) {
+                sb.append(name).append(" Senior\n");
+            } else {
+                sb.append(name).append(" Junior\n");
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
