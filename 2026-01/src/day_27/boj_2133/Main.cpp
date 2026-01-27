4e2280f23487dc434f0d6895296f161f082c6aab
FickleBoBo <yukmw0704@gmail.com>
2026-01-27 23:49:25 +0900

    feat: [BaekJoon] 2133번 - 타일 채우기 [Java][C++]

---
+++ 2026-01/src/day_27/boj_2133/Main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int dp[1 + 30] = {0, 0, 3};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    if (n % 2) {
+        cout << 0;
+        return 0;
+    }
+
+    for (int i = 4; i <= n; i += 2) {
+        dp[i] += dp[i - 2] * 3 + 2;
+
+        for (int j = i - 4; j >= 2; j -= 2) {
+            dp[i] += dp[j] * 2;
+        }
+    }
+
+    cout << dp[n];
+}
