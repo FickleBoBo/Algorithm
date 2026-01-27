e9275ba73bec0495eb7a4f5b1bc6a67acb79264e
FickleBoBo <yukmw0704@gmail.com>
2026-01-27 23:49:43 +0900

    feat: [BaekJoon] 2693번 - N번째 큰 수 [Java][C++]

---
+++ 2026-01/src/day_27/boj_2693/Main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    for (int tc = 1; tc <= t; tc++) {
+        vector<int> v(10);
+        for (int& x : v) cin >> x;
+        sort(v.begin(), v.end());
+
+        cout << v[7] << '\n';
+    }
+}
---
+++ 2026-01/src/day_27/boj_2693/Main.java
@@ -0,0 +1,26 @@
+package day_27.boj_2693;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= T; tc++) {
+            int[] arr = new int[10];
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < 10; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+            Arrays.sort(arr);
+
+            sb.append(arr[7]).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
