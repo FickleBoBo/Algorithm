ece74a0c19d22267798aeff98c4bac8801fc4157
FickleBoBo <yukmw0704@gmail.com>
2026-01-29 07:37:59 +0900

    feat: [BaekJoon] 7562번 - 나이트의 이동 [Java][C++]

---
+++ 2026-01/src/day_29/boj_7562/Main.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int dr[8] = {-2, -2, -1, 1, 2, 2, 1, -1};
+int dc[8] = {-1, 1, 2, 2, 1, -1, -2, -2};
+
+int bfs(int sr, int sc, int er, int ec, int len) {
+    queue<pair<int, int>> q;
+    q.push({sr, sc});
+
+    vector<vector<bool>> visited(len, vector<bool>(len));
+    visited[sr][sc] = true;
+
+    int dist = 0;
+
+    while (!q.empty()) {
+        int sz = q.size();
+
+        while (sz--) {
+            auto [r, c] = q.front();
+            q.pop();
+
+            if (r == er && c == ec) return dist;
+
+            for (int d = 0; d < 8; d++) {
+                int nr = r + dr[d];
+                int nc = c + dc[d];
+
+                if (nr < 0 || nr >= len || nc < 0 || nc >= len) continue;
+                if (visited[nr][nc]) continue;
+
+                q.push({nr, nc});
+                visited[nr][nc] = true;
+            }
+        }
+
+        dist++;
+    }
+
+    return -1;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    for (int tc = 1; tc <= t; tc++) {
+        int len, sr, sc, er, ec;
+        cin >> len >> sr >> sc >> er >> ec;
+
+        int dist = bfs(sr, sc, er, ec, len);
+        cout << dist << '\n';
+    }
+}
