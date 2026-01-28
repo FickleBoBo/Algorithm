630af3000f0fd52f7f6e5417d57d6e0ea4c3988f
FickleBoBo <yukmw0704@gmail.com>
2026-01-29 01:02:15 +0900

    feat: [BaekJoon] 9328번 - 열쇠 [Java][C++]

---
+++ 2026-01/src/day_28/boj_9328/Main.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int dr[4] = {-1, 0, 1, 0};
+int dc[4] = {0, 1, 0, -1};
+
+int bfs(int n, int m, const vector<vector<char>>& grid, vector<bool>& hasKey) {
+    queue<pair<int, int>> q;
+    q.push({0, 0});
+
+    vector<queue<pair<int, int>>> doors(26);
+
+    vector<vector<bool>> visited(n, vector<bool>(m));
+    visited[0][0] = true;
+
+    int cnt = 0;
+
+    while (!q.empty()) {
+        auto [r, c] = q.front();
+        q.pop();
+
+        if (grid[r][c] == '$') cnt++;
+
+        for (int d = 0; d < 4; d++) {
+            int nr = r + dr[d];
+            int nc = c + dc[d];
+
+            if (nr < 0 || nr >= n || nc < 0 || nc >= m) continue;
+            if (grid[nr][nc] == '*' || visited[nr][nc]) continue;
+
+            if ('A' <= grid[nr][nc] && grid[nr][nc] <= 'Z') {
+                if (!hasKey[grid[nr][nc] - 'A']) {
+                    doors[grid[nr][nc] - 'A'].push({nr, nc});
+                    continue;
+                }
+            }
+
+            if ('a' <= grid[nr][nc] && grid[nr][nc] <= 'z') {
+                hasKey[grid[nr][nc] - 'a'] = true;
+                while (!doors[grid[nr][nc] - 'a'].empty()) {
+                    q.push(doors[grid[nr][nc] - 'a'].front());
+                    doors[grid[nr][nc] - 'a'].pop();
+                }
+            }
+
+            q.push({nr, nc});
+            visited[nr][nc] = true;
+        }
+    }
+
+    return cnt;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    for (int tc = 1; tc <= t; tc++) {
+        int h, w;
+        cin >> h >> w;
+
+        int n = h + 2;
+        int m = w + 2;
+
+        vector<vector<char>> grid(n, vector<char>(m));
+        for (int i = 0; i < n; i++) {
+            grid[i][0] = grid[i][m - 1] = '.';
+        }
+        for (int j = 0; j < m; j++) {
+            grid[0][j] = grid[n - 1][j] = '.';
+        }
+        for (int i = 1; i <= h; i++) {
+            string s;
+            cin >> s;
+
+            for (int j = 1; j <= w; j++) {
+                grid[i][j] = s[j - 1];
+            }
+        }
+
+        vector<bool> hasKey(26);
+
+        string s;
+        cin >> s;
+
+        if (!(s == "0")) {
+            for (char c : s) {
+                hasKey[c - 'a'] = true;
+            }
+        }
+
+        int cnt = bfs(n, m, grid, hasKey);
+        cout << cnt << '\n';
+    }
+}
