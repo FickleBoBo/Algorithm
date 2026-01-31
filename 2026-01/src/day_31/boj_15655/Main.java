df9b056e69039c654a7245b416ae9db5355ab8af
FickleBoBo <yukmw0704@gmail.com>
2026-02-01 03:53:03 +0900

    feat: [BaekJoon] 15655번 - N과 M (6) [Java][C++]

---
+++ 2026-01/src/day_31/boj_15655/Main.java
@@ -0,0 +1,48 @@
+package day_31.boj_15655;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static StringBuilder sb = new StringBuilder();
+    static int N;
+    static int M;
+    static int[] arr;
+    static int[] sel;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        sel = new int[M];
+
+        dfs(0, 0);
+        System.out.println(sb);
+    }
+
+    static void dfs(int idx, int selIdx) {
+        if (selIdx == M) {
+            for (int x : sel) {
+                sb.append(x).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = idx; i < N; i++) {
+            sel[selIdx] = arr[i];
+            dfs(i + 1, selIdx + 1);
+        }
+    }
+}
