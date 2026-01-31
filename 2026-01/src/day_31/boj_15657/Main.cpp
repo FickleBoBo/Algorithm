47d34544119bdc89b23aeff20ea049e20f17ba09
FickleBoBo <yukmw0704@gmail.com>
2026-02-01 03:53:27 +0900

    feat: [BaekJoon] 15657번 - N과 M (8) [Java][C++]

---
+++ 2026-01/src/day_31/boj_15657/Main.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int arr[8];
+int sel[8];
+
+void dfs(int idx, int selIdx) {
+    if (selIdx == m) {
+        for (int i = 0; i < m; i++) {
+            cout << sel[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+    for (int i = idx; i < n; i++) {
+        sel[selIdx] = arr[i];
+        dfs(i, selIdx + 1);
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    sort(arr, arr + n);
+
+    dfs(0, 0);
+}
