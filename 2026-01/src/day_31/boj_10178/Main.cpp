df60880bc68a3f8b92250c63e2ced2b64806334d
FickleBoBo <yukmw0704@gmail.com>
2026-02-01 03:51:12 +0900

    feat: [BaekJoon] 10178번 - 할로윈의 사탕 [Java][C++]

---
+++ 2026-01/src/day_31/boj_10178/Main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    for (int tc = 1; tc <= t; tc++) {
+        int c, v;
+        cin >> c >> v;
+
+        cout << "You get " << (c / v) << " piece(s) and your dad gets " << (c % v) << " piece(s).\n";
+    }
+}
---
+++ 2026-01/src/day_31/boj_10178/Main.java
@@ -0,0 +1,23 @@
+package day_31.boj_10178;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= T; tc++) {
+            st = new StringTokenizer(br.readLine());
+            int c = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            sb.append("You get ").append(c / v).append(" piece(s) and your dad gets ").append(c % v).append(" piece(s).\n");
+        }
+
+        System.out.println(sb);
+    }
+}
