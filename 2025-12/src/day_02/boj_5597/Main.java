281f43ace005b249e827272927db7838f13b2a26
FickleBoBo <yukmw0704@gmail.com>
2026-02-07 23:35:30 +0900

    soft reboot

--- 2025-12/src/day_02/boj_5597/Main.java
+++ 2025-12/src/day_02/boj_5597/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        boolean[] visited = new boolean[1 + 30];
+        boolean[] visited = new boolean[31];
         for (int i = 0; i < 28; i++) {
             int n = Integer.parseInt(br.readLine());
             visited[n] = true;
