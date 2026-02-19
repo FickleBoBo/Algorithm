103524eac689407d949dad6731e4ca667f783216
FickleBoBo <yukmw0704@gmail.com>
2026-02-19 12:28:12 +0900

    soft reboot

--- 2025-12/src/day_23/cf_2148A/Main.java
+++ 2025-12/src/day_23/cf_2148A/Main.java
@@ -10,7 +10,7 @@ public class Main {
         StringTokenizer st;
 
         int t = Integer.parseInt(br.readLine());
-        for (int tc = 1; tc <= t; tc++) {
+        while (t-- > 0) {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int n = Integer.parseInt(st.nextToken());
