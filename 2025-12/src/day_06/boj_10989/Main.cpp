dcca15f4fe75acc39122f16a21c7fd932ea175c3
FickleBoBo <yukmw0704@gmail.com>
2026-03-02 03:10:23 +0900

    title: "[BaekJoon] 10989번 - 수 정렬하기 3 [Java][C++]"

--- 2025-12/src/day_06/boj_10989/Main.cpp
+++ 2025-12/src/day_06/boj_10989/Main.cpp
@@ -1,17 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-constexpr int MAX = 10000;
+const int MAX = 10000;
 int cnt[1 + MAX];
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
     int n;
     cin >> n;
 
-    for (int i = 0; i < n; i++) {
+    while (n--) {
         int x;
         cin >> x;
         cnt[x]++;
