75c5ba3a8154a4c95370c0c19f9df6c218910c3a
FickleBoBo <yukmw0704@gmail.com>
2026-03-02 03:20:26 +0900

    title: "[BaekJoon] 33702번 - 비밀번호 [Java][C++]"

---
+++ 2026-03/src/day_01/boj_33702/Main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int k;
+    cin >> k;
+
+    if (k % 2) {
+        cout << 8;
+    } else {
+        cout << 0;
+    }
+}
