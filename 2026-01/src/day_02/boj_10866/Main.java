394123438533a9eb8ceeb4904c60d241bd0f1577
FickleBoBo <yukmw0704@gmail.com>
2026-03-02 03:14:10 +0900

    title: "[BaekJoon] 10866번 - 덱 [Java][C++]"

--- 2026-01/src/day_02/boj_10866/Main.java
+++ 2026-01/src/day_02/boj_10866/Main.java
@@ -14,7 +14,7 @@ public class Main {
         int back = 10000;
 
         int n = Integer.parseInt(br.readLine());
-        for (int i = 0; i < n; i++) {
+        while (n-- > 0) {
             st = new StringTokenizer(br.readLine());
             String command = st.nextToken();
 
