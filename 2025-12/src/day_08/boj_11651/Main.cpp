1017077308bbf66ae3541197b325c97b02ef7cf3
FickleBoBo <yukmw0704@gmail.com>
2026-03-02 03:11:05 +0900

    title: "[BaekJoon] 11651번 - 좌표 정렬하기 2 [Java][C++]"

--- 2025-12/src/day_08/boj_11651/Main.cpp
+++ 2025-12/src/day_08/boj_11651/Main.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
     int n;
     cin >> n;
@@ -12,7 +12,7 @@ int main() {
     for (auto& [y, x] : v) cin >> x >> y;
     sort(v.begin(), v.end());
 
-    for (auto& [y, x] : v) {
+    for (auto [y, x] : v) {
         cout << x << ' ' << y << '\n';
     }
 }
