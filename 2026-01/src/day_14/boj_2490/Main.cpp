08bbf22c90eafac19cb2d9b51554876dddddc441
FickleBoBo <yukmw0704@gmail.com>
2026-03-02 03:59:55 +0900

    title: "[BaekJoon] 2490번 - 윷놀이 [Java][C++]"

--- 2026-01/src/day_14/boj_2490/Main.cpp
+++ 2026-01/src/day_14/boj_2490/Main.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
     for (int i = 0; i < 3; i++) {
         int cnt = 0;
