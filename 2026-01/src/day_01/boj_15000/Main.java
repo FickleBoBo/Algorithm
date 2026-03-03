62126385fd2cf072aa0db6aed5e32cdc46ff9cf8
FickleBoBo <yukmw0704@gmail.com>
2026-03-04 03:31:10 +0900

    title: "[BaekJoon] 15000번 - CAPS [Java][C++]"

--- 2026-01/src/day_01/boj_15000/Main.java
+++ 2026-01/src/day_01/boj_15000/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String str = br.readLine();
-        System.out.println(str.toUpperCase());
+        String s = br.readLine();
+        System.out.println(s.toUpperCase());
     }
 }
