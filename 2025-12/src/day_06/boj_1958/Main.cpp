9fc4d71bc0f6b452bc179d8969aec3f5fd9baf9a
FickleBoBo <yukmw0704@gmail.com>
2026-03-04 08:54:37 +0900

    title: "[BaekJoon] 1958번 - LCS 3 [Java][C++]"

--- 2025-12/src/day_06/boj_1958/Main.cpp
+++ 2025-12/src/day_06/boj_1958/Main.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int dp[101][101][101];
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
     string s1, s2, s3;
     cin >> s1 >> s2 >> s3;
