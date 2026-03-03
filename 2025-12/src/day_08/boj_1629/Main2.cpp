70b65473a6e06d1ecd8332f7126e21b47081f74f
FickleBoBo <yukmw0704@gmail.com>
2026-03-04 03:20:59 +0900

    title: "[BaekJoon] 1629번 - 곱셈 [Java][C++]"

--- 2025-12/src/day_08/boj_1629/Main2.cpp
+++ 2025-12/src/day_08/boj_1629/Main2.cpp
@@ -13,8 +13,8 @@ long long modpow(long long a, long long b, long long c) {
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
     int a, b, c;
     cin >> a >> b >> c;
