53e6a28bec7c87b34ef93142d4fc7dfba0508899
FickleBoBo <yukmw0704@gmail.com>
2026-03-04 08:52:52 +0900

    title: "[BaekJoon] 3009번 - 네 번째 점 [Java][C++]"

--- 2025-12/src/day_05/boj_3009/Main2.cpp
+++ 2025-12/src/day_05/boj_3009/Main2.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
     int x1, y1, x2, y2, x3, y3;
     cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
