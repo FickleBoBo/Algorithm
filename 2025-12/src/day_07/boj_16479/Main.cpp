008e1faabbf7083fb95ad43380fba286a7f874e4
FickleBoBo <yukmw0704@gmail.com>
2026-03-04 03:20:37 +0900

    title: "[BaekJoon] 16479번 - 컵라면 측정하기 [Java][C++]"

--- 2025-12/src/day_07/boj_16479/Main.cpp
+++ 2025-12/src/day_07/boj_16479/Main.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
     int k, d1, d2;
     cin >> k >> d1 >> d2;
