7e58bfc7ef47ac9cb29009620ba727e239dcfd5a
FickleBoBo <yukmw0704@gmail.com>
2026-03-04 03:40:26 +0900

    title: "[BaekJoon] 16170번 - 오늘의 날짜는? [Java][C++]"

--- 2026-01/src/day_09/boj_16170/Main.cpp
+++ 2026-01/src/day_09/boj_16170/Main.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
     cout << "2026" << '\n';
     cout << "01" << '\n';
