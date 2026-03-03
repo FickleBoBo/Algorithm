e554f12ef13db2eebd92c01957ab2c62e27dfe1c
FickleBoBo <yukmw0704@gmail.com>
2026-03-04 08:57:15 +0900

    title: "[BaekJoon] 11050번 - 이항 계수 1 [Java]"

--- 2025-12/src/day_29/boj_11050/Main.cpp
+++ 2025-12/src/day_29/boj_11050/Main.cpp
@@ -9,8 +9,8 @@ int nCr(int n, int r) {
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
     int n, k;
     cin >> n >> k;
