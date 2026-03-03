2be399c4c71b042cfe79a39bf619f436d3ce44ab
FickleBoBo <yukmw0704@gmail.com>
2026-03-04 03:18:53 +0900

    title: "[BaekJoon] 10101번 - 삼각형 외우기 [Java][C++]"

--- 2025-12/src/day_04/boj_10101/Main.cpp
+++ 2025-12/src/day_04/boj_10101/Main.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
     int a1, a2, a3;
     cin >> a1 >> a2 >> a3;
