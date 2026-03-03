97574ee3d1e4591566ffbbb534ecaa16bd10ed43
FickleBoBo <yukmw0704@gmail.com>
2026-03-04 03:43:43 +0900

    title: "[BaekJoon] 6603번 - 로또 [Java][C++]"

--- 2026-01/src/day_31/boj_6603/Main.cpp
+++ 2026-01/src/day_31/boj_6603/Main.cpp
@@ -21,8 +21,8 @@ void dfs(int idx, int selIdx) {
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
     while (true) {
         cin >> k;
