c43a39d87265e0b92f9c8dd9a1b6477c3cb3a9bc
FickleBoBo <yukmw0704@gmail.com>
2026-03-04 09:17:46 +0900

    title: "[BaekJoon] 1106번 - 호텔 [Java][C++]"

--- 2025-12/src/day_12/boj_1106/Main.cpp
+++ 2025-12/src/day_12/boj_1106/Main.cpp
@@ -1,12 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-constexpr int MAX = 100000;
+const int MAX = 100000;
 int dp[21][1101];
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
     int c, n;
     cin >> c >> n;
