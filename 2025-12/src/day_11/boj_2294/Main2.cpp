652862916606f0a00dd3a144b3845bfc22140141
FickleBoBo <yukmw0704@gmail.com>
2026-03-04 09:16:15 +0900

    title: "[BaekJoon] 2294번 - 동전 2 [Java][C++]"

--- 2025-12/src/day_11/boj_2294/Main2.cpp
+++ 2025-12/src/day_11/boj_2294/Main2.cpp
@@ -1,12 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-constexpr int MAX = 10001;
+const int MAX = 10001;
 int dp[10001];
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
     int n, k;
     cin >> n >> k;
