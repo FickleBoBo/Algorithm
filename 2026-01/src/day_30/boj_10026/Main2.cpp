3ee5da131f891ea767440eca921a393e22d89cbe
FickleBoBo <yukmw0704@gmail.com>
2026-03-04 09:34:10 +0900

    title: "[BaekJoon] 10026번 - 적록색약 [Java][C++]"

--- 2026-01/src/day_30/boj_10026/Main2.cpp
+++ 2026-01/src/day_30/boj_10026/Main2.cpp
@@ -37,8 +37,8 @@ void dfs(int sr, int sc, bool isBlind) {
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
     cin >> n;
 
