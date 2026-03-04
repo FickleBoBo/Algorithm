f155dc2699d5b8db1112f265100716119b9a89e0
FickleBoBo <yukmw0704@gmail.com>
2026-03-04 09:18:22 +0900

    title: "[BaekJoon] 2851번 - 슈퍼 마리오 [Java][C++]"

--- 2025-12/src/day_12/boj_2851/Main.cpp
+++ 2025-12/src/day_12/boj_2851/Main.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
     vector<int> v(10);
     for (int& x : v) cin >> x;
