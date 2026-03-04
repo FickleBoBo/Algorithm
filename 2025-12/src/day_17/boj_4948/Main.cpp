dcfecd6f3df24d7cbd09a2cd02134733c45cf27c
FickleBoBo <yukmw0704@gmail.com>
2026-03-04 09:01:45 +0900

    title: "[BaekJoon] 4948번 - 베르트랑 공준 [Java][C++]"

--- 2025-12/src/day_17/boj_4948/Main.cpp
+++ 2025-12/src/day_17/boj_4948/Main.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-constexpr int MAX = 123456 * 2;
+const int MAX = 123456 * 2;
 bool isPrime[1 + MAX];
 
 void sieve() {
@@ -18,8 +18,8 @@ void sieve() {
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
     sieve();
 
