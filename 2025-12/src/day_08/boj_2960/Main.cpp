f06a1b3558144e7e892f4c9f8578e62c34848be4
FickleBoBo <yukmw0704@gmail.com>
2026-03-04 09:10:12 +0900

    title: "[BaekJoon] 2960번 - 에라토스테네스의 체 [Java][C++]"

--- 2025-12/src/day_08/boj_2960/Main.cpp
+++ 2025-12/src/day_08/boj_2960/Main.cpp
@@ -28,8 +28,8 @@ int sieve(int n, int k) {
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
     int n, k;
     cin >> n >> k;
