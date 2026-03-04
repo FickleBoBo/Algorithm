4b800bc007b97b55312f3e8f93ee7d01e07e63f6
FickleBoBo <yukmw0704@gmail.com>
2026-03-04 09:35:50 +0900

    title: "[BaekJoon] 16953번 - A → B [Java][C++]"

--- 2026-01/src/day_31/boj_16953/Main.cpp
+++ 2026-01/src/day_31/boj_16953/Main.cpp
@@ -37,8 +37,8 @@ int bfs(int a, int b) {
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
     int a, b;
     cin >> a >> b;
