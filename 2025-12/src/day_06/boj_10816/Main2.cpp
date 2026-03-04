8200fb015c6e4bea38f6435f1641f8c9f556fcac
FickleBoBo <yukmw0704@gmail.com>
2026-03-04 09:06:00 +0900

    title: "[BaekJoon] 10816번 - 숫자 카드 2 [Java][C++]"

--- 2025-12/src/day_06/boj_10816/Main2.cpp
+++ 2025-12/src/day_06/boj_10816/Main2.cpp
@@ -1,12 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-constexpr int MAX = 10000000;
+const int MAX = 10000000;
 int cnt[20000001];
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
     int n;
     cin >> n;
