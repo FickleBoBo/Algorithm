4ee7941c0d732828562d973755219c9f5a37bfbb
FickleBoBo <yukmw0704@gmail.com>
2026-03-06 05:54:51 +0900

    title: "[BaekJoon] 16478번 - 원의 분할 [Java][C++]"

---
+++ 2026-03/src/day_04/boj_16478/Main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << fixed << setprecision(6) << (double)a * c / b;
+}
