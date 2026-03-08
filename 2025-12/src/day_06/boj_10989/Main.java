2ad2b1334f574d38381103569e9f198768f97ee8
FickleBoBo <yukmw0704@gmail.com>
2026-03-09 08:10:40 +0900

    title: "[BaekJoon] 10989번 - 수 정렬하기 3 [Java][C++]"

--- 2025-12/src/day_06/boj_10989/Main.java
+++ 2025-12/src/day_06/boj_10989/Main.java
@@ -4,20 +4,19 @@ import java.io.*;
 
 public class Main {
 
-    static final int MAX = 10000;
+    static final int MX = 10000;
+    static int[] cnt = new int[1 + MX];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
-
-        int[] cnt = new int[1 + MAX];
         while (n-- > 0) {
             cnt[Integer.parseInt(br.readLine())]++;
         }
 
-        for (int i = 1; i <= MAX; i++) {
+        for (int i = 1; i <= MX; i++) {
             for (int j = 1; j <= cnt[i]; j++) {
                 sb.append(i).append("\n");
             }
