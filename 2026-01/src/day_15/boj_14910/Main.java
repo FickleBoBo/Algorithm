c10a480a70b1adbc7f1470b2f0c4fa9fc8296ebd
FickleBoBo <yukmw0704@gmail.com>
2026-03-26 19:21:58 +0900

    feat: [BaekJoon] 14910번 - 오르막 [Java][C++]

--- 2026-01/src/day_15/boj_14910/Main.java
+++ 2026-01/src/day_15/boj_14910/Main.java
@@ -8,7 +8,7 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int prv = -1_000_000;
+        int prv = -1000000;
         while (st.hasMoreTokens()) {
             int x = Integer.parseInt(st.nextToken());
             if (x >= prv) {
