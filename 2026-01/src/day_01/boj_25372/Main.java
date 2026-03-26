b40db3fb46d63ff71b5b6f777ab4edbd8aa19c3d
FickleBoBo <yukmw0704@gmail.com>
2026-03-26 17:30:23 +0900

    feat: [BaekJoon] 25372번 - 성택이의 은밀한 비밀번호 [Java][C++]

--- 2026-01/src/day_01/boj_25372/Main.java
+++ 2026-01/src/day_01/boj_25372/Main.java
@@ -8,10 +8,10 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
-        for (int i = 0; i < n; i++) {
-            String str = br.readLine();
+        while (n-- > 0) {
+            String s = br.readLine();
 
-            if (6 <= str.length() && str.length() <= 9) {
+            if (6 <= s.length() && s.length() <= 9) {
                 bw.write("yes\n");
             } else {
                 bw.write("no\n");
