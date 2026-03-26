10fd9cefaa70f4a99fc68b41369f6782a8c004ea
FickleBoBo <yukmw0704@gmail.com>
2026-03-26 17:30:24 +0900

    feat: [BaekJoon] 7891번 - Can you add this? [Java][C++]

--- 2026-01/src/day_01/boj_7891/Main.java
+++ 2026-01/src/day_01/boj_7891/Main.java
@@ -6,8 +6,8 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
         int t = Integer.parseInt(br.readLine());
         while (t-- > 0) {
