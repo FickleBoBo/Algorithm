264e5fadfb3df5a7478cba2db54676edde28a37c
FickleBoBo <yukmw0704@gmail.com>
2026-03-26 18:29:32 +0900

    feat: [BaekJoon] 9610번 - 사분면 [Java][C++]

--- 2026-01/src/day_06/boj_9610/Main.cpp
+++ 2026-01/src/day_06/boj_9610/Main.cpp
@@ -1,11 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int cnt[5];
+
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-
-    vector<int> cnt(5);
 
     int n;
     cin >> n;
