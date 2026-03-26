41b0d0ce33663efd98a56a1adad77740fefe6230
FickleBoBo <yukmw0704@gmail.com>
2026-03-26 14:37:25 +0900

    feat: [BaekJoon] 24263번 - 알고리즘 수업 - 알고리즘의 수행 시간 2 [Java][C++]

---
+++ 2026-03/src/day_14/boj_24263/Main.java
@@ -0,0 +1,13 @@
+package day_14.boj_24263;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(n);
+        System.out.println(1);
+    }
+}
