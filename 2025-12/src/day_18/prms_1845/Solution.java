a00fd5271c165101d36aec57298bae845cb5e9d0
FickleBoBo <yukmw0704@gmail.com>
2026-04-01 22:06:46 +0900

    feat: [Programmers] 1845번 - 폰켓몬 [Java][C++]

--- 2025-12/src/day_18/prms_1845/Solution.java
+++ 2025-12/src/day_18/prms_1845/Solution.java
@@ -5,8 +5,8 @@ import java.util.*;
 class Solution {
     public int solution(int[] nums) {
         Set<Integer> set = new HashSet<>();
-        for (int n : nums) {
-            set.add(n);
+        for (int x : nums) {
+            set.add(x);
         }
 
         return Math.min(set.size(), nums.length / 2);
