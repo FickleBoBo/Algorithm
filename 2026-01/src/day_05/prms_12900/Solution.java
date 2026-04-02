be8cac5a734b818e4a46b4a4c485e4394d69a3d0
FickleBoBo <yukmw0704@gmail.com>
2026-04-02 21:09:40 +0900

    feat: [Programmers] 12900번 - 2 x n 타일링 [Java][C++]

--- 2026-01/src/day_05/prms_12900/Solution.java
+++ 2026-01/src/day_05/prms_12900/Solution.java
@@ -10,7 +10,6 @@ class Solution {
         int[] dp = new int[1 + n];
         dp[1] = 1;
         dp[2] = 2;
-
         for (int i = 3; i <= n; i++) {
             dp[i] = (dp[i - 2] + dp[i - 1]) % MOD;
         }
