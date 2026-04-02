ffa78460c71dfa8cd484afe50eb12347aa4a256a
FickleBoBo <yukmw0704@gmail.com>
2026-04-02 19:05:04 +0900

    feat: [BaekJoon] 9658번 - 돌 게임 4 [Java][C++]

---
+++ 2026-04/src/day_02/boj_9658/Main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool dp[1001];
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    dp[2] = dp[4] = true;
+    for (int i = 5; i <= n; i++) {
+        if (!dp[i - 1] || !dp[i - 3] || !dp[i - 4]) {
+            dp[i] = true;
+        }
+    }
+
+    if (dp[n]) {
+        cout << "SK";
+    } else {
+        cout << "CY";
+    }
+}
