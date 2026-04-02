7ed79860193fe010ea54a9370c18557d02b099c9
FickleBoBo <yukmw0704@gmail.com>
2026-04-02 19:53:43 +0900

    feat: [BaekJoon] 1003번 - 피보나치 함수 [Java][C++]

--- 2026-01/src/day_11/boj_1003/Main.cpp
+++ 2026-01/src/day_11/boj_1003/Main.cpp
@@ -1,12 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int dp[41][2] = {{1, 0}, {0, 1}};
+int dp[41][2];
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
+    dp[0][0] = dp[1][1] = 1;
     for (int i = 2; i < 41; i++) {
         dp[i][0] = dp[i - 1][0] + dp[i - 2][0];
         dp[i][1] = dp[i - 1][1] + dp[i - 2][1];
