958a1f0d7027bd6440ed7b87da3522f45373d8e7
FickleBoBo <yukmw0704@gmail.com>
2026-04-06 19:43:44 +0900

    feat: [BaekJoon] 32384번 - 사랑은 고려대입니다 [Java][C++]

---
+++ 2026-03/src/day_31/boj_32384/Main.java
@@ -0,0 +1,14 @@
+package day_31.boj_32384;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        while (n-- > 0) {
+            System.out.print("LoveisKoreaUniversity ");
+        }
+    }
+}
