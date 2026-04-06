a048a4796c1b1943bd27c6bb311d9a40ef95b629
FickleBoBo <yukmw0704@gmail.com>
2026-04-06 23:08:44 +0900

    feat: [BaekJoon] 2217번 - 로프 [Java][C++]

---
+++ 2026-03/src/day_17/boj_2217/Main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+    for (int& x : v) cin >> x;
+    sort(v.begin(), v.end());
+
+    int mx = 0;
+    for (int i = 0; i < n; i++) {
+        mx = max(mx, v[i] * (n - i));
+    }
+
+    cout << mx;
+}
