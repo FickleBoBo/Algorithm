40aaff9582f7e8873a6352626bfbc2b1cd8229d1
FickleBoBo <yukmw0704@gmail.com>
2026-04-14 18:00:47 +0900

    feat: [BaekJoon] 24883번 - 자동완성 [Java][C++]

---
+++ 2026-04/src/day_07/boj_24883/Main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    string s;
+    cin >> s;
+
+    if (s == "N" || s == "n") {
+        cout << "Naver D2";
+    } else {
+        cout << "Naver Whale";
+    }
+}
