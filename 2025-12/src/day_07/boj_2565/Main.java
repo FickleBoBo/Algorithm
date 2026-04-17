ef3bc8529f54cc854b1069d5b3c87689e0e5ed80
FickleBoBo <yukmw0704@gmail.com>
2026-04-17 12:59:38 +0900

    feat: [BaekJoon] 2565번 - 전깃줄 [Java][C++]

--- 2025-12/src/day_07/boj_2565/Main.java
+++ 2025-12/src/day_07/boj_2565/Main.java
@@ -10,20 +10,20 @@ public class Main {
 
         int n = Integer.parseInt(br.readLine());
 
-        int[][] map = new int[n][2];
+        int[][] arr = new int[n][2];
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            map[i][0] = Integer.parseInt(st.nextToken());
-            map[i][1] = Integer.parseInt(st.nextToken());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(map, (o1, o2) -> Integer.compare(o1[1], o2[1]));
+        Arrays.sort(arr, (o1, o2) -> Integer.compare(o1[1], o2[1]));
 
         int[] dp = new int[n];
         Arrays.fill(dp, 1);
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < i; j++) {
-                if (map[i][0] > map[j][0]) dp[i] = Math.max(dp[i], dp[j] + 1);
+                if (arr[i][0] > arr[j][0]) dp[i] = Math.max(dp[i], dp[j] + 1);
             }
         }
 
