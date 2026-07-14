7dfaa4a68fe8ed059003f5614ce65cf8e893648a
FickleBoBo <yukmw0704@gmail.com>
2026-07-14 09:13:04 +0900

    feat: [Programmers] 120583번 - 중복된 숫자 개수 [Java][C++]

---
+++ 2026-07/src/day_14/prms_120583/Solution.java
@@ -0,0 +1,13 @@
+package day_14.prms_120583;
+
+class Solution {
+    public int solution(int[] array, int n) {
+        int cnt = 0;
+
+        for (int x : array) {
+            if (x == n) cnt++;
+        }
+
+        return cnt;
+    }
+}
