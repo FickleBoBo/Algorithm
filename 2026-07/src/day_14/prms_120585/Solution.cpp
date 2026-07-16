93de42b6ddb1cc151de467d332a643ae67cd1bbe
FickleBoBo <yukmw0704@gmail.com>
2026-07-16 12:39:38 +0900

    feat: [Programmers] 120585번 - 머쓱이보다 키 큰 사람 [Java][C++]

--- 2026-07/src/day_14/prms_120585/Solution.cpp
+++ 2026-07/src/day_14/prms_120585/Solution.cpp
@@ -4,7 +4,6 @@ using namespace std;
 
 int solution(vector<int> array, int height) {
     int cnt = 0;
-
     for (int x : array) {
         if (x > height) cnt++;
     }
--- 2026-07/src/day_14/prms_120585/Solution.java
+++ 2026-07/src/day_14/prms_120585/Solution.java
@@ -3,7 +3,6 @@ package day_14.prms_120585;
 class Solution {
     public int solution(int[] array, int height) {
         int cnt = 0;
-
         for (int x : array) {
             if (x > height) cnt++;
         }
