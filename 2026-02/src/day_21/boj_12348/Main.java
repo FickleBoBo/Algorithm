8c69024d686428997d71c1bfd7efc874b27d793b
FickleBoBo <yukmw0704@gmail.com>
2026-07-17 09:00:16 +0900

    feat: [BaekJoon] 12348번 - 분해합 2 [Java][C++]

--- 2026-02/src/day_21/boj_12348/Main.java
+++ 2026-02/src/day_21/boj_12348/Main.java
@@ -6,16 +6,16 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String str = br.readLine();
-        int len = str.length();
+        String s = br.readLine();
+        int len = s.length();
 
-        long n = Long.parseLong(str);
+        long n = Long.parseLong(s);
         for (long i = n - 9 * len; i <= n; i++) {
-            long num = i;
-            long sum = num;
-            while (num > 0) {
-                sum += num % 10;
-                num /= 10;
+            long x = i;
+            long sum = x;
+            while (x > 0) {
+                sum += x % 10;
+                x /= 10;
             }
 
             if (sum == n) {
