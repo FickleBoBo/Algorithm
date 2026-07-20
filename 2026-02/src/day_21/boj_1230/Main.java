f3ada478e23e219b6f187b4d4a39ccdc42ee9b73
FickleBoBo <yukmw0704@gmail.com>
2026-07-20 16:29:44 +0900

    feat: [BaekJoon] 1230번 - 문자열 거리 [Java][C++]

--- 2026-02/src/day_21/boj_1230/Main.java
+++ 2026-02/src/day_21/boj_1230/Main.java
@@ -9,10 +9,10 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        char[] str1 = br.readLine().toCharArray();
-        char[] str2 = br.readLine().toCharArray();
-        int n = str1.length;
-        int m = str2.length;
+        char[] s1 = br.readLine().toCharArray();
+        char[] s2 = br.readLine().toCharArray();
+        int n = s1.length;
+        int m = s2.length;
 
         int[][] match = new int[1 + n][1 + m];
         int[][] insert = new int[1 + n][1 + m];
@@ -29,18 +29,18 @@ public class Main {
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
-                if (str1[i - 1] == str2[j - 1]) {
+                if (s1[i - 1] == s2[j - 1]) {
                     match[i][j] = Math.min(match[i - 1][j - 1], insert[i - 1][j - 1]);
                 }
                 insert[i][j] = Math.min(match[i][j - 1] + 1, insert[i][j - 1]);
             }
         }
 
-        int result = Math.min(match[n][m], insert[n][m]);
-        if (result == INF) {
+        int res = Math.min(match[n][m], insert[n][m]);
+        if (res == INF) {
             System.out.println(-1);
         } else {
-            System.out.println(result);
+            System.out.println(res);
         }
     }
 }
